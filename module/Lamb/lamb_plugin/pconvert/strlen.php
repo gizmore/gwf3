a54157c087f6971e2f568bc22ad3ac0f3889a9cb
Gizmore <gizmore@wechall.net>
2011-05-08 05:58:16 +0000

    2 new lamb plugins: strlen and strlen8.

---
+++ module/Lamb/lamb_plugin/pconvert/strlen.php
@@ -0,0 +1,3 @@
+<?php # Usage: %TRIGGER%strlen <string>. Print the length of an ascii string. See %TRIGGER%strlen8.
+$bot->reply(strlen($message));
+?>
---
+++ module/Lamb/lamb_plugin/pconvert/strlen8.php
@@ -0,0 +1,3 @@
+<?php # Usage: %TRIGGER%strlen8 <string>. Print the length of an utf8 string. See %TRIGGER%strlen.
+$bot->reply(Common::strlen($message));
+?>
