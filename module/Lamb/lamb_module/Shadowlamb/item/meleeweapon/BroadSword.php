0c7076de24a0a884f1f96270522850fc0649ecfc
Gizmore <gizmore@wechall.net>
2011-05-10 09:09:37 +0000

    New Item Broad Sword

---
+++ module/Lamb/lamb_module/Shadowlamb/item/meleeweapon/BroadSword.php
@@ -0,0 +1,19 @@
+<?php
+final class Item_BroadSword extends SR_MeleeWeapon
+{
+	public function getAttackTime() { return 30; }
+	public function getItemLevel() { return 6; }
+	public function getItemWeight() { return 950; }
+	public function getItemPrice() { return 725; }
+	public function getItemDescription() { return 'A broad steel sword.'; }
+	public function getItemModifiersA(SR_Player $player)
+	{
+		return array(
+			'attack' => 8.5, 
+			'min_dmg' => 2.5,
+			'max_dmg' => 8.5,
+		);
+	}
+	
+}
+?>
