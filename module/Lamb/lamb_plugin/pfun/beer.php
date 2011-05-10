4dc3712557d382d64c2db6de0c44d8f8976bcc0c
Gizmore <gizmore@wechall.net>
2011-05-10 11:14:48 +0000

    Fixed Lambs .beer command

--- module/Lamb/lamb_plugin/pfun/beer.php
+++ module/Lamb/lamb_plugin/pfun/beer.php
@@ -1,14 +1,14 @@
 <?php # Codemonkey cool down get a beer...
 $server instanceof Lamb_Server;
 $user instanceof Lamb_User;
-$channels = $server->getChannels();
-$channel = array_shift($channels);
-$channel instanceof Lamb_Channel;
-$channel = $channel->getName();
 if ($user->getName() === 'gizmore') {
 	$steal = '.';
-} else {
+}
+elseif (rand(0,1)) {
+	$steal = '.';
+}
+else {
 	$steal = ', but gizmore steals it!';
 }
-$server->sendAction($channel, sprintf('hands %s a cold beer%s', $user->getName(), $steal));
+$server->sendAction($origin, sprintf('hands %s a cold beer%s', $user->getName(), $steal));
 ?>
