82ffd93f37672e8ec9914deb89d982cf435de73f
spaceone <space@wechall.net>
2011-05-14 09:33:31 +0000

    DIE BUG FIXED

--- module/PM/unread.php
+++ module/PM/unread.php
@@ -10,7 +10,6 @@ function module_PM_unread(array $args, $format='[%s]')
 		$count = GDO::table('GWF_PM')->countRows("pm_owner=$userid AND pm_to=$userid AND pm_options&$read=0");
 		if ($count > 0)
 		{
-			die("$count");
 			return sprintf($pattern, $count);
 		}
 	}
