adfb222f4e213309b695153a9347bda148b96bd9
Gizmore <gizmore@wechall.net>
2011-05-16 21:54:43 +0000

    Added stubs for DB_Postgres (thx livinskull)

--- inc/GDO/db/GDO_DB_postgres.php
+++ inc/GDO/db/GDO_DB_postgres.php
@@ -1,6 +1,32 @@
 <?php
 final class GDO_DB_postgres extends GDO_Database
 {
+	public function connect($host, $user, $pass) {}
+	public function setCharset($charset) {}
+	public function useDatabase($db) {}
 	
+	public function queryRead($query) {}
+	public function queryWrite($query) {}
+	public function free($result) {}
+	
+	public function fetchRow($result) {}
+	public function fetchAssoc($result) {}
+	public function fetchObject($result) {}
+	
+	public function insertID() {}
+	public function numRows($result) {}
+	public function affectedRows() {}
+
+	public function escape($s) {}
+	public function escapeIdentifier($s) {}
+	
+	public function tableExists($tablename) {}
+	public function truncateTable($tablename) {}
+	public function dropTable($tablename) {}
+	public function dropColumn($tablename, $columnname) {}
+	public function createTable($tablename, array $defines) {}
+	public function createColumn($tablename, $columnname, array $define) {}
+	public function renameTable($old_tablename, $new_tablename) {}
+	public function changeColumn($tablename, $old_columnname, $new_columnname, array $define) {}
 }
 ?>
