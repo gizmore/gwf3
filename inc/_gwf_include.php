6d1b2546f1ddd8c7e2222878952ef4052c26f147
Gizmore <gizmore@wechall.net>
2011-05-27 01:16:55 +0000

    GWF_WEB_ROOT detect cleanup

--- inc/_gwf_include.php
+++ inc/_gwf_include.php
@@ -1,5 +1,5 @@
 <?php
-define('GWF_CORE_VERSION', '3.01-2011.MAY.04');
+define('GWF_CORE_VERSION', '3.01-2011.MAY.27');
 
 # Get the config
 if (!defined('GWF_HAVE_CONFIG'))
@@ -9,30 +9,30 @@ if (!defined('GWF_HAVE_CONFIG'))
 	define('GWF_HAVE_CONFIG', 1);
 }
 
-# Lang Web Root
+
+# Web Root
+$root = GWF_WEB_ROOT_NO_LANG;
 if (isset($_SERVER['REQUEST_URI']))
 {
-	if (preg_match('#^'.GWF_WEB_ROOT_NO_LANG.'([a-z]{2}/)#', $_SERVER['REQUEST_URI'], $matches)) {
-		if ($matches[1] === 'pm/') {
-			define('GWF_WEB_ROOT', GWF_WEB_ROOT_NO_LANG);
-		} else {
-			define('GWF_WEB_ROOT', GWF_WEB_ROOT_NO_LANG.$matches[1]);
+	if (preg_match('#^'.GWF_WEB_ROOT_NO_LANG.'([a-z]{2}/)#', $_SERVER['REQUEST_URI'], $matches))
+	{
+		if ($matches[1] !== 'pm/')
+		{
+			$root .= $matches[1];
 		}
-	} else {
-		define('GWF_WEB_ROOT', GWF_WEB_ROOT_NO_LANG);
 	}
 }
-else
-{
-	define('GWF_WEB_ROOT', GWF_WEB_ROOT_NO_LANG);
-}
+define('GWF_WEB_ROOT', $root);
+
 
 # Require the Database
 require_once 'inc/GDO/GDO.php';
 
+
 # Require the util
 require_once 'inc/util/Common.php';
 require_once 'inc/_gwf_autoload.php';
+
 
 # Enable the error handlers
 GWF_Debug::enableErrorHandler();
