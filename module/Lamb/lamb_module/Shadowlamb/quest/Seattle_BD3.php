77fd226f99bbf9e6f1257f55723a89bdd1a12a86
Gizmore <gizmore@wechall.net>
2011-05-31 07:01:04 +0000

    2 new quests at the Seattle Blacksmith

---
+++ module/Lamb/lamb_module/Shadowlamb/quest/Seattle_BD3.php
@@ -0,0 +1,86 @@
+<?php
+final class Quest_Seattle_BD3 extends SR_Quest
+{
+	const NEED_LEG = 6;
+	const NEED_ARMOR = 6;
+	const NEED_HELMET = 6;
+	
+	const REWARD_XP = 4;
+	const REWARD_NUYEN = 1500;
+	
+	public function getQuestName() { return 'PoorSmith3'; }
+	public function getQuestDescription() { return sprintf('Bring %s ChainLegs, %s ChainBody and %s ChainHelmet to the Seattle Blacksmith.', self::NEED_LEG, self::NEED_ARMOR, self::NEED_HELMET); }
+	
+	public function checkQuest(SR_NPC $npc, SR_Player $player)
+	{
+		if ($this->isDone($player))
+		{
+			return false;
+		}
+		
+		$data = $this->getQuestData();
+		if (!isset($data['LEG'])) { $data = array('LEG'=>0,'ARMOR'=>0,'HELMET'=>0); }
+		
+		$data['LEG'] = $this->giveQuesties($player, $npc, 'ChainLegs', $data['LEG'], self::NEED_LEG);
+		$data['ARMOR'] = $this->giveQuesties($player, $npc, 'ChainBody', $data['ARMOR'], self::NEED_ARMOR);
+		$data['HELMET'] = $this->giveQuesties($player, $npc, 'ChainHelmet', $data['HELMET'], self::NEED_HELMET);
+		
+		$this->saveQuestData($data);
+		
+		$need_leg = $data['LEG'] - self::NEED_LEG;
+		$need_armor = $data['ARMOR'] - self::NEED_ARMOR;
+		$need_helmet = $data['HELMET'] - self::NEED_HELMET;
+		
+		$need = array();
+		if ($need_leg > 0)
+		{
+			$need[$need_leg.' ChainLegs'] = $need_leg;
+		}
+		if ($need_armor > 0)
+		{
+			$need[$need_armor.' ChainBodies'] = $need_armor;
+		}
+		if ($need_helmet > 0)
+		{
+			$need[$need_helmet.' ChainHelmets'] = $need_helmet;
+		}
+		
+		if (count($need) === 0)
+		{
+			$npc->reply('Thank you very very much.');
+			$npc->reply('Please take this as reward!');
+			$player->message('The dwarf hands you %s. You also gain %s XP.', Shadowfunc::displayPrice(self::REWARD_NUYEN), self::REWARD_XP);
+			$player->giveXP(self::REWARD_XP);
+			$player->giveNuyen(self::REWARD_NUYEN);
+			$this->onSolve($player);
+		}
+		else
+		{
+			$npc->reply(sprintf('I still need %s.', Common::implodeHuman(array_keys($need))));
+		}
+		return true;
+	}
+	
+	public function onNPCQuestTalkB(SR_TalkingNPC $npc, SR_Player $player, $word)
+	{
+		switch ($word)
+		{
+			case 'confirm':
+				$npc->reply('It would be so great if you could help me again, yes?');
+				break;
+			case 'shadowrun':
+				$npc->reply('Thanks to you i have some runes now, and the customers are already coming.');
+				$npc->reply('However, i need Chain armory for the Arena and i have no time to smith it.');
+				$npc->reply(sprintf('Could you bring me %s ChainLegs, %s ChainBodies and %s ChainHelmets?', self::NEED_LEG, self::NEED_ARMOR, self::NEED_HELMET));
+				$npc->reply(sprintf('I can pay you %s for that job! Yes?', Shadowfunc::displayPrice(self::REWARD_NUYEN)));
+				break;
+			case 'yes':
+				$npc->reply(sprintf('Thank you so very much. Please hurry. The Arena frequently needs melee armory.'));
+				break;
+			case 'no':
+				$npc->reply('Anyway check out my offers!');
+				break;
+		}
+	}
+}
+?>
---
+++ module/Lamb/lamb_module/Shadowlamb/quest/Seattle_BD4.php
@@ -0,0 +1,65 @@
+<?php
+final class Quest_Seattle_BD4 extends SR_Quest
+{
+	const REWARD_XP = 8;
+	const REWARD_NUYEN = 2000;
+	
+	public function getQuestName() { return 'PoorSmithRevenge'; }
+	public function getNeededAmount() { return 10; }
+	public function getQuestDescription() { return sprintf('Bring %s/%s Tenugui to the Seattle Blacksmith.', $this->getAmount(), $this->getNeededAmount()); }
+	
+	public function checkQuest(SR_NPC $npc, SR_Player $player)
+	{
+		if ($this->isDone($player))
+		{
+			return false;
+		}
+		
+		$need = $this->getNeededAmount();
+		$have = $this->getAmount();
+		$have = $this->giveQuesties($player, $npc, 'Tenugui', $have, $need);
+		$this->saveAmount($have);
+		if ($have >= $need)
+		{
+			$ny = Shadowfunc::displayPrice(self::REWARD_NUYEN);
+			$xp = self::REWARD_XP;
+			$npc->reply('Haha, great! You are the best. This might have been a lesson for them.');
+			$npc->reply(sprintf('I am still a bit short on money. But you can have %s.', $ny));
+			$player->message(sprintf('The smith hands you %s. You also gain %s XP.', $ny, $xp));
+			$player->giveNuyen(self::REWARD_NUYEN);
+			$player->giveXP($xp);
+			$this->onSolve($player);
+			sleep(2);
+			$npc->reply('You know what ... i will teach you how to use lockpicking.');
+			$player->alterField('lockpicking', 1);
+			$player->message(sprintf('Your lockpicking skill has increased by 1.'));
+		}
+		else
+		{
+			$npc->reply(sprintf('Please bring me another %d Tenugui. I will reward you well.', $need-$have));
+		}
+		return true;
+	}
+	
+	public function onNPCQuestTalkB(SR_TalkingNPC $npc, SR_Player $player, $word)
+	{
+		switch ($word)
+		{
+			case 'confirm':
+				$npc->reply('I promise i will reward you well this time. Ok?');
+				break;
+			case 'shadowrun':
+				$npc->reply('Thanks to you i am back in business.');
+				$npc->reply('If you really want a run, go kill all those Ninjas who robbed me and get me '.$this->getNeededAmount().' Tenugui.');
+				$npc->reply('I promise i will reward you well this time. Ok?');
+				break;
+			case 'yes':
+				$npc->reply(sprintf('Yay! I await you back!'));
+				break;
+			case 'no':
+				$npc->reply('Anyway check out my offers!');
+				break;
+		}
+	}
+}
+?>
