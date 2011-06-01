4f6032917047960734dcb1dbc88ae05469fe6da1
Gizmore <gizmore@wechall.net>
2011-06-01 15:40:36 +0000

    fix for .seen plugin

--- module/Lamb/lamb_plugin/ppublic/seen.php
+++ module/Lamb/lamb_plugin/ppublic/seen.php
@@ -17,7 +17,7 @@ if (false === ($user = GDO::table('Lamb_User')->selectFirstObject('*', "lusr_nam
 $timestamp = $user->getVar('lusr_timestamp');
 $date = GWF_Time::getDate(GWF_Date::LEN_SECOND, $timestamp);
 $ago = time() - $timestamp;
-$ago = GWF_Time::humanDurationEN($ago);
+$ago = GWF_Time::humanDuration($ago);
 //$bot->reply(sprintf('I have seen %s %s ago in %s saying: "%s".', $username, GWF_Time::displayAge($user->getVar('lusr_last_date')), $user->getVar('lusr_last_channel'), $user->getVar('lusr_last_message')));
 $bot->reply(sprintf('I have seen %s at %s (%s ago) in %s saying: "%s".', $username, GWF_Time::displayDate($date), $ago, $user->getVar('lusr_last_channel'), $user->getVar('lusr_last_message')));
 ?>
