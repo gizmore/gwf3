2e3d5b78ba2a4316efb31b135c2592bdd8813600
Gizmore <gizmore@wechall.net>
2011-06-06 10:12:12 +0000

    Lamb now knows when it has connected to a server and is logged into IRCD.

--- module/Lamb/lamb_event/002.php
+++ module/Lamb/lamb_event/002.php
@@ -1,2 +1,5 @@
-<?php
-# RPL_YOURHOST
+<?php # %EVENT% RPL_YOURHOST. 
+# :irc.giz.org 002 Lamb3 :Your host is irc.giz.org, running version Unreal3.2.8.1
+$server instanceof Lamb_Server;
+$server->setOnline();
+?>
