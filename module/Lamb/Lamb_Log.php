edc7e54f4387cf37d17dc6a27c92f5dc9e6bc66d
Gizmore <gizmore@wechall.net>
2011-06-06 01:12:45 +0000

    Lamb_Log now has more separate files.

--- module/Lamb/Lamb_Log.php
+++ module/Lamb/Lamb_Log.php
@@ -1,11 +1,11 @@
 <?php
+/**
+ * Lamb log wrapper.
+ * @author gizmore
+ * @version 3.0
+ */
 final class Lamb_Log
 {
-	private static function getFilename()
-	{
-		return 'logs/'.date('Ymd').'_lamb.log';
-	}
-	
 	public static function log($message)
 	{
 		if (PHP_SAPI === 'cli')
@@ -13,14 +13,64 @@ final class Lamb_Log
 			echo $message.PHP_EOL;
 		}
 		GWF_Log::log('lamb', $message, true);
+		return true;
 	}
 	
+	public static function logChat(Lamb_Server $server, $message)
+	{
+		if (PHP_SAPI === 'cli')
+		{
+			echo $message.PHP_EOL;
+		}
+		
+		# One file per server
+		$host = preg_replace('/[^a-z0-9_\\.]/', '', strtolower($server->getHostname()));
+		
+		# Log it
+		GWF_Log::log("lamb_chat_{$host}", $message, true);
+		return true;
+	}
+	
+	public static function logError($message)
+	{
+		if (PHP_SAPI === 'cli')
+		{
+			echo 'ERROR: '.$message.PHP_EOL;
+		}
+		GWF_Log::log('lamb_error', $message, true);
+		GWF_Log::log('lamb_error_details', GWF_Debug::backtrace($message, false));
+		return false;
+	}
+	
+	public static function logDebug($message)
+	{
+		if (PHP_SAPI === 'cli')
+		{
+			echo $message.PHP_EOL;
+		}
+		GWF_Log::log('lamb_debug', $message, true);
+		return true;
+	}
+	
+	/**
+	 * Output info about errorneous ircd message.
+	 * @param Lamb_Server $server
+	 * @param unknown_type $command
+	 * @param unknown_type $from
+	 * @param array $args
+	 */
 	public static function debugCommand(Lamb_Server $server, $command, $from, array $args)
 	{
-		echo 'Lamb_Log::debugCommand'.PHP_EOL;
-		echo 'CMD: '.$command.PHP_EOL;
-		echo 'FROM: '.$from.PHP_EOL;
-		echo 'ARGS: '.implode(',', $args).PHP_EOL;
+		if (PHP_SAPI === 'cli')
+		{
+			echo '======================'.PHP_EOL;
+			echo 'Lamb_Log::debugCommand'.PHP_EOL;
+			echo 'CMD: '.$command.PHP_EOL;
+			echo 'FROM: '.$from.PHP_EOL;
+			echo 'ARGS: '.implode(',', $args).PHP_EOL;
+			echo '======================'.PHP_EOL;
+		}
+		return true;
 	}
 }
 ?>
