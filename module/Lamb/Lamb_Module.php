9eecd8cd028c5252da10365c951f462d932644bc
Gizmore <gizmore@wechall.net>
2011-06-06 11:23:22 +0000

    Code cleanup.

--- module/Lamb/Lamb_Module.php
+++ module/Lamb/Lamb_Module.php
@@ -13,6 +13,7 @@ abstract class Lamb_Module
 	private $name;
 	public function getName() { return $this->name; }
 	public function setName($name) { $this->name = $name; }
+	
 	############
 	### Init ###
 	############
@@ -28,24 +29,41 @@ abstract class Lamb_Module
 		return false;
 	}
 	
+	############
+	### Help ###
+	############
+	public function getHelpText($command)
+	{
+		$h = $this->getHelp();
+		if (!isset($h[$command]))
+		{
+			return '';
+		}
+		return str_replace('%CMD%', LAMB_TRIGGER.$command, $h[$command]);
+	}
+	
+	# --------------------------- #
 	# --- Override Below here --- #
+	# --------------------------- #
 	
 	###############
 	### Getters ###
 	###############
-	public abstract function getHelp();# { return array(); }
-	public function getTriggers($priviledge);# { return array(); }
+	public function getHelp() { return array(); }
+	public function getTriggers($priviledge) { return array(); }
 	################
 	### Triggers ###
 	################
 	public function onInstall() {} # Always called once before startup.
 	public function onInit() {} # Called once on startup.
 	public function onInitServer(Lamb_Server $server) {} # Called when a server goes online.
-	public function onNotice(Lamb_Server $server, Lamb_User $user, $from, $origin, $message) {}
-	public function onCTPC(Lamb_Server $server, Lamb_User $user, $from, $origin, $message) {}
+	public function onOnline() {} # Called once after all severs are online.
+	public function onInitTimers() {} # Called after onOnline and after a timer flush. 
+	public function onEvent(Lamb $bot, Lamb_Server $server, $event, $from, $args) {}
 	public function onJoin(Lamb_Server $server, Lamb_User $user, Lamb_Channel $channel) {}
+	public function onCTPC(Lamb_Server $server, Lamb_User $user, $from, $origin, $message) {}
+	public function onNotice(Lamb_Server $server, Lamb_User $user, $from, $origin, $message) {}
 	public function onPrivmsg(Lamb_Server $server, Lamb_User $user, $from, $origin, $message) {}
 	public function onTrigger(Lamb_Server $server, Lamb_User $user, $from, $origin, $command, $message) {}
-	public function onEvent(Lamb $bot, Lamb_Server $server, $event, $from, $args) {}
 }
 ?>
