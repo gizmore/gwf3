78664fdae734f1261a9d54c21e36b317ffa98685
Gizmore <gizmore@wechall.net>
2011-06-11 22:44:23 +0000

    Elevtaor found percentage is 100 by default. (thx noother)

--- module/Lamb/lamb_module/Shadowlamb/city/Renraku/location/Elevator.php
+++ module/Lamb/lamb_module/Shadowlamb/city/Renraku/location/Elevator.php
@@ -1,7 +1,7 @@
 <?php
 class Renraku_Elevator extends SR_Elevator
 {
-	public function getElevatorCity() { return $this->getCity(); }
+	public function getElevatorCity() { return 'Renraku'; }
 	public function getElevatorMaxKG() { return 2000; }
 	public function getElevatorTime() { return 60; }
 }
