dbf4afaf7334889e805361587db124205ad303f7
Gizmore <gizmore@wechall.net>
2011-06-13 16:53:31 +0000

    re-implemented class GWF_Result for displaying and returning messages.

---
+++ inc/util/GWF_Result.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Result message.
+ * @author Kender
+ */
+final class GWF_Result
+{
+	private $message;
+	private $is_error;
+	
+	public function __construct($message, $is_error)
+	{
+		$this->message = $message;
+		$this->is_error = $is_error;
+	}
+	
+	public function isError()
+	{
+		return $this->is_error === true;
+	}
+	
+	public function getMessage()
+	{
+		return $this->message;
+	}
+	
+	public function display($title='Error')
+	{
+		if ($this->is_error)
+		{
+			return GWF_HTML::error($title, $this->message);
+		}
+		else
+		{
+			return GWF_HTML::message($title, $this->message);
+		}
+	}
+}
+?>
