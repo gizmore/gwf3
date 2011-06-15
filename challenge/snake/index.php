ce271d893cb78d311a36201391780c44fb617f5f
Gizmore <gizmore@wechall.net>
2011-06-15 21:07:41 +0000

    Commited the snake challenge + fixed it (thx livinskull)

---
+++ challenge/snake/index.php
@@ -0,0 +1,19 @@
+<?php
+chdir('../../');
+define('GWF_PAGE_TITLE', 'Snake');
+define('CHEAT_SNAKE_SCORE', 300000);
+require_once('html_head.php');
+if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE))) {
+	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 4, 'challenge/snake/index.php', false);
+}
+$chall->showHeader();
+
+htmlTitleBox($chall->lang('title'), $chall->lang('info', array(CHEAT_SNAKE_SCORE, 'CGI_Highscore.php', 'http://snake.gizmore.org', 'http://snake.gizmore.org/CGI_Highscore.php')));
+
+echo '<div class="box box_c">'.PHP_EOL;
+echo '<applet code="SnakeApplet.class" archive="snake.jar?v=1.06" width="500" height="400"><param name="sessid" value="'.GWF_HTML::display(GWF_Session::getSessID()).'" ></param></applet>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+
+echo $chall->copyrightFooter();
+require_once('html_foot.php');
+?>
---
+++ challenge/snake/install.php
@@ -0,0 +1,21 @@
+<?php
+chdir("../../");
+require_once("html_head.php");
+if (!GWF_User::isAdminS()) {
+	echo GWF_HTML::err('ERR_NO_PERMISSION');
+	return;
+}
+$title = "Snake";
+$solution = false;
+$score = 7;
+$url = "challenge/snake/index.php";
+$creators = "Gizmore";
+$tags = 'Java,PHP,Fun';
+
+$_GET['installing'] = true;
+require_once 'CGI_Highscore.php';
+GDO::table('Snake_row')->createTable(true);
+
+WC_Challenge::installChallenge($title, $solution, $score, $url, $creators, $tags, true);
+require_once("html_foot.php");
+?>
