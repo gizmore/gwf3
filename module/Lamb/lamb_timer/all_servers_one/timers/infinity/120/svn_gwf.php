22154ae4b1f8f480adc750a5d5f5a4c7afa656ec
Gizmore <gizmore@wechall.net>
2011-06-17 21:08:04 +0000

    SVN fetch timer fixes. (thx livinskull)

--- module/Lamb/lamb_timer/all_servers_one/timers/infinity/120/svn_gwf.php
+++ module/Lamb/lamb_timer/all_servers_one/timers/infinity/120/svn_gwf.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "HERE\n";
 require_once LAMB::DIR.'Lamb_SVN_Info.php';
 
 lamb_svn_info('GWF3', 'https://80.246.62.155/GWF3/', '', '', 'http://trac.gwf3.gizmore.org/changeset/%REV%', array('#shadowlamb', '#wechall'));
