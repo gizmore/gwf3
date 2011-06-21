09bf0b9785de237f71615df4c2552dc8a8c7cb46
livinskull <livinskull@wechall.net>
2011-06-21 20:46:36 +0000

    some minor german lingo fixes

--- challenge/bsdhell/thelasthope/lang/chall_de.php
+++ challenge/bsdhell/thelasthope/lang/chall_de.php
@@ -2,9 +2,9 @@
 $lang = array(
 	'title' => 'Die letzte Hoffnung',
 	'info' => 
-		'Wir sind auf der Spur von Terroristen, die ein VPN für kriminellen Aktivitäten nutzen.<br/>'.PHP_EOL.
+		'Wir sind auf der Spur von Terroristen, die ein VPN für kriminelle Aktivitäten nutzen.<br/>'.PHP_EOL.
 		'Leider sind alle ihre VPN-Server verschlüsselt und mitgeloggt wird natürlich auch nichts.<br/>'.PHP_EOL.
-		'Wir haben beschlossen, den Computer, von einem Ihrer Admins zu beschlagnahmen, aber leider gibt auch dieser nicht viele Informationen preis.<br/>'.PHP_EOL.
+		'Wir haben beschlossen, den Computer von einem Ihrer Admins zu beschlagnahmen, aber leider gibt auch dieser nicht viele Informationen preis.<br/>'.PHP_EOL.
 		'Das einzige, was interessant aussieht, ist <a href="%1%">eine ausführbare Datei</a> auf einer der Boxen, welche ein Passwort enthält.<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
 		'Unsere letzte Hoffnung ist, dass dieses Passwort auch für das VPN verwendet wird :('.PHP_EOL,
