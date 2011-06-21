3191f4af8022c3b031eb3954dac0f32c5772f003
livinskull <livinskull@wechall.net>
2011-06-21 20:15:59 +0000

    preg_evasion goes german ;)

---
+++ challenge/noother/preg_evasion/lang/chall_de.php
@@ -0,0 +1,19 @@
+<?php
+$lang = array(
+	'title' => 'Preg Evasion',
+	'info' =>
+		'Deine Aufgabe ist es die preg_match() Funktion auszutricksen.<br/>'.
+		'Wie immer kannst du den <a href="%1%">Quelltext</a> anschauen, auch mit <a href="%2%">Syntaxhighlighting</a>.<br/>'.
+		'<br/>'.
+		'Viel Glück!',
+
+	'evil' => 'Bösartige Eingabe erkannt: %1%.',
+	'lovely' => 'Diese Eingabe sieht gut aus!',
+
+	'ft_the_form' => 'Ihr Formular',
+	'btn_hackit' => 'Ihr Knopf',
+	'th_text' => 'Ihr Text',
+
+	'noote' => 'Bitte senden Sie uns etwas Text für unsere persönliche Datensammlung über Sie.',
+);
+?>
