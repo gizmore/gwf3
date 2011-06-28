429b5b76b0cdf6d237a0394ac503566bad618cfb
Gizmore <gizmore@wechall.net>
2011-06-28 16:56:30 +0000

    Poorly implementation of CategorySelect. TODO: nice select output for subcategories.

--- module/Category/GWF_CategorySelect.php
+++ module/Category/GWF_CategorySelect.php
@@ -3,11 +3,32 @@ final class GWF_CategorySelect
 {
 	public static function single($name, $selected, $parent_id=0)
 	{
+		if (false === ($mod_cat = GWF_Module::loadModuleDB('Category', true, true)))
+		{
+			return GWF_HTML::err('ERR_MODULE_MISSING', array('Category'));
+		}
 		
+		$langid = GWF_Language::getCurrentID();
 		$data = array();
-		$data[] = array('0',GWF_HTML::lang('select_category'));
-		
+		$data[] = array('0', $mod_cat->lang('th_sel'));
+		if (false !== ($cats = GWF_Category::getAllCategoriesCached()))
+		{
+			foreach ($cats as $cat)
+			{
+				$cat instanceof GWF_Category;
+				$data[] = array($cat->getID(), $cat->getTranslation($langid));
+			}
+		}
 		return GWF_Select::display($name, $data, $selected);
+	}
+	
+	public static function validateCat($arg, $allowZero=true)
+	{
+		if ( ( ($arg == 0) && ($allowZero) ) || (GWF_Category::categoryExists($arg)) )
+		{
+			return false;
+		}
+		return GWF_Module::loadModuleDB('Cateogry', false, true)->lang('err_cat');
 	}
 }
 ?>
--- module/Category/Module_Category.php
+++ module/Category/Module_Category.php
@@ -8,7 +8,7 @@ final class Module_Category extends GWF_Module
 	public function getVersion() { return 1.01; }
 	public function onLoadLanguage() { return $this->loadLanguage('lang/category'); }
 	public function getAdminSectionURL() { return $this->getMethodURL('Admin'); }
-	public function getClasses() { return array('GWF_Category', 'GWF_CategoryTranslation'); }
-	public function getDefaultPriority() { return 40; } # Has Deps
+	public function getClasses() { return array('GWF_Category', 'GWF_CategorySelect', 'GWF_CategoryTranslation'); }
+	public function getDefaultPriority() { return 30; } # Has Deps
 }
 ?>
