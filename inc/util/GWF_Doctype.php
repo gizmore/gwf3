4f64816cec929d50db84631faf90c9743e5fd844
spaceone <space@wechall.net>
2011-06-28 18:28:24 +0000

    GWF_Doctype: from method to constant

--- inc/util/GWF_Doctype.php
+++ inc/util/GWF_Doctype.php
@@ -1,6 +1,12 @@
 <?php
 final class GWF_Doctype
 {
+	const HTML4 = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n\"http://www.w3.org/TR/html4/loose.dtd\">\n";
+	const HTML4STRICT = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"\n\"http://www.w3.org/TR/html4/strict.dtd\">\n";
+	const HTML5 = "<!DOCTYPE html>\n";
+	const XHTML = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n";
+	const XHTMLSTRICT = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n";
+
 	private static $_Doctype = NULL;
 	public static function getDoctype() { 
 		if(self::$_Doctype === NULL) {
@@ -11,46 +17,14 @@ final class GWF_Doctype
 	public static function setDoctype($doctype) {
 	
 		switch($doctype) {
-			case 'html4' : self::$_Doctype = self::html4(); break;
-			case 'html4strict' : self::$_Doctype = self::html4strict(); break;
-			case 'html5' : self::$_Doctype = self::html5(); break;
-			case 'xhtml' : self::$_Doctype = self::xhtml(); break;
-			case 'xhtmlstrict' : self::$_Doctype = self::xhtmlstrict(); break;
+			case 'html4' : self::$_Doctype = self::HTML4; break;
+			case 'html4strict' : self::$_Doctype = self::HTML4STRICT; break;
+			case 'html5' : self::$_Doctype = self::HTML5; break;
+			case 'xhtml' : self::$_Doctype = self::XHTML; break;
+			case 'xhtmlstrict' : self::$_Doctype = self::XHTMLSTRICT; break;
 		}
 		return true;
 	}
-	
-	public static function html4()
-	{
-		return 
-			'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"'.PHP_EOL.
-			'"http://www.w3.org/TR/html4/loose.dtd">'.PHP_EOL;
-	}
-
-	public static function html4strict()
-	{
-		return
-			'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"'.PHP_EOL.
-			'"http://www.w3.org/TR/html4/strict.dtd">'.PHP_EOL;
-	}
-
-	public static function xhtml()
-	{
-		return
-			'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"'.PHP_EOL.
-			'"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'.PHP_EOL;
-	}
-
-	public static function xhtmlstrict()
-	{
-		return
-			'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"'.PHP_EOL.
-			'"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'.PHP_EOL;
-	}
-
-	public static function html5()
-	{
-		return '<!DOCTYPE html>'.PHP_EOL;
-	}
 }
+
 ?>
