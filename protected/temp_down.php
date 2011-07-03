9a0a2355d3be6eb7957317c5bf0e5ea976386f41
spaceone <space@wechall.net>
2011-07-03 22:38:11 +0000

    better temp_down.php now

--- protected/temp_down.php
+++ protected/temp_down.php
@@ -1,9 +1,10 @@
 <?php
-#$workerIP = '192.168.0.4'; # <-- Set Down here
-if (isset($workerIP))
+#define GWF_WORKER_IP in your config!
+
+if (defined('GWF_WORKER_IP'))
 {
 	$ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
-	if ($workerIP !== $ip)
+	if (GWF_WORKER_IP !== $ip)
 	{
 		$msg = 'Converting the database atm. should be back within 45 minutes.';
 		# Use one of these:
