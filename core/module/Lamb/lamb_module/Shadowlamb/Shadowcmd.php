e70df88789d67c2c51d32ce5dd5e5ce76c4e7269
Gizmore <gizmore@wechall.net>
2011-07-19 11:26:44 +0000

    Shadowlamb: #bye is a leader command now. (thx Geo)

--- core/module/Lamb/lamb_module/Shadowlamb/Shadowcmd.php
+++ core/module/Lamb/lamb_module/Shadowlamb/Shadowcmd.php
@@ -76,7 +76,7 @@ class Shadowcmd
 	public static $CMDS = array(
 		'delete' => array(),
 		'sleep' => array(),
-		'talk' => array('u','r','eq','uq','j','part','gi','drop','ca','say','bye'),
+		'talk' => array('u','r','eq','uq','j','part','gi','drop','ca','say'),
 		'fight' => array('fl','eq','uq','gi','fw','bw','u','ca','#'),
 		'inside' => array('j','part','u','ca','eq','uq','gi','drop','look','info'),
 		'outside' => array('j','part','u','ca','eq','uq','gi','drop','look','info'),
@@ -90,7 +90,7 @@ class Shadowcmd
 	public static $CMDS_LEADER = array(
 		'delete' => array(),
 		'sleep' => array('stop'),
-		'talk' => array('kick','fight'),
+		'talk' => array('kick','fight','bye'),
 		'fight' => array(),
 		'inside' => array('g','exp','hunt','kick','exit'),
 		'outside' => array('g','exp','hunt','kick','en'),
