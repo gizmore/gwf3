20fbeb633d4c998686f1f393c5a90f3d9a1638f3
Gizmore <gizmore@wechall.net>
2011-07-23 05:51:48 +0000

    New site: Enigmagroup joined. (thx EG)

---
+++ core/module/WeChall/sites/WCSite_EG.php
@@ -0,0 +1,42 @@
+<?php
+# <username>:<rank>:<points>:<max-points>:<num-of-solved-tasks>:<num-of-tasks>:<num-of-players>
+class WCSite_EG extends WC_Site
+{
+	public function parseStats($url)
+	{
+		if (false === ($result = GWF_HTTP::getFromURL($url, false))) {
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+		if (false === ($result2 = Common::substrFrom($result, '<br /><br />', false)))
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+
+		$stats = explode(':', $result2);
+		if (count($stats) !== 7) {
+			if ($result2 === '0') {
+				return array(0, 0);
+			}
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+	 	# username:rank:score:maxscore:challssolved:challcount:usercount
+		$uname = $stats[0];
+		$rank = intval($stats[1]);
+		$onsitescore = intval($stats[2]);
+		$maxscore = intval($stats[3]);
+		$challssolved = intval($stats[4]);
+		$challcount = intval($stats[5]);
+		$usercount = intval($stats[6]);
+		
+		if ($maxscore === 0 || $challcount === 0 || $usercount === 0) {
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+		$this->updateSite($maxscore, $usercount, $challcount);
+		
+		return array($onsitescore, $rank, $challssolved);
+	}
+}
+?>
