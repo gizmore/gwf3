08b9384da515c076c7357b7777607137f2e05ad9
Gizmore <gizmore@wechall.net>
2011-07-23 14:33:37 +0000

    Shadowlamb: Moped now extends RacingBike. (thx ynori7)

--- core/module/Lamb/lamb_module/Shadowlamb/item/mount/Moped.php
+++ core/module/Lamb/lamb_module/Shadowlamb/item/mount/Moped.php
@@ -1,5 +1,6 @@
 <?php
-final class Item_Moped extends SR_Mount
+require_once 'RacingBike.php';
+final class Item_Moped extends Item_RacingBike
 {
 	public function getItemDescription() { return 'An old 125ccm moped.'; }
 	public function getItemPrice() { return 4500; }
@@ -7,6 +8,6 @@ final class Item_Moped extends SR_Mount
 	public function getMountWeight() { return 4000; }
 	public function getMountPassengers() { return 2; }
 	public function getMountLockLevel() { return 1; }
-	public function getMountTime($eta) { return $eta * 0.7; }
+	public function getMountTime($eta) { return parent::getMountTime($eta) * 0.88; }
 }
 ?>
