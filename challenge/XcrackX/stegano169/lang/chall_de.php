b7fe5067e94f3af8dbdcd0942761c4fca94c7428
Gizmore <gizmore@wechall.net>
2011-07-23 17:51:51 +0000

    WC5: New Challenge by XcrackX (thx XcrackX)

---
+++ challenge/XcrackX/stegano169/lang/chall_de.php
@@ -0,0 +1,13 @@
+<?php
+$lang = array(
+	'title' => 'Stegano 169',
+	'info' => 
+		'Ein Paranoider Freund hat mir folgende Nachricht übermittelt,'.PHP_EOL.
+		'aber leider hat er in seiner Paranoia vergessen mir den Lösungsweg mitzuteilen.<br/>'.PHP_EOL.
+		'%1%<br/>'.PHP_EOL.
+		'Was mache ich nun?<br/>',
+	'hint' => 'Wenn Picasso einen Computer gehabt hätte wüsste er was zu tun ist!',
+	'embed' => 'Das hier ist leider nicht die Lösung die du suchst :( Bitte weiter suchen!',
+	'title_ct' => 'Hier ist die Nachricht',
+);
+?>
---
+++ challenge/XcrackX/stegano169/lang/chall_en.php
@@ -0,0 +1,13 @@
+<?php
+$lang = array(
+	'title' => 'Stegano 169',
+	'info' => 
+		'A paranoid friend has sent me message,'.PHP_EOL.
+		'but in his paranoid mind he forgot to tell me how to decode it.<br/>'.PHP_EOL.
+		'%1%<br/>'.PHP_EOL.
+		'Was mache ich nun?<br/>',
+	'hint' => 'If Picasso got a computer he would know what to do!',
+	'embed' => 'Sadly, this is not the message you are looking for :( Try further!',
+	'title_ct' => 'Here is the message',
+);
+?>
