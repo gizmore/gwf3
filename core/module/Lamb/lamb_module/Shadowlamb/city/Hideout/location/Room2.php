4320a51f5d2e735dbe33c113696ff0ece8067a8c
Gizmore <gizmore@wechall.net>
2011-07-23 07:10:30 +0000

    Shadowlamb: Hideout Room2 cannot be abused anymore to farm excessively. (thx CPUKiller)

--- core/module/Lamb/lamb_module/Shadowlamb/city/Hideout/location/Room2.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Hideout/location/Room2.php
@@ -3,7 +3,7 @@ final class Hideout_Room2 extends SR_Location
 {
 	public function getFoundPercentage() { return 100; }
 	public function getFoundText(SR_Player $player) { return 'You found another room. It seems to be quiet in there.'; }
-	public function getEnterText(SR_Player $player) { return 'You see two Lamers sleeping.'; }
+	public function getEnterText(SR_Player $player) { return 'You see three Lamers sleeping.'; }
 	public function getLeaderCommands(SR_Player $player) { return array('wakeup'); }
 	public function onEnter(SR_Player $player)
 	{
@@ -12,9 +12,23 @@ final class Hideout_Room2 extends SR_Location
 	
 	public function on_wakeup(SR_Player $player, array $args)
 	{
-		$party = $player->getParty();
-		$party->notice('You wake the two lamers kindly... as they realize what`s happening they attack.');
-		SR_NPC::createEnemyParty('Redmond_Lamer','Redmond_Lamer')->fight($party, true);
+		if (!$player->hasTemp('HIDE_R2_ONCE'))
+		{
+			$player->setTemp('HIDE_R2_ONCE', 1);
+			$party = $player->getParty();
+			$party->notice('You wake the three lamers kindly... as they realize what`s happening they attack.');
+			SR_NPC::createEnemyParty('Redmond_Lamer','Redmond_Lamer','Redmond_Lamer')->fight($party, true);
+		}
+		else
+		{
+			$player->message("There are no lamers to wake.");
+		}
+	}
+	
+	public function onCityEnter(SR_Party $party)
+	{
+		$party->getLeader()->unsetTemp('HIDE_R2_ONCE');
+		return parent::onCityEnter($party);
 	}
 }
 ?>
