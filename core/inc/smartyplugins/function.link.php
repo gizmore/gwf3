0f355ff6caa8bd4755ba90949898bbfa35fdd7cc
spaceone <space@wechall.net>
2011-07-24 23:14:32 +0000

    [+] smartyplugin: function link [+] menu_page.tpl

---
+++ core/inc/smartyplugins/function.link.php
@@ -0,0 +1,14 @@
+<?php
+function smarty_function_link($params, $template)
+{
+	$class = empty($params['class']) ? '' : ' class="'.$params['class'].'"';
+	$id = empty($params['id']) ? '' : ' id="'.$params['id'].'"';
+	$onclick = empty($params['onclick']) ? '' : ' onclick="'.$params['onclick'].'"';
+	$title = empty($params['title']) ? '' : ' title="'.$params['title'].'"';
+	$text = empty($params['text']) ? '' : $params['text'];
+	$pre = empty($params['pre']) ? '' : $params['pre'];
+	$post = empty($params['post']) ? '' : $params['post'];
+	$url = empty($params['url']) ? '#' : htmlspecialchars($params['url']);
+	
+	return sprintf('%s<a href="%s"%s%s%s%s>%s</a>%s'.PHP_EOL, $pre, $url, $title, $class, $id, $onclick, $text, $post);
+}
