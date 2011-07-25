803b542d8858587396a7692d9071ac4e8fd70fa4
spaceone <space@wechall.net>
2011-07-25 10:51:58 +0000

    added meta 'robots' to PageBuilder->Show

--- core/module/PageBuilder/method/Show.php
+++ core/module/PageBuilder/method/Show.php
@@ -113,6 +113,9 @@ final class PageBuilder_Show extends GWF_Method
 	{
 		$page->increase('page_views', 1);
 		
+		$robot = ($page->isOptionEnabled(GWF_PAGE::INDEX)) ? 'index,' : 'noindex,';
+		$robot .= ($page->isOptionEnabled(GWF_PAGE::FOLLOW)) ? 'follow' : 'nofollow';
+		GWF_Website::addMeta(array('robot', $robot, false), true);
 		GWF_Website::setMetaDescr($page->getVar('page_meta_desc'));
 		GWF_Website::setMetaTags($page->getVar('page_meta_tags'));
 		GWF_Website::setPageTitle($page->getVar('page_title'));
