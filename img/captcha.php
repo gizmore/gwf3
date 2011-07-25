77032ae836de042368f704b619cec95868add268
spaceone <space@wechall.net>
2011-07-25 12:16:16 +0000

    i'm sorry, but again baselayout

--- img/captcha.php
+++ img/captcha.php
@@ -5,7 +5,7 @@ GWF3::onLoadConfig(GWF_CONFIG_PATH);
 GWF_Session::start(false);
 require('core/inc3p/Class_Captcha.php');
 GWF_HTTP::noCache();
-$aFonts = array('font/teen.ttf');
+$aFonts = array('extra/font/teen.ttf');
 $rgbcolor = GWF_CAPTCHA_COLOR_BG;
 $oVisualCaptcha = new PhpCaptcha($aFonts, 210, 42, $rgbcolor);
 $oVisualCaptcha->Create('', Common::getGet('chars', true));
