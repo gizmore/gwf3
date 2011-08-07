853a5b224eeed632af92b50d530b7cbc9114b3f0
Gizmore <gizmore@wechall.net>
2011-08-07 18:53:22 +0000

    Shadowlamb: The whisper command is now implemented.

--- core/module/Lamb/lamb_module/Shadowlamb/cmd/chat/whisper.php
+++ core/module/Lamb/lamb_module/Shadowlamb/cmd/chat/whisper.php
@@ -3,7 +3,28 @@ final class Shadowcmd_whisper extends Shadowcmd
 {
 	public static function execute(SR_Player $player, array $args)
 	{
+		$c = count($args);
+		if ($c < 2)
+		{
+			$player->message(Shadowhelp::getHelp($player, 'whisper'));
+			return false;
+		}
 		
+		$name = array_shift($args);
+		$message = implode(' ', $args);
+		
+		$result = Shadowrun4::getPlayerByShortName($name);
+		switch ($result)
+		{
+			case false: return $player->message('The player is unknown or not in memory.');
+			case -1: return $player->message('The player name is ambigous. Try the {server} version.');
+			default: return self::onWhisper($player, $result, $message);
+		}
+	}
+	
+	public static function onWhisper(SR_Player $from, SR_Player $to, $message)
+	{
+		return $to->message(sprintf('%s whisper: "%s"', $from->getName(), $message));
 	}
 }
 ?>
