504289a4f9b840facf39983221272b07bc4228d3
Gizmore <gizmore@wechall.net>
2011-08-21 03:43:15 +0000

    Shadowlamb: Patched heal and calm spell.

--- core/module/Lamb/lamb_module/Shadowlamb/spell/heal.php
+++ core/module/Lamb/lamb_module/Shadowlamb/spell/heal.php
@@ -8,12 +8,21 @@ final class Spell_heal extends SR_HealSpell
 	public function getManaCost(SR_Player $player) { return 6 + $this->getLevel($player); }
 	public function cast(SR_Player $player, SR_Player $target, $level, $hits)
 	{
-		$hits = ($hits+$wisdom) / 10;
+		$min = $level + 1;
+		$max = $min + ($level/4) + ($hits/10);
+		$hp_gain = Shadowfunc::diceFloat($min, $max);
+
+		
+//		$hits = ($hits+$wisdom) / 10;
+		
+		
 //		$hits = round($hits/4, 1) + $player->getBase('wisdom');
-		$min = $level + $hits;
-		$max = ($level + $hits) * intval($level / 2) + rand(0, $player->get('wisdom'));
-		$hp_gain = rand($min*10, $max*10);
-		$hp_gain /= 10;
+//		$min = $level + $hits;
+//		$max = ($level + $hits) * intval($level / 2) + rand(0, $player->get('wisdom'));
+//		$hp_gain = rand($min*10, $max*10);
+//		$hp_gain /= 10;
+
+		
 		$target->healHP($hp_gain);
 		$hp = $target->getHP();
 		$max_hp = $target->getMaxHP();
