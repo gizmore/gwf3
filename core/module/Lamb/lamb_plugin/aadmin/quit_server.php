3ffe63a6ad82738b4a124961b4e6d9372ea5c129
Gizmore <gizmore@wechall.net>
2011-08-30 04:01:46 +0000

    Lamb: trying to implement .quit_server

--- core/module/Lamb/lamb_plugin/aadmin/quit_server.php
+++ core/module/Lamb/lamb_plugin/aadmin/quit_server.php
@@ -5,41 +5,33 @@ if ($message === '') {
 	return $bot->processMessageA($server, LAMB_TRIGGER.'help quit_server', $from);
 }
 
-//global $LAMB_JOIN_SERVER_CONFIRM;
+if (is_numeric($message))
+{
+	$_server = $bot->getServer($message);
+}
+else
+{
+	$_server = false;
+	foreach ($bot->getServers() as $srv)
+	{
+		$srv instanceof Lamb_Server;
+		if (stripos($srv->getHostname(), $message) !== false)
+		{
+			$_server = $srv;
+			break;
+		}
+	}
+}
 
-//if (is_numeric($message))
-//{
-//	return $bot->processMessageA($server, LAMB_TRIGGER.'help join_server', $from);
-////	if (false === ($server = Lamb_Server::getByID())) {
-////		return $bot->reply('Unknown Server');
-////	}
-//}
-//else
-//{
-//	$args = explode(' ', $message);
-//	$hostname = $args[0];
-//	$nickname = isset($args[1]) ? $args[1] : 'Lamb';
-//	$password = isset($args[2]) ? $args[2] : '';
-//	$channels = isset($args[3]) ? $args[3] : '';
-//	$admins = isset($args[4]) ? $args[4] : LAMB_OWNER;
-//	
-//	$host = Common::substrFrom($hostname, '://', $hostname);
-//	$host = Common::substrUntil($host, ':');
-//	if ($host === ($ip = gethostbyname($host)))
-//	{
-//		$bot->reply('Can not resolve hostname: '.$host);
-//		return;
-//	}
-//	else
-//	{
-//		$message = 'Resolved IP: '.$ip.'. ';
-//	}
-//	
-//	$server = new Lamb_Server($hostname, $nickname, $password, $channels, $admins);
-//}
-//
-//if (false === $bot->getServer($server->getID())) {
-//	$bot->reply($message.'Unknown Server!');
-//}
-//$bot->removeServer($server);
+if ($_server === false)
+{
+	return $bot->reply('This server is unkown or not connected.');
+}
+
+$bot->reply('Issueing a quit.');
+
+$_server->disconnect('Quit server has been issued by '.$user->getName().'.');
+$bot->removeServer($_server);
+$_server = Lamb_Server::getByID($_server->getID());
+$bot->addServer($_server);
 ?>
