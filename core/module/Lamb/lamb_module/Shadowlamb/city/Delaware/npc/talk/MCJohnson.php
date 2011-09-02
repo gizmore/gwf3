e4bf8a6c9d8c5d4f2959a7e4830183706a1a7397
Gizmore <gizmore@wechall.net>
2011-09-02 18:36:44 +0000

    Shadowlamb: New quest in the Delaware MCLarens.

--- core/module/Lamb/lamb_module/Shadowlamb/city/Delaware/npc/talk/MCJohnson.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Delaware/npc/talk/MCJohnson.php
@@ -2,7 +2,7 @@
 final class Delaware_MCJohnson extends SR_TalkingNPC
 {
 	public function getName() { return 'Mr.Johnson'; }
-	public function getNPCQuests(SR_Player $player) { return array('Delaware_MCJohnson1','Delaware_MCJohnson2'); }
+	public function getNPCQuests(SR_Player $player) { return array('Delaware_MCJohnson1','Delaware_MCJohnson2','Delaware_MCJohnson3'); }
 	public function onNPCTalk(SR_Player $player, $word, array $args)
 	{
 		if ($this->onNPCQuestTalk($player, $word, $args))
---
+++ core/module/Lamb/lamb_module/Shadowlamb/quest/Delaware/Delaware_MCJohnson3.php
@@ -0,0 +1,50 @@
+<?php
+final class Quest_Delaware_MCJohnson3 extends SR_Quest
+{
+	public function getQuestName() { return 'Cops'; }
+	public function getQuestDescription() { return sprintf("Bring %d CopCaps to Mr.Johnson in the Mac Larens pub in Delaware.", $this->getNeededAmount()); }
+	public function getNeededAmount() { return 4; }
+	public function getRewardXP() { return 6; }
+	public function getRewardNuyen() { return 2500; }
+	
+	public function checkQuest(SR_NPC $npc, SR_Player $player)
+	{
+		$have = $this->giveQuesties($player, $npc, 'CopCap', $this->getAmount(), $this->getNeededAmount());
+		$this->saveAmount($have);
+		
+		if ($have >= $this->getNeededAmount())
+		{
+			$npc->reply("You did a great job again. Thank you. Very well done.");
+			return $this->onSolve($player);
+		}
+		else
+		{
+			return $npc->reply(sprintf('We still need %d more CopCaps.', $this->getNeededAmount()-$have));
+		}
+	}
+	
+	public function onNPCQuestTalkB(SR_TalkingNPC $npc, SR_Player $player, $word, array $args=NULL)
+	{
+		$need = $this->getNeededAmount();
+		$dp = $this->displayRewardNuyen();
+		switch ($word)
+		{
+			case 'shadowrun':
+				$npc->reply("I got some difficult job for you now.");
+				$npc->reply("All you need to know is that we need {$need} CopCaps for a client.");
+				$npc->reply("Your payment would be $dp. Accepted?");
+				break;
+			case 'confirm':
+				$npc->reply("I will pay you $dp.");
+				break;
+			case 'yes':
+				$npc->reply("Great, i will await you back soon.");
+				break;
+			case 'no':
+				$npc->reply('Ok.');
+				break;
+		}
+		return true;
+	}
+}
+?>
