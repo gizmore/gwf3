23307aafc1628812c3334f0cb40b4ed9d22c2737
Gizmore <gizmore@wechall.net>
2011-09-30 17:06:24 +0000

    GWF3: New jquery plugin: ghostwriter.

--- www/js/jq/color.js
+++ www/js/jq/color.js
@@ -1,33 +1,38 @@
+/**
+ * Color interpolation.
+ * @author gizmore
+ * TODO: Optimize for speed.
+ */
 function Color()
 {
-	this.dec2hex = function(n)
-	{
-//	    n = parseInt(n);
-	    var c = 'ABCDEF', b = n / 16, r = n % 16;
-	    b = b-(r/16); b = ((b>=0) && (b<=9)) ? b : c.charAt(b-10);    
-	    return ((r>=0) && (r<=9)) ? b+''+r : b+''+c.charAt(r-10);
-	}
-	
-	this.Interpolate = function(hex1, hex2, fraction)
-	{
-		var c1 = new Array(
-			hex2dec(hex1.substr(1, 2)),
-			hex2dec(hex1.substr(3, 2)),
-			hex2dec(hex1.substr(5, 2))
-		);
-		var c2 = new Array(
-			hex2dec(hex2.substr(1, 2)),
-			hex2dec(hex2.substr(3, 2)),
-			hex2dec(hex2.substr(5, 2))
-		);
-		var c3 = new Array(
-			_interpolate(c1[0], c2[0], fraction),
-			_interpolate(c1[1], c2[1], fraction),
-			_interpolate(c1[2], c2[2], fraction)
-		);
-		
-		return '#'+c1[0].toString(16)
-		
-		return hex1;
-	};
-}
+};
+Color.dec2hex = function(n)
+{
+	n = parseInt(n);
+    var c = 'ABCDEF', b = n / 16, r = n % 16;
+    b = b-(r/16); b = ((b>=0) && (b<=9)) ? b : c.charAt(b-10);    
+    return ((r>=0) && (r<=9)) ? b+''+r : b+''+c.charAt(r-10);
+};
+Color.interpolate = function(hex1, hex2, fraction)
+{
+	var c1 = new Array(
+		parseInt(hex1.substr(1, 2), 16),
+		parseInt(hex1.substr(3, 2), 16),
+		parseInt(hex1.substr(5, 2), 16)
+	);
+	var c2 = new Array(
+		parseInt(hex2.substr(1, 2), 16),
+		parseInt(hex2.substr(3, 2), 16),
+		parseInt(hex2.substr(5, 2), 16)
+	);
+	var c3 = new Array(
+		this._interpolate(c1[0], c2[0], fraction),
+		this._interpolate(c1[1], c2[1], fraction),
+		this._interpolate(c1[2], c2[2], fraction)
+	);
+	return '#'+this.dec2hex(c3[0])+this.dec2hex(c3[1])+this.dec2hex(c3[2]);
+};
+Color._interpolate = function(c1, c2, fraction)
+{
+	return ((c2-c1)*fraction)+c1;
+};
--- www/js/jq/ghostwriter.js
+++ www/js/jq/ghostwriter.js
@@ -1,105 +1,248 @@
+/**
+ * Ghostwriter smarty plugin.
+ * @author gizmore
+ * @version 0.1
+ */
 (function($)
 {
 	var methods = {
+			
 		init: function(options)
 		{
 			// Copy settings
 			var settings = {
-				'color_begin': '#ffffff',
-				'color_end': '#fea965',
-				'ms': 150,
-				'speed': 1,
-				'length': 5,
-				'start_delay': 50,
+				// Animation
+				'size_left': 16,
+				'size_right': 16,
+				'bold_left': 400,
+				'bold_right': 400,
+				'opaq_left': 1.0,
+				'opaq_right': 1.0,
+				'color_left': '#000000',
+				'color_right': '#000000',
+				'style_processed': '',
+				'style_unprocessed': 'zoom: 1; filter: alpha(opacity=5); opacity: 0.05;',
+				'callback': function(){},
+				// Speed
+				'group': 2,      // group*ngroups chars at once.
+				'ngroups': 12,   // group*ngroups chars at once.
+				'delay': 1200,   // delay start in millis.
+				'interval': 110, // timer interval in millis.
+				'running': 1,
+				'paused': false
 			};
 			return this.each(function(){
 				$(this).data('ghostwriter', $.extend(settings, options));
 			});
 		},
-
+		
 		type: function(options)
 		{
 			return this.each(function(){
-				$this = $(this);
-				$this.data('ghostwriter', $.extend($this.data('ghostwriter'), options));
-				_renderInit($this);
-				setTimeout(function(){_render($this);}, $this.data('ghostwriter')['start_delay']);
+				var $this = $(this);
+				var data = $.extend($this.data('ghostwriter'), options);
+				$this.data('ghostwriter', data);
+				if (_renderInit($this))
+				{
+					setTimeout(function(){ $this.show(); _render($this); }, data['delay']);
+				}
+				else
+				{
+					$.error('ghostwriter._renderInit failed.');
+				}
+			});
+		},
+		
+		pause: function(options)
+		{
+			return this.each(function(){
+				var $this = $(this);
+				var data = $.extend($this.data('ghostwriter'), options, {'running': false});
+				$this.data('ghostwriter', data);
+			});
+		},
+
+		resume: function(options)
+		{
+			return this.each(function(){
+				var $this = $(this);
+				var data = $.extend($this.data('ghostwriter'), options, {'running': true});
+				$this.data('ghostwriter', data);
 			});
 		}
 	};
 	
 	_renderInit = function(p)
 	{
-		var struct = {
-				'text': p.text(),
-				'pos': 0
-		};
-		$this.data('ghostwriter', $.extend($this.data('ghostwriter'), struct));
-	},
+		p.hide();
+		p.data('ghostwriter', $.extend(p.data('ghostwriter'), {
+			'text': p.html(), // Original text.
+			'pos': 0, // Current pos.
+			'len': 0  // Current len.
+		}));
+		return _checkSettings(p);
+	};
+	
+	_checkSettings = function(p)
+	{
+//		var data = p.data('ghostwriter');
+		return true;
+	};
+	
+	_skipChars = function(text, pos)
+	{
+		var back = '';
+		var len = text.length;
+		var c, i;
+		while (pos < len)
+		{
+			c = text.charCodeAt(pos);
+			if (c === 32)
+			{
+				back += ' ';
+				pos++;
+			}
+			else if (c === 60)
+			{
+				i = text.indexOf('>', pos);
+				if (i === -1)
+				{
+					return back + text.substr(pos);
+				}
+				c = text.substring(pos, i+1);
+				pos += c.length;
+				back += c;
+			}
+			else
+			{
+				break;
+			}
+		}
+		return back;
+	};
 	
 	_render = function(p)
 	{
 		var data = p.data('ghostwriter');
 		
-		var done = false, style='';
 		var text = data['text'];
-		var pos = data['pos'];
 		var len = text.length;
-		var out = '';
+		var pos = data['pos'];
+		var alen = data['len'];
+		var speed = data['group'];
+		var out = '', skip = '', i = 0, done = false;
 		
-		// Front
-		out += text.substr(0, pos++);
-//		text = text.substr(pos);
+		// Already processed.
+		out += '<span style="'+ data['style_processed'] +'">'+ text.substr(0, pos) +'</span>';
 		
-		for (var i = 0; i < data['length']; i++)
+		// Increase current length in the beginning.
+		if (data['running'] === 1)
 		{
+			if (alen < (data['ngroups'] * speed))
+			{
+				alen += speed;
+				data['len'] = alen;
+			}
+			else
+			{
+				data['running'] = 2;
+			}
+		}
+		
+		// Decrase current length at the end.
+		else if (data['running'] === 3)
+		{
+			alen -= speed;
+			data['len'] = alen;
+			if (alen <= 0)
+			{
+				data['running'] = 4;
+			}
+		}
+		
+		
+		// Now process current length chars.
+		for (i = 0; i < alen;)
+		{
+			// Done?
 			if (pos >= len)
 			{
-				done = true;
+				data['running'] = 3;
 				break;
 			}
-			var c = text.substr(pos++, 1);
 			
-			if (c === '<')
+			// Process next char
+			skip = _skipChars(text, pos);
+			if (skip.length > 0)
 			{
-				while (pos < len)
-				{
-					c = text.substr(pos++, 1);
-					if (c === '>')
-					{
-						pos++;
-						if (pos >= len)
-						{
-							done = true;
-						}
-						break;
-					}
+				// skip this.
+				out += skip;
+				pos += skip.length;
+			}
+			else
+			{
+				// Draw char.
+				out += '<span style="'+ _getStyle(i, alen, data) +'">'+ text.charAt(pos++) +'</span>';
+				i++;
+				
+				// Set current mark.
+				if (i === speed) {
+					data['pos'] = pos;
 				}
 			}
 			
-			if (done === true)
-			{
-				break;
-			}
-			
-			style = '';
-			out += '<span style="">'+style+'</span>';
-
 		}
 		
-		// Back
-		style = 'zoom: 1; filter: alpha(opacity=10); opacity: 0.1;';
-		out += '<span style="'+style+'">'+text.substr(pos)+'</span>';
-		
-		p.html(out);
-		data['pos'] = pos;
+		// Save state
 		p.data('ghostwriter', data);
 		
-		if (!done)
-		{
-			setTimeout(function(){_render(p);}, p.data('ghostwriter')['ms']);
+		// Rest
+		out += '<span style="'+data['style_unprocessed']+'">'+text.substr(pos)+'</span>';
+		
+		// Output
+		p.html(out);
+		
+		// Next cycle
+		if (data['running'] === 4) {
+//			setTimeout(function(){p.html(text);}, data['interval']);
+			setTimeout(data['callback'], 10);
+		} else {
+			setTimeout(function(){_render(p);}, data['interval']);
 		}
-	},
+	};
+	
+	_getStyle = function(i, elen, data)
+	{
+		var style = '', f = i/(elen-1);
+		
+		if (data['bold_left'] !== data['bold_right'])
+		{
+			style += ' font-weight: '+ parseInt(((data['bold_right']-data['bold_left'])*f)+data['bold_left']) +';';
+		}
+		
+		if (data['size_left'] !== data['size_right'])
+		{
+			style += ' font-size: '+ parseInt(((data['size_right']-data['size_left'])*f)+data['size_left']) +'px;';
+		}
+		
+		if (data['opaq_left'] !== data['opaq_right'])
+		{
+			style += _getOpaque(data['opaq_left'], data['opaq_right'], f);
+		}
+		
+		if (data['color_left'] !== data['color_right'])
+		{
+			style += ' color: '+Color.interpolate(data['color_left'], data['color_right'], f)+';';
+		}
+		
+		return style;
+	};
+	
+	_getOpaque = function(left, right, f)
+	{
+		var val = ((right-left)*f)+left;
+		return ' zoom: 1; filter: alpha(opacity='+parseInt(val*100)+'); opacity: '+val.toFixed(2)+';';
+	};
 
 	$.fn.ghostwriter = function(method, options)
 	{
@@ -117,8 +260,7 @@
 		}
 		else
 		{
-			$.error('Method '+method+' does not exist on jQuery.tinyzoom');
+			$.error('Method '+method+' does not exist in jQuery.ghostwriter');
 		}
 	};
-			
 })(jQuery);
