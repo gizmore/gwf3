1cf8915685561f959c475cea869ad7a56bb94b2d
Gizmore <gizmore@wechall.net>
2011-10-16 18:13:52 +0000

    Shadowlamb: The MCBartender quest could not get accepted. (thx jjk)

--- core/module/Lamb/lamb_module/Shadowlamb/city/Delaware/npc/talk/MCBarkeeper.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Delaware/npc/talk/MCBarkeeper.php
@@ -3,6 +3,8 @@ final class Delaware_MCBarkeeper extends SR_TalkingNPC
 {
 	public function getName() { return 'The bartender'; }
 	
+	public function getNPCQuests(SR_Player $player) { return array('Delaware_MCBartender'); }
+	
 	public function onNPCTalk(SR_Player $player, $word, array $args)
 	{
 		switch ($word)
