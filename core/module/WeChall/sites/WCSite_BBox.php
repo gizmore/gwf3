9625b18fcf85d884a71d285649a058bf5d5844f6
Gizmore <gizmore@wechall.net>
2011-10-16 16:07:17 +0000

    WC5: thebackupbox now repsonds the onsiterank for a user. (thx epoch)

--- core/module/WeChall/sites/WCSite_BBox.php
+++ core/module/WeChall/sites/WCSite_BBox.php
@@ -16,12 +16,14 @@ class WCSite_BBox extends WC_Site
 		$onsitescore = Common::clamp($onsitescore, 0, false);
 		$maxscore = intval($stats[1]);
 		$usercount = intval($stats[2]);
+		$onsiterank = isset($stats[3]) ? intval($stats[3]) : -1;
+		
 		$challcount = $maxscore;
 		if ($maxscore === 0 || $challcount === 0 || $usercount === 0) {
 			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
 		}
 		$this->updateSite($maxscore, $usercount, $challcount);
-		return array($onsitescore, -1, $onsitescore);
+		return array($onsitescore, $onsiterank, $onsitescore);
 	}
 	
 }
