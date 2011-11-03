f2dfa123bacab391aa2798c9b6f4d979f97b8c46
Gizmore <gizmore@wechall.net>
2011-11-03 00:07:42 +0000

    WC5: Fixed a bug in the french translation of the baim challenge. (thx kwisatz)

---
+++ www/challenge/B.AiM/index.php
@@ -0,0 +1,39 @@
+<?php
+chdir('../../'); # chroot to web root
+define('GWF_PAGE_TITLE', 'B.AiM'); # Wrapper hack
+require_once('html_head.php'); # output start of website
+# Get the challenge
+if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE))) {
+	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 7, 'challenge/B.AiM/index.php');
+}
+# And display the header
+$chall->showHeader();
+
+if (false === ($u = GWF_User::getByName('gizmore'))) {
+	$gizmore = '<b>gizmore</b>';
+	$egizmore = 'gizmore{at}wechall{dot}net';
+} else {
+	$gizmore = $u->displayProfileLink();
+	$egizmore = $u->displayEMail();
+}
+
+if (false === ($u = GWF_User::getByName('BAIM'))) {
+	$baim = '<b>BAIM</b>';
+	$ebaim = 'baim{at}wechall{dot}net';
+} else {
+	$baim = $u->displayProfileLink();
+	$ebaim = $u->displayEMail();
+}
+
+$url = 'http://baim.gizmore.org';
+echo GWF_Box::box($chall->lang('info', array($baim, $gizmore, $url, $ebaim, $egizmore, GWF_User::getStaticOrGuest()->displayUsername())), $chall->lang('title'));
+
+require_once 'challenge/B.AiM/baim_solve_form.php';
+if (WC_BaimForms::hasPermission()) {
+	echo GWF_Box::box($chall->lang('info_unlock', array('solved_it.php')), $chall->lang('form_title'));
+	
+}
+
+echo $chall->copyrightFooter();
+require_once('html_foot.php'); # output start of website
+?>
--- www/challenge/B.AiM/lang/chall_fr.php
+++ www/challenge/B.AiM/lang/chall_fr.php
@@ -2,10 +2,10 @@
 $lang = array(
 	'title' => 'B.AiM',
 	'info' =>
-		'B.AiM et gizmore sont heureux de vous présenter: Le challenge de cracking réaliste B.AIM.<br/>'.
-		'Votre mission est claire. Votre travail est de cracker une <a href="%1%">application réelle</a>.<br/>'.
+		'%s et %s sont heureux de vous présenter: Le challenge de cracking réaliste B.AIM.<br/>'.
+		'Votre mission est claire. Votre travail est de cracker une <a href="%s">application réelle</a>.<br/>'.
 		'Pour faire ceci, enregistrez vous sur le site web. Téléchargez la démo et réparez là pour la faire travailler sans connexion internet et sans clé<br/>'.
-		'Veuillez envoyer l\'exe réparé à gizmore[at]wechall(dot)net.<br/>'.
+		'Veuillez envoyer l\'exe réparé à %s et %s.<br/>'.
 		'<br/>'.
 		'Bonne chance!'.PHP_EOL.
 		'<!--Translation by grimmjowbo -->'.PHP_EOL,
