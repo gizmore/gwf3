121eb850433bfec8d61f51547d16c1ba0ab7ec24
Gizmore <gizmore@wechall.net>
2011-11-11 20:27:20 +0000

    GWF3: The db_backup.sh now zips old logfiles. In gwf2 this was done with a fat script in the logging module.

--- www/protected/db_backup.sh
+++ www/protected/db_backup.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-# Call this script once a day
-# via Crontab at 3 o'clock: 0 3 * * * cd /path/to/protected && ./db_backup.sh
-
-# Database Settings
+#
+# Daily database backup and log zipping.
+# Call this script once a day.
+# Example crontab for 3 o'clock: 0 3 * * * cd /path/to/protected && ./db_backup.sh
+#
+# @author: gizmore
+#
+###################
+### DB settings ###
+###################
 # Please escape password properly.
-MY_DB=lamb3;
-MY_USER=lamb3;
-MY_PASS=lamb3;
+MY_DB=lamb3;   # Edit me! #
+MY_USER=lamb3; # Edit me! #
+MY_PASS=lamb3; # Edit me! #
+MY_SALT=_CHANGE_ME_; # Edit me! # We don't rely on the salt, but it might be a hurdle for an attacker.
 
-# Generate often used Vars
+#################
+### More vars ###
+#################
 MY_DAY=`date +%d`
 MY_DATE=`date +%y%m%d`
-MY_FILENAME="$MY_DATE""_db_backup.sql.gz";
+MY_FILENAME="./""$MY_DATE""_db_backup""$MY_SALT"".sql.gz";
 
+##################
+### DB backups ###
+##################
 # Delete all files older than 7 days
-find db_backups/* -mtime +7 -exec rm {} \;
+find ./db_backups -name "*.sql.gz" -mtime +7 -exec rm {} \;
 
 # Back it up
 mysqldump --user=$MY_USER --password=$MY_PASS $MY_DB | gzip -9 > "db_backups/$MY_FILENAME"
@@ -24,3 +36,17 @@ if [ "$MY_DAY" == 01 ] || [ "$MY_DAY" == 15 ]
 then
 	mv "db_backups/$MY_FILENAME" "db_backups_old/$MY_FILENAME"
 fi
+
+################
+### Logfiles ###
+################
+# Compress old logfiles (24h ago)
+MY_DATE=`date +%y%m%d%H%M%S`
+MY_FILENAME="./zipped/""$MY_DATE""$MY_SALT""_logfiles.tar.gz";
+find ./logs -name "*.txt" -mmin +86401 | xargs tar --gzip -cf "$MY_FILENAME"
+
+# Remove old logfiles
+find ./logs -name *.txt -mmin +86401 -exec rm {} \;
+
+# Remove empty logdirs
+find ./logs -depth -type d -empty -exec rmdir {} \;
