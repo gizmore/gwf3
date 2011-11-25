51028338abfdbe9590efded748552e0ac5005e3d
spaceone <space@wechall.net>
2011-11-25 16:44:27 +0000

    GWF3: deleted html_{head,foot}.php and updated changelog

--- html_foot.php
+++
@@ -1,9 +0,0 @@
-<?php
-### This is wechall html_foot!
-
-if(defined('WC_HTML_HEAD__DEFINED')) { return; }
-define('WC_HTML_HEAD__DEFINED', true);
-
-GWF_Session::commit();
-echo '</div></div>';
-echo GWF_Template::templateWC('chall_foot.tpl', array('wcfooter' => WC_HTML::displayFooter()) );
