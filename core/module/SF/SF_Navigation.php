ef5478f8214b4ebcf4b872be310e0ac951499c00
spaceone <space@wechall.net>
2011-12-03 14:26:31 +0000

    SF: fixed fatal errors in SF_Navigation. It should work now again and updating should also be possible.

--- core/module/SF/SF_Navigation.php
+++ core/module/SF/SF_Navigation.php
@@ -3,20 +3,14 @@
 final class SF_Navigation extends GDO
 {
 	const VISIBLE = 0x01;
-	
-	const SECTIONS = 0x02;
-	const CATEGORIES = 0x04;
-	const SUBCATS = 0x08;
-	const LINKS = 0x10;
-	
-	const SECLINK = 0x20;
-	const CATLINK = 0x40;
-	const SUBLINK = 0x80;
-	
-	const SIDE_LEFT = 0x100;
-	const SIDE_RIGHT = 0x200;
-	const SIDE_OTHER = 0x400;
-	
+
+	const LINK = 0x02;
+//	const NOLINK = 0x04;
+
+	const SIDE_LEFT = 0x10;
+	const SIDE_RIGHT = 0x20;
+	const SIDE_OTHER = 0x40;
+
 	public function getClassName() { return __CLASS__; }
 	public function getTableName() { return GWF_TABLE_PREFIX.'SF_Navigation'; }
 	public function getColumnDefines()
@@ -57,37 +51,20 @@ final class SF_Navigation extends GDO
 	
 	public static function fetchAll($where, $orderby='') 
 	{
-		return GDO::table(__CLASS__)->selectAll('*', 'options & '.self::VISIBLE. ' = '.self::VISIBLE, $orderby);
+		return GDO::table(__CLASS__)->selectAll('*', sprintf('options & %s = %s AND %s', self::VISIBLE, self::VISIBLE, $where), $orderby);
 	}
 	public static function sql_secs($side)
 	{
-#		return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', $side, $side, self::SECTIONS, self::SECTIONS), 'position');
-		return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', $side, $side, self::SECTIONS, self::SECTIONS), 'position');
+		return self::fetchAll(sprintf('parent_id = 0 AND options & %s = %s', $side, $side), 'position');
 	}
-	public static function sql_cats($secid)
+	public static function sql_cats($pid)
 	{
-#		return self::fetchAll(sprintf('parent_id = %s', $secid),'parent_id, position');
-		return self::fetchAll(sprintf('parent_id = %s AND options & %s = %s ', $secid, self::CATEGORIES, self::CATEGORIES), 'parent_id, position');
+		return self::fetchAll(sprintf('parent_id = %s AND options & %s != %s', $pid, self::LINK, self::LINK), 'position');
+#		return self::fetchAll(sprintf('parent_id = %s AND options & %s = %s ', $pid, self::NOLINK, self::NOLINK), 'position');
 	}
-	public static function sql_subs($catid)
+	public static function sql_links($pid)
 	{
-#		return self::fetchAll(sprintf('parent_id = %s', $catid), 'parent_id, position');
-		return self::fetchAll(sprintf('parent_id = %s AND options & %s = %s ', $catid, self::SUBCATS, self::SUBCATS), 'parent_id, position');
-	}
-	public static function sql_seclinks($secid)
-	{
-#			return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', $secid, self::LINKS, self::LINKS, self::SECLINK, self::SECLINK), 'parent_id, position');
-			return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', $secid, self::LINKS, self::LINKS, self::SECLINK, self::SECLINK), 'parent_id, position');
-	}
-	public static function sql_catlinks($catid)
-	{
-#		return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', self::LINKS, self::LINKS, self::CATLINK, self::CATLINK), 'parent_id, position');
-		return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', self::LINKS, self::LINKS, self::CATLINK, self::CATLINK), 'parent_id, position');
-	}
-	public static function sql_sublinks($subid)
-	{
-#		return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', self::LINKS, self::LINKS, self::SUBLINK, self::SUBLINK), 'parent_id, position');
-		return self::fetchAll(sprintf('options & %s = %s AND options & %s = %s', self::LINKS, self::LINKS, self::SUBLINK, self::SUBLINK), 'parent_id, position');
+		return self::fetchAll(sprintf('parent_id = %s AND options & %s = %s', $pid, self::LINK, self::LINK), 'position');
 	}
 	public static function display_navigation($side) 
 	{
@@ -99,7 +76,7 @@ final class SF_Navigation extends GDO
 					self::link('sec='.$sec['short'].'#'.self::$idprefix.$sec['short'], $sec['name'], '', 2, true) .
 					"<ul>".PHP_EOL;
 
-			$seclinks = self::sql_seclinks($sec['ID']);
+			$seclinks = self::sql_links($sec['ID']);
 			foreach($seclinks as $seclink) 
 			{
 				$ret .= self::liste('sec_link', '', 3) . 
@@ -113,7 +90,7 @@ final class SF_Navigation extends GDO
 						self::link('sec=' . $sec['short'] . '&amp;cat=' . $cat['short'] . '#nav_' . $sec['short'] . '_' . $cat['short'], $cat['name'], '', 4 ,false ,'h2') . 
 						self::tab(4) . "<ul>".PHP_EOL;
 
-				$catlinks = self::sql_catlinks($cat['ID']);
+				$catlinks = self::sql_links($cat['ID']);
 				foreach($catlinks as $catlink) 
 				{
 					$ret .= self::liste('cat_link' ,'', 5) . 
@@ -121,14 +98,14 @@ final class SF_Navigation extends GDO
 							self::tab(5) . '</li>' . "".PHP_EOL ;
 				}
 
-				$subs = self::sql_subs($cat['ID']);
+				$subs = self::sql_cats($cat['ID']);
 				foreach($subs as $sub) 
 				{
 					$ret .= self::liste("subcat",$sec['short'] . '_' . $cat['short'] . '_' . $sub['short'], 5) .
 							self::link('sec=' . $sec['short'] . '&amp;cat=' . $cat['short'] . '&amp;subcat=' . $sub['short'] .'#nav_' . $sec['short'] . '_' . $cat['short'] /*. '_' . $sub['short'] */ , $sub['name'], '', 6) .
 							self::tab(6) . "<ul>".PHP_EOL ;
 
-					$sublinks = self::sql_sublinks($sub['ID']);
+					$sublinks = self::sql_links($sub['ID']);
 					foreach($sublinks as $sublink) 
 					{
 						$ret .= self::liste('subcat_link', '', 7) .
--- core/module/SF/Upgrade_SF_1_03.php
+++ core/module/SF/Upgrade_SF_1_03.php
@@ -9,23 +9,14 @@ function Upgrade_SF_1_03()
 		return GWF_HTML::err('ERR_DATABASE', array( __FILE__, __LINE__));
 	}
 
-	ini_set('memory_limit', '2048MB');
-	ini_set('max_execution_time', '3600');
-	
 	$foo = GDO::table('SF_Navigation');
 	$succ = true;
 	
 	$fo = array();
 	$fo[] = array("side='left'", SF_Navigation::SIDE_LEFT);
 	$fo[] = array("side='right'", SF_Navigation::SIDE_RIGHT);
-	$fo[] = array("side='SEC'", SF_Navigation::SECLINK);
-	$fo[] = array("side='CAT'", SF_Navigation::CATLINK);
-	$fo[] = array("side='SUB'", SF_Navigation::SUBLINK);
 	$fo[] = array("is_visible=1", SF_Navigation::VISIBLE);
-	$fo[] = array("TID=1", SF_Navigation::SECTIONS);
-	$fo[] = array("TID=2", SF_Navigation::CATEGORIES);
-	$fo[] = array("TID=3", SF_Navigation::SUBCATS);
-	$fo[] = array("TID=4", SF_Navigation::LINKS);
+	$fo[] = array("TID=4", SF_Navigation::LINK);
 	foreach($fo as $arr)
 		foreach($foo->selectAll('ID', $arr[0]) as $id)
 		{
