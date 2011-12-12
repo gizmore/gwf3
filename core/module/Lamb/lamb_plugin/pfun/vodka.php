54122750581538af703573fe4608bcec273a2c5e
freeartman <freeartman@wechall.net>
2011-12-12 16:17:06 +0000

    There are new mans ready for vodka

--- core/module/Lamb/lamb_plugin/pfun/vodka.php
+++ core/module/Lamb/lamb_plugin/pfun/vodka.php
@@ -1,8 +1,10 @@
 <?php # Drink some vodka with Lamb!
 switch($user->getName())
 {
-	case 'puolse': $msg = "Cheers and congrats!"; break;
-	case 'Oz': $msg = "Nastrovje!"; break;
+	case 'FreeArtMan': $msg = "Ah ... at least. I am not kid!"; break;
+	case 'paipai':     $msg = "Thank you. I will not forget your daily slap fam ;]"; break;
+	case 'puolse':     $msg = "Cheers and congrats!"; break;
+	case 'Oz':         $msg = "Nastrovje!"; break;
 	default: $msg = "You are too young for vodka, have no age specified, or your brain would suffer too much!"; break;
 } 
 $bot->reply($msg);
