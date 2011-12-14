2858d46c7db01943e41270fe586594a7a2f34c22
Gizmore <gizmore@wechall.net>
2011-12-14 16:57:21 +0000

    GWF3: Added an implementation of a polyalphabetic ROT cipher.

---
+++ core/inc/util/GWF_PolyROT.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Implementation of a polyalphabetic rot cipher.
+ * @author gizmore
+ */
+final class GWF_PolyROT
+{
+	public static function encrypt($message, $key)
+	{
+		$charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$message = strtoupper($message);
+		$key = strtoupper($key);
+		if (false === self::checkArgs($message, $key))
+		{
+			return false;
+		}
+		
+		$back = '';
+		$len = strlen($message);
+		$klen = strlen($key);
+		$clen = strlen($charset);
+		for ($i = 0; $i < $len; $i++)
+		{
+			$j = strpos($charset, $message[$i]);
+			$k = strpos($charset, $key[$i%$klen]);
+			$back .= $charset[($j+$k)%$clen]; 
+		}
+		return $back;
+	}
+
+	public static function decrypt($message, $key)
+	{
+		$charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$message = strtoupper($message);
+		$key = strtoupper($key);
+		if (false === self::checkArgs($message, $key))
+		{
+			return false;
+		}
+		
+		$back = '';
+		$len = strlen($message);
+		$klen = strlen($key);
+		$clen = strlen($charset);
+		for ($i = 0; $i < $len; $i++)
+		{
+			$j = strpos($charset, $message[$i]);
+			$k = strpos($charset, $key[$i%$klen]);
+			
+			$j -= $k;
+			while ($j < 0)
+			{
+				$j += $clen;
+			}
+			$back .= $charset[$j]; 
+		}
+		return $back;
+	}
+	
+	private static function checkArgs($message, $key)
+	{
+		if ($key === '')
+		{
+			return false;
+		}
+		
+		if (!preg_match('/^[A-Z ]+$/D', $message))
+		{
+			return false;
+		}
+
+		if (!preg_match('/^[A-Z]+$/D', $key))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+}
+?>
