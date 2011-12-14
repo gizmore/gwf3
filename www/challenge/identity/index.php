f8b6950a0a1be1edf1cf9c01399acc6f3b1d80ff
Gizmore <gizmore@wechall.net>
2011-12-14 21:04:28 +0000

    WC5: Fixes to identity. (thx kwisatz)

---
+++ www/challenge/identity/index.php
@@ -0,0 +1,68 @@
+<?php
+chdir('../../');
+define('GWF_PAGE_TITLE', 'Identity');
+require_once('html_head.php');
+require_once GWF_CORE_PATH.'module/WeChall/solutionbox.php';
+if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE)))
+{
+	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 4, 'challenge/identity/index.php', false);
+}
+$chall->showHeader();
+
+
+$score_needed = 500;
+$title = $chall->lang('title');
+
+
+if (false !== ($user = GWF_Session::getUser()))
+{
+	if ($user->getLevel() >= $score_needed)
+	{
+		$pre = $_POST['answer'];
+		identity_filter($chall);
+		$chall->onCheckSolution();
+		$_POST['answer'] = $pre;
+		
+		$gizmore = GWF_User::getByName('gizmore');
+		$profile = $gizmore->displayProfileLink();
+		echo GWF_Box::box($chall->lang('info', array($profile)), $title);
+		
+		require_once GWF_CORE_PATH.'module/WeChall/solutionbox.php';
+		echo formSolutionbox($chall);
+	}
+	else
+	{
+		$score = $user->getLevel();
+		echo GWF_HTML::error($title, $chall->lang('err_score', array($score, $score_needed)));
+	}
+}
+else
+{
+	echo GWF_HTML::error($title, $chall->lang('err_login'));
+}
+
+echo $chall->copyrightFooter();
+require_once('html_foot.php');
+?>
+
+<?php
+function identity_filter(WC_Challenge $chall)
+{
+	if ( (!isset($_POST['answer'])) || (is_string($_POST['answer'])) )
+	{
+		return;
+	}
+	
+	$answer = $_POST['answer'];
+	$answer = str_replace(array(' ',','), '', $answer);
+	$answer = strtolower($answer);
+	$answer = str_replace('049', '0', $answer);
+
+	if (strpos($answer, '17659598844') !== false)
+	{
+		echo GWF_HTML::error($chall->lang('title'), $chall->lang('err_home_phone'));
+	}
+	
+	$_POST['answer'] = $answer;
+}
+?>
--- www/challenge/identity/lang/chall_en.php
+++ www/challenge/identity/lang/chall_en.php
@@ -2,13 +2,16 @@
 $lang = array(
 	'title' => 'Identity',
 	'info' =>
-		'We got a mission from a contactor:<br/>'.
-		'Find out who is %1%!<br/>'.
-		'The solution is Firstname,Lastname,StreetNum,City,HomePhone separated by comma.<br/>'.
-		'Example: Peter,Lustig,Peters Weg 5,Hannover,049444455555<br/>'.
-		'<br/>'.
-		'Hint: We suppose it might be a good start to know that the domain gizmore.org and the server are owned by gizmore himself.<br/>'.
-		'<br/>'.
-		'We await your success!',
+		'We got a mission from a contactor:<br/>'.PHP_EOL.
+		'Find out who is %s!<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'The solution is Firstname,Lastname,Street,House,ZIP,City,HomePhone separated by commas.<br/>'.PHP_EOL.
+		'Example: Maria,Mustermann,Musterstraße,14,12345,Musterstadt,04915112345678<br/>'.PHP_EOL.
+		'<span style="color:#eee">We suppose it might be a good start to know that the domain gizmore.org and the server are owned by gizmore himself.</span><br/>'.PHP_EOL.
+		'We await your success!'.PHP_EOL,
+		
+	'err_login' => 'This challenge is about identities. Without beeing logged in, you don\'t have one, so please login.',
+	'err_score' => 'It seems like your account has only %s points, but to accept this mission, you need at least %s.',
+	'err_home_phone' => 'It seems like you try to enter the mobile phone number, but we need the <b>home</b> phone number.',
 );
 ?>
