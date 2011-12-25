42eeff0730b8e929d9e95a65ab63928a9fa7ded4
spaceone <space@wechall.net>
2011-12-25 14:53:56 +0000

    GWF3: created util/un_svn.php

--- util/un_svn.php
+++ util/un_svn.php
@@ -1,7 +1,38 @@
 <?php
 /**
  * Helper file to remove all .svn files.
- * 
+ * @author spaceone
+ * @todo svn export is way better
+ * @todo analyze return values
+ * @todo do with PHP?
  */
 
 chdir('../');
+
+//$cmd = 'svn export --force . .';
+$cmd = 'rm -fR .svn; find -name \'*.swp\' -exec rm -f {} \;';
+
+$error = false;
+if( !function_exists('system') )
+{
+	$funcs = array('shell_exec', 'passthru', 'exec', 'popen');
+	foreach($funcs as $f)
+	{
+		if(function_exists($f))
+		{
+			function system($arg) { return $f($arg); }
+			break;
+		}
+		$error = true;
+	}
+}
+elseif(false === $error)
+{
+	echo system($cmd);
+	echo PHP_EOL.PHP_EOL.'I dunno if errors occured!';
+}
+else
+{
+	echo 'no system() function is allowed! Try to manually delete the .svn directory and all .swp files!'.PHP_EOL;
+	echo '$ '.$cmd;
+}
