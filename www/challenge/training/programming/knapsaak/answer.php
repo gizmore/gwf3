6c693f48620e7277a2a6e80ed3e1fc15c0167d93
Gizmore <gizmore@wechall.net>
2012-01-03 07:20:36 +0000

    WC5: Fixes for the knapsaak problem.

--- www/challenge/training/programming/knapsaak/answer.php
+++ www/challenge/training/programming/knapsaak/answer.php
@@ -6,7 +6,7 @@ require_once 'challenge/gwf_include.php';
 GWF_Website::init(getcwd());
 
 require_once 'challenge/training/programming/knapsaak/salesman.php';
-$wechall = GWF_Module::loadModuleDB('WeChall');
+$wechall = GWF_Module::loadModuleDB('WeChall', true, true);
 if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE))) {
 	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 4, 'challenge/training/programming/knapsaak/index.php');
 }
