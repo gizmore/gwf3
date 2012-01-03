792d19d04858648e0c964feccf58734cad3e549e
spaceone <space@wechall.net>
2012-01-03 02:04:41 +0000

    GWF3: module Languages can now be in GWF_WWW_PATH/lang/

--- core/inc/util/GWF_Module.php
+++ core/inc/util/GWF_Module.php
@@ -94,7 +94,22 @@ class GWF_Module extends GDO
 	### Lang ###
 	############
 	public function getLanguage() { return $this->lang; }
-	public function loadLanguage($path) { if ($this->lang === NULL) { $this->lang = new GWF_LangTrans(sprintf('%smodule/%s/%s', GWF_CORE_PATH, $this->getName(), $path)); } }
+	public function loadLanguage($path)
+	{
+		if ($this->lang === NULL)
+		{
+			$path2 = sprintf('module/%s/%s', $this->getName(), $path);
+			if(true === file_exists(GWF_WWW_PATH.'lang/'.$path2))
+			{
+				$path3 = GWF_WWW_PATH.'lang/'.$path2;
+			}
+			else
+			{
+				$path3 = GWF_CORE_PATH.$path2;
+			}
+			$this->lang = new GWF_LangTrans($path3);
+		}
+	}
 	public function onLoadLanguage() { $this->lang = GWF_HTML::getLang(); }
 	public function error($key, $args=NULL, $log=true, $to_smarty=false) { return GWF_HTML::error($this->getName(), $this->lang($key, $args), $log, $to_smarty); }
 	public function message($key, $args=NULL, $log=true, $to_smarty=false) { return GWF_HTML::message($this->getName(), $this->lang($key, $args), $log, $to_smarty); }
