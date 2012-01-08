3bef57d56e224f3a0c09758331e03444d4a016e9
Gizmore <gizmore@wechall.net>
2012-01-08 13:58:33 +0000

    WC5: Fixes for the warchall beginning.

--- core/module/Audit/Module_Audit.php
+++ core/module/Audit/Module_Audit.php
@@ -8,7 +8,7 @@ final class Module_Audit extends GWF_Module
 	const DEFAULT_LOGFILE = '/var/log/remote/box1/local2';
 	
 	public function getVersion() { return 1.04; }
-	public function getClasses() { return array('GWF_AuditLog', 'GWF_AuditLogin', 'GWF_AuditMails'); }
+	public function getClasses() { return array('GWF_AuditAddUser', 'GWF_AuditLog', 'GWF_AuditLogin', 'GWF_AuditMails'); }
 	public function onCronjob() { $this->includeClass('GWF_AuditCronjob'); GWF_AuditCronjob::onCronjob($this); }
 	public function onInstall($dropTable) { $this->includeClass('GWF_AuditInstall'); return GWF_AuditInstall::onInstall($this, $dropTable); }
 	public function onLoadLanguage() { return $this->loadLanguage('lang/audit'); }
