3590720fc6622ccc7c560028ed46abc1bc7372ca
tehron <tehron@wechall.net>
2012-01-10 19:41:06 +0000

    Shadowlamb: Fix Chicago Subway Guy strings

--- core/module/Lamb/lamb_module/Shadowlamb/city/Chicago/npc/talk/SubwayGuy.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Chicago/npc/talk/SubwayGuy.php
@@ -11,20 +11,20 @@ final class Chicago_SubwayGuy extends SR_TalkingNPC
 		$b = chr(2);
 		switch ($word)
 		{
-			case 'renraku': return $this->reply("The nightmares. Please don\'t...");
-			case 'shadowrun': return $this->reply("I was once a {b}shadowrun{b}ner. Then they got hold of me...");
+			case 'renraku': return $this->reply("The nightmares. Please don't...");
+			case 'shadowrun': return $this->reply("I was once a {$b}shadowrun{$b}ner. Then they got hold of me...");
 			case 'cyberware': return $this->reply("I know nothing of this. Beep");
-			case 'magic': return $this->reply("The world is full of {b}magic{b}");
-			case 'hire': return $this->reply("I don\'t think I dare... Sorry chummer");
+			case 'magic': return $this->reply("The world is full of {$b}magic{$b}");
+			case 'hire': return $this->reply("I don't think I dare... Sorry chummer");
 			case 'blackmarket': return $this->reply("You need a permission to enter. Ask around");
-			case 'bounty': return $this->reply("There is no {b}bounty{b} on my head!");
+			case 'bounty': return $this->reply("There is no {$b}bounty{$b} on my head!");
 			case 'seattle': return $this->reply("The city is big. It is dangerous here");
 			case 'alchemy': return $this->reply("I know nothing of this");
-			case 'invite': return $this->reply("No. No I can\'t show my face");
+			case 'invite': return $this->reply("No. No I can't show my face");
 			case 'malois': return $this->reply("Who?");
-			case 'hello': return $this->reply("Hi... Please don\'t hurt me!");
-			case 'yes': return $this->reply("Don\'t say {b}yes{b} to everything. You can get into a lot of trouble");
-			case 'no': return $this->reply("I wish I\'d have said {b}no{b}");
+			case 'hello': return $this->reply("Hi... Please don't hurt me!");
+			case 'yes': return $this->reply("Don't say {$b}yes{$b} to everything. You can get into a lot of trouble");
+			case 'no': return $this->reply("I wish I'd have said {$b}no{$b}");
 			//case 'negotiation':
 			default:
 				return $this->reply("Leave me alone.");
