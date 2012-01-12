6c6f49cd7bae3466fc6154774d8de0c877ceb1a8
Gizmore <gizmore@wechall.net>
2012-01-12 23:17:23 +0000

    Shadowlamb: Increase drop rates for runes.

--- core/module/Lamb/lamb_module/Shadowlamb/item/runes/Rune.php
+++ core/module/Lamb/lamb_module/Shadowlamb/item/runes/Rune.php
@@ -2,7 +2,7 @@
 final class Item_Rune extends SR_Rune
 {
 	public function getItemLevel() { return 4; }
-	public function getItemDropChance() { return 50.00; }
+	public function getItemDropChance() { return 100.00; }
 	public function getItemPrice() { return 100; }
 	public function getItemDescription() { return 'A rune to upgrade equipment at a blacksmith.'; }
 	public function getItemWeight() { return 150; }
