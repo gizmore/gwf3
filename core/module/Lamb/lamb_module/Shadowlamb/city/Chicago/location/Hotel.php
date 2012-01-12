c6b560c5e2a7bc006fefcb024c250fc1799cd5e1
tehron <tehron@wechall.net>
2012-01-12 16:34:55 +0000

    Shadowlamb: Fix typos and smaller errors

--- core/module/Lamb/lamb_module/Shadowlamb/city/Chicago/location/Hotel.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Chicago/location/Hotel.php
@@ -2,8 +2,8 @@
 final class Chicago_Hotel extends SR_Hotel
 {
 	public function getNPCS(SR_Player $player) { return array('talk' => 'Chicago_Hotelier', 'ttw'=>'Chicago_HotelWoman'); }
-	public function getFoundText(SR_Player $player) { return 'You find a Hotel. A sign reads: "Rooms, 160 Nuyen a day".'; }
-	public function getEnterText(SR_Player $player) { return 'You enter the Hotel. You see the Hotelier and walk to the counter. You also see a suspicios woman in a loungy corner.'; }
+	public function getFoundText(SR_Player $player) { return 'You find a Hotel. A sign reads: "Rooms, 150 Nuyen a day".'; }
+	public function getEnterText(SR_Player $player) { return 'You enter the Hotel. You see the Hotelier and walk to the counter. You also see a suspicious woman in a loungy corner.'; }
 	public function getHelpText(SR_Player $player) { return "Use #talk to talk to the hotelier. Use #ttw to talk to the woman."; }
 	public function getFoundPercentage() { return 100.00; }
 	
