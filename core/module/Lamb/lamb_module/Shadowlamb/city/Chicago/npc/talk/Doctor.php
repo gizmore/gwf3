657560627b9d740a60d3c9dab4460810742fac3b
livinskull <livinskull@wechall.net>
2012-01-13 01:01:23 +0000

    Shadowlamb: let Chicago doctor talk

---
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Chicago/npc/talk/Doctor.php
@@ -0,0 +1,30 @@
+<?php
+final class Chicago_HospitalDoctor extends SR_TalkingNPC
+{
+	public function getName() { return 'The doctor'; }
+	
+	public function onNPCTalk(SR_Player $player, $word, array $args)
+	{
+		$c = Shadowrun4::SR_SHORTCUT;
+		$b = chr(2);
+		switch ($word)
+		{
+			case 'malois':
+				return $this->reply('I do not talk about customers.');
+				
+			case 'heal':
+				return $this->reply("We can heal you for some nuyen. Just use {$c}heal here.");
+			
+			case 'yes': case 'no':
+			case 'cyberware':
+				return $this->reply("We have the best Renraku Cyberware available. Use {$c}view, {$c}implant and {$c}unplant to manage your accessoires.");
+				
+			case 'hello':
+			default:
+				$this->reply("Hello chummer, need some {$b}heal{$b} or {$b}cyberware{$b}?");
+				$player->giveKnowledge('words', 'Cyberware','Yes','No');
+				return true;
+		}
+	}
+}
+?>
