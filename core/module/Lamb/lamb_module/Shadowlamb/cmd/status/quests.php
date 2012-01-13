b394e9f408541023938d1fb020fab503c6fcf03e
Gizmore <gizmore@wechall.net>
2012-01-13 02:58:12 +0000

    Shadolamb: Sync

--- core/module/Lamb/lamb_module/Shadowlamb/cmd/status/quests.php
+++ core/module/Lamb/lamb_module/Shadowlamb/cmd/status/quests.php
@@ -137,74 +137,12 @@ final class Shadowcmd_quests extends Shadowcmd
 	
 	private static function onDisplayQuest(SR_Player $player, SR_Quest $quest, $id)
 	{
-		if ($quest->isUnknown())
+		if ($quest->isUnknown($player))
 		{
 			return self::reply($player, 'This quest is unknown to you.');
 		}
-			
-		
 		$message = sprintf('%d: %s - %s', $id, $quest->getQuestName(), $quest->getQuestDescription());
 		return self::reply($player, $message);
 	}
-	
-	
-	
-	
-	
-	
-// 	public static function executeOLD(SR_Player $player, array $args)
-// 	{
-// 		$bot = Shadowrap::instance($player);
-		
-// 		$quest = false;
-// 		switch (count($args))
-// 		{
-// 			case 2:
-// 				$id = $args[1];
-// 				if (!is_numeric($id)) {
-// 					$bot->reply(Shadowhelp::getHelp($player, 'quests'));
-// 					return false;
-// 				}
-// 				else {
-// 					if (false === ($quest = SR_Quest::getByID($player, $args[0], $id))) {
-// 						$bot->reply('This quest-id is unknown.');
-// 						return false;
-// 					} 
-// 				}
-// 			case 1:
-// 				$section = $args[0];
-// 				if (is_numeric($section)) {
-// 					if (false === ($quest = SR_Quest::getByID($player, 'accepted', $section))) {
-// 						$bot->reply('This quest-id is unknown.');
-// 						return false;
-// 					}
-// 					$section = 'open';
-// 				}
-// 				break;
-				
-// 			case 0:
-// 				$section = 'open';
-// 				break;
-				
-// 			default: $bot->reply(Shadowhelp::getHelp($player, 'quests')); return false;
-				
-// 		}
-		
-// 		# Display info
-// 		if ($quest === false)
-// 		{
-// 			if (false === ($message = Shadowfunc::getQuests($player, $section))) {
-// 				$bot->reply(Shadowhelp::getHelp($player, 'quests'));
-// 				return false;
-// 			}
-// 			$bot->reply(sprintf('Your %s quests: %s.', $section, $message));
-// 			return true;
-// 		}
-// 		else
-// 		{
-// 			$bot->reply($quest->displayQuest());
-// 			return true;
-// 		}
-// 	}
 }
 ?>
