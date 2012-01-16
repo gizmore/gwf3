4e4b3fb3d961d7883f86c041ba91f1a2039db9f4
sabretooth <sabretooth@wechall.net>
2012-01-16 10:26:34 +0000

    ShadowLamb: New NPC dialogue for the Chicago BlackSmithSalesman. (poor guy... having to deal with that drunken fool daily... )

--- core/module/Lamb/lamb_module/Shadowlamb/city/Chicago/npc/talk/BlackSmithSalesman.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Chicago/npc/talk/BlackSmithSalesman.php
@@ -15,24 +15,24 @@ final class Chicago_BlackSmithSalesman extends SR_TalkingNPC
 		$b = chr(2); # bold
 		switch ($word)
 		{
-			case 'seattle': return $this->reply("");
-			case 'shadowrun': return $this->reply("");
-			case 'cyberware': return $this->reply("");
-			case 'magic': return $this->reply("");
-			case 'hire': return $this->reply("");
-			case 'blackmarket': return $this->reply("");
-			case 'bounty': return $this->reply("");
-			case 'alchemy': return $this->reply("");
-			case 'invite': return $this->reply("");
-			case 'renraku': return $this->reply("");
-			case 'malois': return $this->reply("");
-			case 'bribe': return $this->reply("");
-			case 'yes': return $this->reply("");
-			case 'no': return $this->reply("");
-			case 'negotiation': return $this->reply("");
-			case 'hello': return $this->reply("");
+			case 'seattle': return $this->reply("There are smiths in many cities, but ours is the best.");
+			case 'shadowrun': return $this->reply("No, no! I am a salesman!");
+			case 'cyberware': return $this->reply("Maybe one day the smith will look into implanting it. For now, leave it to the doctor, chummer.");
+			case 'magic': return $this->reply("Runes can be broken away from statted items. See the smith!");
+			case 'hire': return $this->reply("Why would you want to hire a salesman? Are you shy?");
+			case 'blackmarket': return $this->reply("Many items can be bought from there. Watch out for the quality though!");
+			case 'bounty': return $this->reply("I have no idea.");
+			case 'alchemy': return $this->reply("Fascinating, you think?");
+			case 'invite': return $this->reply("No, no. He has had too much already.");
+			case 'renraku': return $this->reply("I know nothing of them.");
+			case 'malois': return $this->reply("Yeah... I know him.");
+			case 'bribe': return $this->reply("For what chummer?");
+			case 'yes': return $this->reply("Finally. Someone who agrees with me. Okay smithy... Stop drinking!");
+			case 'no': return $this->reply("You hear that smithy? He said no more booze!");
+			case 'negotiation': return $this->reply("Maybe a little.");
+			case 'hello': return $this->reply("Welcome! Better talk to me... The smith is a bit... busy.");
 			default:
-				return $this->reply("I do not know anything about $word.");
+				return $this->reply("The smith is always drunk, but he does a great job!");
 		}
 	}
 }
