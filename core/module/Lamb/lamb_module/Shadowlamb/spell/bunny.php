54e0e0f3108209a85bcbc2c009df0531c2991a4e
Gizmore <gizmore@wechall.net>
2012-01-26 00:22:31 +0000

    Shadowlamb: Patches for the bunny and rabbit spells. (thx noother)

--- core/module/Lamb/lamb_module/Shadowlamb/spell/bunny.php
+++ core/module/Lamb/lamb_module/Shadowlamb/spell/bunny.php
@@ -3,31 +3,22 @@ final class Spell_bunny extends SR_SupportSpell
 {
 	public function getSpellLevel() { return 1; }
 	
-	public function getHelp() { return 'Make yourself #flee a combat.'; }
+	public function getHelp() { return 'Make a party member #flee from combat.'; }
 	public function getRequirements() { return array('magic'=>2); }
 	public function getCastTime($level) { return Common::clamp(rand(10, 30)); }
 	public function getManaCost(SR_Player $player, $level)
 	{
-//		$level = $this->getLevel($player);
 		return Common::clamp(7-$level/4, 1);
 	}
 	
 	public function cast(SR_Player $player, SR_Player $target, $level, $hits)
 	{
-		if ($target->getID() === $player->getID())
-		{
-			$player->message('You cannot cast bunny on other players.');
-			return false;
-		}
-
 		$this->announceADV($player, $target, $level);
-		
 		if ($hits > $this->getManaCost())
 		{
-			Shadowcmd_flee::onFlee($player);
+			Shadowcmd_flee::onFlee($target);
 			return true;
 		}
-		
 		return true;
 	}
 }
--- core/module/Lamb/lamb_module/Shadowlamb/spell/rabbit.php
+++ core/module/Lamb/lamb_module/Shadowlamb/spell/rabbit.php
@@ -3,12 +3,11 @@ final class Spell_rabbit extends SR_SupportSpell
 {
 	public function getSpellLevel() { return 3; }
 	
-	public function getHelp() { return 'Make a party member #flee a combat.'; }
+	public function getHelp() { return 'Make your whole party flee from combat.'; }
 	public function getRequirements() { return array('magic'=>2); }
 	public function getCastTime($level) { return Common::clamp(rand(20, 40)); }
 	public function getManaCost(SR_Player $player, $level)
 	{
-//		$level = $this->getLevel($player);
 		return Common::clamp(10-$level/2, 2);
 	}
 	
@@ -18,8 +17,10 @@ final class Spell_rabbit extends SR_SupportSpell
 		
 		if ($hits > $this->getManaCost())
 		{
-			Shadowcmd_flee::onFlee($target);
-			return true;
+			foreach ($player->getParty()->getMembers() as $member)
+			{
+				Shadowcmd_flee::onFlee($member);
+			}
 		}
 		return true;
 	}
