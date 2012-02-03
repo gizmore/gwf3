9e5c0b95e445bc7370d5c1babacb54d09c2e02aa
Gizmore <gizmore@wechall.net>
2012-02-03 03:29:03 +0000

    Lamb: New fun command: .predict.

---
+++ core/module/Lamb/lamb_plugin/pfun/predict.php
@@ -0,0 +1,21 @@
+<?php # Usage: %CMD% <integer>. Predict the next sequentiell integer polled from rand(). When you can predict 20 numbers in a row i send you $100. (gizmore)
+$bot = Lamb::instance();
+
+if ( ($message === '') || (false === Common::isNumeric($message)) )
+{
+	return $bot->getHelp('predict');
+}
+
+$predicted = (int)$message;
+$max = getrandmax();
+$rand = rand(0, getrandmax());
+
+if ($rand === $predicted)
+{
+	return $bot->reply("Well done!");
+}
+else
+{
+	return $bot->reply(sprintf('You predicted %s but the outcome was %s.', $predicted, $rand));
+}
+?>
