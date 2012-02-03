ca34346a3598cb28bee7d7c32e9b89bb76f78805
Gizmore <gizmore@wechall.net>
2012-02-03 00:15:51 +0000

    Shadowlamb: Firewall now depends on firebolt and fireball. (thx ynori7)

--- core/module/Lamb/lamb_module/Shadowlamb/spell/firewall.php
+++ core/module/Lamb/lamb_module/Shadowlamb/spell/firewall.php
@@ -11,12 +11,24 @@ final class Spell_firewall extends SR_CombatSpell
 	
 	public function getManaCost(SR_Player $player, $level)
 	{
-		return 4 + ($level);
+		return 3 + ($level*1.5);
 	}
 	
 	public function cast(SR_Player $player, SR_Player $target, $level, $hits)
 	{
 // 		echo "Casting Firewall with level $level and $hits hits.\n";
+		
+		# Firebolt ads 0.20 per level
+		$firebolt = $player->getSpell('firebolt');
+		$firebolt = $firebolt === false ? 0 : $firebolt->getLevel($player);
+		$firebolt = round($firebolt/5, 1);
+		$level += $firebolt;
+		
+		# Fireball ads 0.25 per level
+		$fireball = $player->getSpell('firebolt');
+		$fireball = $fireball === false ? 0 : $fireball->getLevel($player);
+		$fireball = round($fireball/4, 1);
+		$level += $fireball;
 		
 		$line = $target->getY();
 		$damage = array();
