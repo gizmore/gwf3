01ebbab6ab36e1606e98eb7f1bb519f7f7f44be2
Gizmore <gizmore@wechall.net>
2012-02-05 03:03:36 +0000

    WC5: Fixed lang file for tropical fruits challenge.

--- www/challenge/warchall/tropical/7/lang/chall_en.php
+++ www/challenge/warchall/tropical/7/lang/chall_en.php
@@ -2,7 +2,7 @@
 $lang = array(
 	'title' => 'Warchall - Seven Tropical Fruits',
 	'info' =>
-		'This is level 7 on the <a href="%1%">warchall box</a>.<br/>'.PHP_EOL.
+		'This is level 7 on the <a href="%s">warchall box</a>.<br/>'.PHP_EOL.
 		'You can find the vulnerable binary + sourcecode in /home/level/tropic/7.<br/>'.PHP_EOL.
 		'Your mission is to exploit it, circumvent the ASLR protection and read a file in the name of the executable.<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
