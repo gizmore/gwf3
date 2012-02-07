39aed53cb435f59ea8130c115ded5a589b7844c0
Gizmore <gizmore@wechall.net>
2012-02-07 11:43:45 +0000

    Shadowlamb: A backpack now increases your max weight by 2kg. (thx dloser)

--- core/module/Lamb/lamb_module/Shadowlamb/item/mount/Backpack.php
+++ core/module/Lamb/lamb_module/Shadowlamb/item/mount/Backpack.php
@@ -2,12 +2,20 @@
 require_once 'Pockets.php';
 class Item_Backpack extends Item_Pockets
 {
-	public function getItemDescription() { 'A backpack allows you to walk a bit faster.'; }
+	const BACKPACK_WEIGHT = 2000;
+	public function getItemDescription() { 'A backpack allows you to walk a bit faster and carry '.Shadowfunc::displayWeight(self::BACKPACK_WEIGHT); }
 	public function getItemPrice() { return 125.00; }
 	
 	public function getMountWeight() { return 0; }
 	public function getMountPassengers() { return 1; }
 	public function getMountLockLevel() { return 0; }
 	public function getMountTime($eta) { return parent::getMountTime($eta) * 0.95; }
+
+	public function getItemModifiersA(SR_Player $player)
+	{
+		return array(
+			'max_weight' => self::BACKPACK_WEIGHT,
+		);
+	}
 }
 ?>
