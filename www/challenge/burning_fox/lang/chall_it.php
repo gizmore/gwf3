e264b925ea85b79d4c57a8823a27940097985dbd
monnino <monnino@wechall.net>
2012-02-24 20:41:21 +0000

    GWF3: Fixed shadowlamb_it typo, translated lastest challenge (burning fox).

---
+++ www/challenge/burning_fox/lang/chall_it.php
@@ -0,0 +1,11 @@
+<?php
+$lang = array(
+	'title' => 'The Burning Fox ',
+	'info' =>
+		'La nostra agenzia di sicurezza ha ricevuto una copia portabile di Firefox.<br/>'.
+		'Il tuo lavoro è di recuperare la password principale così che le autorità possano investigare sui siti e sulle informazioni private generate da questa persona.<br/>'.
+		'Puoi scaricare la versione portatile da questa cartella, nome del file &quot;burningfox.zip&quot;.<br/>'.
+		'<br/>'.
+		'Buona fortuna!',
+);
+?>
