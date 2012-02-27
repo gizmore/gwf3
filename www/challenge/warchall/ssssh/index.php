fbbe78e34b88168d0f6fda04dca9a0b9622edf79
Gizmore <gizmore@wechall.net>
2012-02-27 02:35:14 +0000

    WC5: Moved zssh challenge to warchall folder. (thx Z)

---
+++ www/challenge/warchall/ssssh/index.php
@@ -0,0 +1,21 @@
+<?php
+chdir('../../../');
+define('GWF_PAGE_TITLE', 'SSH... Z is sleeping');
+require_once('challenge/html_head.php');
+require_once GWF_CORE_PATH.'module/WeChall/solutionbox.php';
+if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE)))
+{
+	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 3, 'challenge/Z/ssssh/index.php', false);
+}
+$chall->showHeader();
+
+$chall->onCheckSolution();
+
+$href_warchall = GWF_WEB_ROOT.'challenge/warchall/begins';
+echo GWF_Box::box($chall->lang('info', array($href_warchall)), $chall->lang('title'));
+formSolutionbox($chall, 14);
+
+echo $chall->copyrightFooter();
+require_once('challenge/html_foot.php');
+
+?>
---
+++ www/challenge/warchall/ssssh/lang/chall_it.php
@@ -0,0 +1,7 @@
+<?php 
+$lang = array(
+	'title' => 'SSH... Z stà dormendo',
+	'info' => 'Z sta dormendo, se ti sbrighi puoi rubare la sua chiave SSH ed accedere come level8 sulla <a href="%s">warchall box</a>',
+);
+
+?>
