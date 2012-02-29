0f18404614ab843f0cfb8e65d22ac6904ac54737
Gizmore <gizmore@wechall.net>
2012-02-29 02:07:06 +0000

    WC5: German translation for Inka challenge.

---
+++ www/challenge/inka/lang/chall_de.php
@@ -0,0 +1,29 @@
+<?php
+$lang = array(
+	'title' => 'Inka',
+	'info' =>
+		'In ferner Zukunft finden Archäologen komische Zeichnungen an der Wand:<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Solo, I\'m a soloist on a solo list<br/>'.PHP_EOL.
+		'All live, never on a floppy disk<br/>'.PHP_EOL.
+		'Inka, inka, bottle of ink<br/>'.PHP_EOL.
+		'Paintings of rebellion<br/>'.PHP_EOL.
+		'Drawn up by the thoughts I think<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Zusammen mit einer seltsamen Formel:<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'<a href="%s">INKA</a><br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Sende deine Lösung hierhin: <a href="%s">INKA</a><br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Dank und Gruß an: livinskull für die Idee, monnino für seine harte Arbeit, dloser fürs testen.<br/>'.PHP_EOL.
+		'Auch dank an ein Open Source Projekt, daß ich hier nicht namentlich erwähnen kann.<br/>'.PHP_EOL.
+		'%s<br/>'.PHP_EOL.
+		'Good luck!',
+	
+	'err_no_question' => 'Du hast keine Aufgabe erhalten.',
+	'err_wrong' => 'Falsch, Falsch, Falsch! Richtig gewesen wäre: %s',
+	'err_slow' => 'Zu langsam! Du hast %.02fs gebraucht, hast aber nur %.02f.',
+	'msg_grats' => 'Inka Inka, Bottle of Ink! Du hast es geschafft!',
+);
+?>
