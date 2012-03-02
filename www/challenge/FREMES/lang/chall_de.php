de3b6c307282998dd198aa3c54a7430d02fcd053
Gizmore <gizmore@wechall.net>
2012-03-02 05:21:49 +0000

    WC5: German translation of the FREMES challenge.

---
+++ www/challenge/FREMES/lang/chall_de.php
@@ -0,0 +1,16 @@
+<?php
+$lang = array(
+	'title' => 'Fremen',
+	'info' =>
+		'Wir haben erfolgreich die FREMEN Gruppe unterwandert und können <a href="%2$s">Ihre Nachrichten mitlesen</a>.<br/>'.PHP_EOL.
+		'Unsere Steganographieexperten vermuten Sie tauschen %1$s Bit Schlüssel mit einer bislang unbekannten Methode aus.<br/>'.PHP_EOL.
+		'Deine Aufgabe ist es an einen geheimen Schlüssel zu gelangen, so das wir weiteres Material entschlüsseln können.<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Als Lösung, sende den kompletten %1$s Bit Schlüssel als Hexadezimalzahl. Besipiel: 0xdb213449933ddf9ba2d41b8224a43292.'.
+		'<br/>'.PHP_EOL.
+		'Hinweis: Der Geheimschlüssel ist an deine <b>Session</b> gebunden.<br/>'.
+		'Gruß an XcrackX für die Inspiration. Sei weiter so fleissig!',
+
+	'err_try' => 'Bitte rufe zuerst <a href="fremes.php">fremes.php</a> auf.',
+);
+?>
