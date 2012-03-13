bc1f06ecb8c8708da8aff682a1f75d1451f9b3ef
Gizmore <gizmore@wechall.net>
2012-03-13 14:47:35 +0000

    WC5: Fixes for new "Repeating History" Challenge. Also french translation by awe. (thx awe)

--- www/challenge/subversive/history/install.php
+++ www/challenge/subversive/history/install.php
@@ -1,5 +1,5 @@
 <?php
-chdir('../../');
+chdir('../../../');
 require_once('challenge/html_head.php');
 define('GWF_PAGE_TITLE', 'Repeating History');
 $title = GWF_PAGE_TITLE;
@@ -12,7 +12,7 @@ if (!GWF_User::isAdminS())
 $solution = '2bda2998d9b0ee197da142a0447f6725';
 $score = 2;
 $url = 'challenge/subversive/history/index.php';
-$creators = 'spaceone,Gizmore';
+$creators = 'space,Gizmore';
 $tags = 'Research';
 
 WC_Challenge::installChallenge($title, $solution, $score, $url, $creators, $tags, true);
---
+++ www/challenge/subversive/history/lang/chall_de.php
@@ -0,0 +1,15 @@
+<?php
+$lang = array(
+	'title' => 'Die Vergangenheit Wiederholt Sich',
+	'info' =>
+		'In dieser Aufgabe geht es darum sich den Aufbau der <a href="%s">WeChall Challenges</a> und der <a href="%s">GWF3 Codebase</a> genauer anzusehen.<br/>'.PHP_EOL.
+		'Ich habe zwei Passwörter in dieser Aufgaber versteckt ...<br/>'.PHP_EOL.
+		'<b>aber wo zum Geier sind die?!</b><br/>'.PHP_EOL.
+		'Das erste Passwort ist offensichtlich, das zweite etwas subversiv. Beide sind nah. Kombiniere beide ohne Trennzeichen.<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Viel Erfolg bei Deinen Nachforschungen!<br/>'.PHP_EOL.
+		'space und gizmore<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Gruß an <a href="%s">kwisatz</a>. Danke für Deine Motivation :)'.PHP_EOL,
+);
+?>
