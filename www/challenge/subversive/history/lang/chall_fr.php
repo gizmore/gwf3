c0091e289d542ee13352bd57d30682caad55b781
Gizmore <gizmore@wechall.net>
2012-03-13 15:54:44 +0000

    WC5: A little typo :( ... sry for flood.

--- www/challenge/subversive/history/lang/chall_fr.php
+++ www/challenge/subversive/history/lang/chall_fr.php
@@ -4,7 +4,7 @@ $lang = array(
 	'info' =>
 		'L\'objectif de cette épreuve est de rechercher parmi les <a href="%s">épreuves de WeChall</a> et la <a href="%s">GWF3 codebase</a>.<br/>'.PHP_EOL.
 		'J\'ai mis 2 mots de passe quelque part, mais ...<br/>'.PHP_EOL.
-		'<b>où bon dieu les ai-je mis?!<b><br/>'.PHP_EOL.
+		'<b>où bon dieu les ai-je mis?!</b><br/>'.PHP_EOL.
 		'Le premier mot de passe est évident et le second est très subversif. Les deux sont proches. Combinez-les sans séparateurs.<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
 		'Bonne chance!<br/>'.PHP_EOL.
