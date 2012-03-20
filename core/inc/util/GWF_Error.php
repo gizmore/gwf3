f4d4a693d9d7c21ceb6b6e1b94f968d021603c7b
spaceone <space@wechall.net>
2012-03-20 19:43:28 +0000

    GWF3: enhancements for GWF_Error, added CSS

--- core/inc/util/GWF_Error.php
+++ core/inc/util/GWF_Error.php
@@ -5,10 +5,12 @@
  */
 final class GWF_Error
 {
-	private static $_errors = array();
-	private static $_messages = array();
-	private static $_criticals = array();
-	private static $_warnings = array();
+	private static $_all = array(
+		'messages' => array(),
+		'criticals' => array(),
+		'errors' => array(),
+		'warnings' => array(),
+	);
 
 	public static function decode($s) { return htmlspecialchars_decode($s, ENT_QUOTES); }
 
@@ -16,10 +18,10 @@ final class GWF_Error
 	 * shortpath and language
 	 **/
 	public static function err($key, $args=NULL) { self::error('GWF', GWF_Debug::shortpath(GWF_HTML::lang($key, $args))); }
-	public static function error($title, $messages) { self::add(self::$_errors, $title, $messages); self::log_error($messages); }
-	public static function message($title, $messages) { self::add(self::$_messages, $title, $messages); self::log_message($messages); }
-	public static function warn($title, $messages) { self::add(self::$_criticals, $title, $messages); self::log_warning($messages); }
-	public static function critical($title, $messages) { self::add(self::$_warnings, $title, $messages); self::log_critical($messages); }
+	public static function error($title, $messages) { self::add('errors', $title, $messages); self::log_error($messages); }
+	public static function message($title, $messages) { self::add('messages', $title, $messages); self::log_message($messages); }
+	public static function warn($title, $messages) { self::add('criticals', $title, $messages); self::log_warning($messages); }
+	public static function critical($title, $messages) { self::add('warnings', $title, $messages); self::log_critical($messages); }
 
 	public static function log($s) { return self::decode(implode("\n", (array)$s)); }
 	public static function log_error($content) { GWF_Log::logError(self::log($content)); }
@@ -28,82 +30,75 @@ final class GWF_Error
 	public static function log_critical($content) { GWF_Log::logCritical(self::log($content)); }
 
 	/** @deprecated */
-	public static function displayErrors()
-	{
-		$back = '';
-		$back .= self::display(self::$_criticals, 'error.tpl');
-		$back .= self::display(self::$_errors, 'error.tpl');
-		$back .= self::display(self::$_warnings, 'error.tpl');
-		return $back;
-	}
+//	public static function displayErrors()
+//	{
+//		$back = '';
+//		$back .= self::display('criticals', 'error.tpl');
+//		$back .= self::display('errors', 'error.tpl');
+//		$back .= self::display('warnings', 'error.tpl');
+//		return $back;
+//	}
 
 	/** @deprecated */
-	public static function displayMessages()
-	{
-		return self::display(self::$_messages, 'message.tpl');
-	}
+//	public static function displayMessages()
+//	{
+//		return self::display('messages', 'message.tpl');
+//	}
 
 	/**
+	 * @param string $type criticals|errors|warnings|messages
 	 * @param string $title
 	 * @param string|array $messages
-	 * @todo Log via string?
 	 */
-	private static function add(&$subject, $title, $messages)
+	private static function add($type, $title, $messages)
 	{
 		$messages = (array) $messages;
 
 		if (0 === count($messages))
 			return;
 
-		if (true === isset($subject[$title]))
-			$subject[$title] = array_merge($subject[$title], $messages);
+		if (true === isset(self::$_all[$type][$title]))
+			self::$_all[$type][$title] = array_merge(self::$_all[$type][$title], $messages);
 		else
-			$subject[$title] = $messages;
+			self::$_all[$type][$title] = $messages;
 	}
 
 	/** @deprecated */
-	private static function display(&$subject, $tpl)
-	{
-		if (true === empty($subject))
-		{
-			return '';
-		}
-		if (true === isset($_GET['ajax']))
-		{
-			return self::displayAjax($subject);
-		}
+//	private static function display($type, $tpl)
+//	{
+//		if (true === empty(self::$_all[$type]))
+//		{
+//			return '';
+//		}
+//		if (true === isset($_GET['ajax']))
+//		{
+//			return self::displayAjax(self::$_all[$type]);
+//		}
 
-		return GWF_Template::templateMain($tpl, array('messages' => $subject));
-	}
+//		return GWF_Template::templateMain($tpl, array('messages' => self::$_all[$type]));
+//	}
 
 	public static function displayAll()
 	{
-		$all = array(
-			'messages' => self::$_messages,
-			'criticals' => self::$_criticals,
-			'errors' => self::$_errors,
-			'warnings' => self::$_warnings,
-		);
-
-		foreach ($all as $k => $subject)
+		foreach (self::$_all as $k => $subject)
 		{
 			if (true === empty($subject))
 			{
-				unset($all[$k]);
+				unset(self::$_all[$k]);
 			}
 		}
 
 		if (true === isset($_GET['ajax']))
 		{
 			$back = '';
-			foreach ($all as $subject)
+			foreach (self::$_all as $subject)
 			{
 				$back .= self::displayAjax($subject);
 			}
 			return $back;
 		}
 
-		return GWF_Template::templateMain('errors.tpl', array('messages' => $all));
+		return GWF_Template::templateMain('errors.tpl', array('messages' => self::$_all));
 	}
 
 	private static function displayAjax(&$subject)
