d8f5770a4346384ed906cc2187dd223646c3397b
Gizmore <gizmore@wechall.net>
2012-03-24 00:29:13 +0000

    WC5: New challenge: Choose your Path II (thx Mart)

---
+++ www/challenge/warchall/choose_your_path2/lang/chall_en.php
@@ -0,0 +1,10 @@
+<?php
+$lang = array(
+	'title' => 'Choose your Path II',
+	'info' =>
+		'This is the level11 mini challenge found in %s on the <a href="%s">warchall box</a>.<br/>'.PHP_EOL.
+		'The sequel to Choose your Path.<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Proudly presented by Mart and the warchall staff.'.PHP_EOL,
+);
+?>
