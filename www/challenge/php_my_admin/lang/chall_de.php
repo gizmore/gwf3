b8cb2c7bdf179977d1d6bf27a38bd45b0eb71d26
Gizmore <gizmore@wechall.net>
2012-04-01 16:55:55 +0000

    WC5: Fixed a tiny typo in PMA challenge. (thx livinskull)

--- www/challenge/php_my_admin/lang/chall_de.php
+++ www/challenge/php_my_admin/lang/chall_de.php
@@ -2,7 +2,7 @@
 $lang = array(
 	'title' => 'PHP Mein Verwalter',
 	'info' =>
-		'Deine Aufgaber hier ist leicht erklärt:<br/>'.PHP_EOL.
+		'Deine Aufgabe hier ist leicht erklärt:<br/>'.PHP_EOL.
 		'Du sollst die &quot;PHPMyAdmin&quot; Webseite aufspüren die von den Admins benutzt wird um die Datenbank zu verwalten.<br/>'.PHP_EOL.
 		'<p style="color:#fefefe">Du brauchst doch wohl keinen Hinweis, oder?</p>'.PHP_EOL.
 		'Wir hoffen auf Deinen Erfolg.<br/>'.PHP_EOL.
