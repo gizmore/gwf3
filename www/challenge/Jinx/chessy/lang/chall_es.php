2752f92c4f40c27d67619d81b90411f2b6b1dff8
paipai <paipai@wechall.net>
2012-04-04 15:42:41 +0000

    chessy hawks challenge translation to spanish.

---
+++ www/challenge/Jinx/chessy/lang/chall_es.php
@@ -0,0 +1,14 @@
+<?php
+$lang = array(
+	'title' => 'Chessy Hawks',
+	'info' =>
+		'Hoy te ha sucedido algo extraño.<br/>'.PHP_EOL.
+		'Estabas dando un paseo por el parque y encontraste una memoria USB sobre un tablero de ajedrez, junto con unos extraños números en los cuadros.<br/>'.PHP_EOL.
+		'Al instante te preguntaste si estaban relacionados entre sí, y te llevaste la memoria USB y un croquis del tablero a casa.<br/>'.PHP_EOL.
+		'Tus pensamientos eran correctos. La memoria USB parece estar encriptada y el tablero de ajedrez probablemente te mostrará la contraseña:<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'<img src="%s" title="Tú croquis del tablero" alt="Reto Tablero" /><br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'¡Disfruta!',
+);
+?>
