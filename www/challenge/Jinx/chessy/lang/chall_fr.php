01aafe5c38666066bc44b1cd9d406a5c777bd77d
Gizmore <gizmore@wechall.net>
2012-04-04 15:36:28 +0000

    WC5: French translation for Chessy challenge. (thx Overclok)

---
+++ www/challenge/Jinx/chessy/lang/chall_fr.php
@@ -0,0 +1,14 @@
+<?php # By Overclok
+$lang = array(
+	'title' => 'Chessy Hawks',
+	'info' =>
+	'Aujourd\'hui, quelque chose de bizarre est arrivé.<br/>'.PHP_EOL.
+	'Vous preniez l\'air dans le parc, quand soudainement vous avez trouvé une clé USB posé sur un jeu d’échecs avec des numéros bizarres.<br/>'.PHP_EOL.
+	'Vous vous êtes instantanément demandé si les objets étaient liés entres-eux, puis vous les avez rapportés chez vous.<br/>'.PHP_EOL.
+	'Vous aviez raison. La clé USB semble être chiffrée et le damier doit probablement détenir le mot de passe:<br/>'.PHP_EOL.
+	'<br/>'.PHP_EOL.
+	'<img src="%s" title="L\'esquisse du jeu d\'échecs" alt="L\'énigme du jeu d\'échecs" /><br/>'.PHP_EOL.
+	'<br/>'.PHP_EOL.
+	'Amusez-vous!',
+);
+?>
