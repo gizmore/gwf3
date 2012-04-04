09532b907163964ce636f9a08bb7409d52712b75
paipai <paipai@wechall.net>
2012-04-04 15:59:48 +0000

    burning fox challenge translation to spanish

---
+++ www/challenge/burning_fox/lang/chall_es.php
@@ -0,0 +1,18 @@
+﻿<?php
+$lang = array(
+	'title' => 'Zorro Ardiente',
+	'info' =>
+		'Nuestra agencia de seguridad nos ha entregado una copia de una instalación portable de Firefox.<br/>'.
+		'Tu misión es romper la contraseña maestra, para que las autoridadees puedan investigar los sitios web y el contenido generado por esta persona.<br/>'.
+		'Puedes descargar la versión portable desde esta carpeta, nombre de archivo &quot;burningfox.zip&quot;.<br/>'.
+		'<br/>'.
+		'¡Buena Suerte!',
+		
+	'disc_title' => 'Renuncia',
+	'disclaimer' =>
+		'Firefox es una marca de Mozilla y &quot;Open Source&quot;<br/>'.PHP_EOL.
+		'Tienes permitido distribuir la fuente pero no la marca.<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Mozilla Firefox es usado por gizmore diariamente desde 2003, y de momento no existe un navegador mejor, según él.',
+);
+?>
