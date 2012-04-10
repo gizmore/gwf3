2a7409ca2cd7a8b53c118e4a685b707b560f30f1
spaceone <space@wechall.net>
2012-04-10 20:17:09 +0000

    WC5: enhanced the new challenge: removed /proc possibility (thx kwisatz, livinskull)

--- www/challenge/space/foo/foo.py
+++ www/challenge/space/foo/foo.py
@@ -5,29 +5,38 @@ import os
 
 # Your main objective is to return 0
 def main(argv):
-	if len(argv) < 2 or not os.path.exists(argv[1]):
+	if not os.path.exists(argv):
 		return 1
 
-	print('opening '+argv[1])
-	gizmore = open(argv[1])
+	print('opening '+argv)
+	gizmore = open(argv)
 	jjk = ''.join(gizmore.readlines())
 	gizmore.close()
 	print('closed')
 
-	if not os.path.exists(argv[1]):
+	if not os.path.exists(argv):
 		print('You are l33t')
 		return 0
 	else:
-		spaceone = open(argv[1])
+		spaceone = open(argv)
 		kwisatz = ''.join(spaceone.readlines())
+		spaceone.close()
 		if jjk != kwisatz:
 			print('You are a winner')
 			return 0
 	return 1
 
+def sanitize_arg(value):
+	# no symlinks, etc. please
+	value = os.path.realpath(value)
+	while -1 != value.find('proc'):
+		value = value.replace('proc', '')
+
 if __name__ == "__main__":
+	rc = 1
 	try:
-		rc = main(sys.argv)
+		if len(sys.argv[1]) > 1:
+			rc = main(sanitize_arg(sys.argv[1]))
 	except:
 		rc = 1
 	sys.exit(rc)
