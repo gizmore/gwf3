bd6b65784952d05d09d732bf72a948c83be3fe69
paipai <paipai@wechall.net>
2012-04-12 18:48:03 +0000

    py-tong challenge spanish translation

---
+++ www/challenge/space/pytong/lang/chall_es.php
@@ -0,0 +1,13 @@
+﻿<?php
+$lang = array(
+	'title' => 'Py-Tong',
+	'info' =>
+		'Un amigo hacker esta retando a explotar un simple script de python.<br/>'.PHP_EOL.
+		'Para explotarlo en vivo, tienes que crear tu <a href="%s">cuenta de Warchall</a> y probar tu suerte allí.<br/>'.PHP_EOL.
+		'Para ver de que se trata puedes <a href="%s">ver el código fuente aquí</a><br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Nota: No se requiere hacer una condición de carrera, pero debería funcionar así.<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'¡Buena Suerte!'.PHP_EOL,
+);
+?>
