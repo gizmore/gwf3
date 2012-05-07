dd75597bfe0774b7ef655f116b6f400ef03aa953
Gizmore <gizmore@wechall.net>
2012-05-07 19:32:48 +0000

    WC5: French translation of Tryouts challenge. (thx mirmo) (thx ThunderLord)

---
+++ www/challenge/warchall/tryouts/lang/chall_fr.php
@@ -0,0 +1,11 @@
+<?php # by mirmo and ThunderLord
+$lang = array(
+	'title' => 'Remuer Que',
+	'info' =>
+		'%s, le vétéran des sites de challenge vous met au défi d\'exploiter son premier challenge warchall que vous pourrez trouver dans %s sur la box.'.PHP_EOL.
+		'Bien sûr, vous devez créer votre <a href="%s">compte warchall</a> si vous ne l\'avait pas déjà fait.<br/>'.PHP_EOL.
+		'N\'essayez pas des choses au hasard sur votre cible parce qu\'il est assez simple.<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Bon challenge!'.PHP_EOL,
+);
+?>
