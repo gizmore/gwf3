956a66c12a5cc025c30870c7b322e739624f9214
Gizmore <gizmore@wechall.net>
2012-05-07 19:07:38 +0000

    WC5: German translation of Tryouts challenge.

---
+++ www/challenge/warchall/tryouts/lang/chall_de.php
@@ -0,0 +1,11 @@
+<?php
+$lang = array(
+	'title' => 'Die Versuchung',
+	'info' =>
+		'Der Challenge Seiten Veteran %s fordert Dich heraus seine erste Warchall Challenge zu lösen welche du in %s auf der Box finden kannst.<br/>'.PHP_EOL.
+		'Natürlich brauchst du einen <a href="%s">Warchall Account</a> falls Du noch keinen hast.<br/>'.PHP_EOL.
+		'Versuch\' keine zufälligen Dinge. Es ist doch sehr offensichtlich!<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Viel Spaß!'.PHP_EOL,
+);
+?>
--- www/challenge/warchall/tryouts/lang/chall_en.php
+++ www/challenge/warchall/tryouts/lang/chall_en.php
@@ -4,7 +4,7 @@ $lang = array(
 	'info' =>
 		'The challenge site veteran %s is challenging you to exploit his first warchall challenge which you can find in %s on the box.<br/>'.PHP_EOL.
 		'Of course you need to create your <a href="%s">warchall account</a> if you haven\'t done so already.<br/>'.PHP_EOL.
-		'Don\'t get randomly lost in your tries, as it\'s pretty straight forward.<br/>'.PHP_EOL.
+		'Don\'t try random things on your target as it\'s pretty straight forward.<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
 		'Happy Challenging!'.PHP_EOL,
 );
