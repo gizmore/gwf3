eea25390d0a96e97c100e682255559747c20ecaa
livinskull <livinskull@wechall.net>
2012-05-24 22:15:50 +0000

    Shadowlamb: TrollHQ 2 shamane typo fix

--- core/module/Lamb/lamb_module/Shadowlamb/city/TrollHQ2/lang/location/shamane/shamane_en.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/TrollHQ2/lang/location/shamane/shamane_en.php
@@ -1,6 +1,6 @@
 <?php
 $lang = array(
 	'found' => 'You found the residence of the TrollHQ shamane, as proposed by the sign "Shamanee".',
-	'enter' => 'You enter a quite cleaned room, full off weird accessiors and magic utilities.',
+	'enter' => 'You enter a quite cleaned room, full off weird accessoires and magic utilities.',
 );
 ?>
