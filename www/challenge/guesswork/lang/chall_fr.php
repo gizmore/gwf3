b0bab6789eed0df04b4c09f30eb820c6f4ce57c6
Gizmore <gizmore@wechall.net>
2012-05-30 19:11:54 +0000

    WC5: Language fixes.

--- www/challenge/guesswork/lang/chall_fr.php
+++ www/challenge/guesswork/lang/chall_fr.php
@@ -1,11 +1,11 @@
-﻿<?php
+﻿﻿<?php
 $lang = array(
 	'msg_title' => 'Compte Craqué',
 	'msg_wechall' => 
 		'Je pense que certaines personnes ont craqué mon compte wechall.'.PHP_EOL.
 		'Pensez-vous réellement que vos scripts sont sûrs ?',
 	'msg_gizmore' =>
-		'Eh bien, peut-être que votre mot de passe a été très facile à deviner ou que vous le réutilisez d'un autre site ?'.PHP_EOL.
+		'Eh bien, peut-être que votre mot de passe a été très facile à deviner ou que vous le réutilisez d\'un autre site ?'.PHP_EOL.
 		'Ne choisissez pas des mots de passe qui sont affiliés au site et à votre personne etc ...'.PHP_EOL.
 		'Vous devriez peut-être réinitialiser votre mot de passe ?'.PHP_EOL.
 		PHP_EOL.
@@ -18,8 +18,8 @@ $lang = array(
 	'err_wrong' => 'Le couple user/password est inconnu.',
 	'msg_congrats' =>
 		'Bien joué. Vous avez maintenant le contrôle sur le compte WeChall.<br/>'.
-		'N'en abusez pas trop, cela dit, vous pouvez vous amuser sur les forums ou via la shoutbox.<br/>'.
+		'N\'en abusez pas trop, cela dit, vous pouvez vous amuser sur les forums ou via la shoutbox.<br/>'.
 		'WeChall est un utilisateur régulier, les PM ne sont pas autorisés.<br/>',
 		'Amusez vous bien, bons challenges et :&nbsp;<span style="color:red;">pas d\'abus!</span><br/>'.
-		'Si vous trouvez qu'il y a moyen d'en abuser, faites-nous en part!',
+		'Si vous trouvez qu\'il y a moyen d \'en abuser, faites-nous en part!',
 );
--- www/challenge/training/www/robots/lang/chall_fr.php
+++ www/challenge/training/www/robots/lang/chall_fr.php
@@ -2,9 +2,9 @@
 $lang = array(
 	'title' => 'WWW-Robots',
 	'info' =>
-		'Dans de petit challenge d'entraînement, vous allez vous familiariser sur le <br/><a href="http://fr.wikipedia.org/wiki/Protocole_d%27exclusion_des_robots">protocole d\'exclusion des robots</a>.<br/>'.
-		'Le fichier robots.txt est utilisé par les robots d'indexation pour verifier s\'ils sont autorisés d'explorer et d\'indexer votre site internet ou seulement une partie de celui-ci.<br/>'.
-		'Quelquefois, ces fichiers révèlent la structure des dossiers au lieu de protéger leur contenu contre l'exploration.<br/><br/>'.
+		'Dans de petit challenge d\'entraînement, vous allez vous familiariser sur le <br/><a href="http://fr.wikipedia.org/wiki/Protocole_d%27exclusion_des_robots">protocole d\'exclusion des robots</a>.<br/>'.
+		'Le fichier robots.txt est utilisé par les robots d\'indexation pour verifier s\'ils sont autorisés d\'explorer et d\'indexer votre site internet ou seulement une partie de celui-ci.<br/>'.
+		'Quelquefois, ces fichiers révèlent la structure des dossiers au lieu de protéger leur contenu contre l\'exploration.<br/><br/>'.
 		'Enjoy!',
 );
 ?>
