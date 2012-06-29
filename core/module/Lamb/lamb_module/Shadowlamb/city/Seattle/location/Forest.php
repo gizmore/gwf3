a4570dfb6699ce52e236e0b5003f3773bbf00463
Gizmore <gizmore@wechall.net>
2012-06-29 16:16:23 +0000

    Shadowlamb: The new Forest area will be unlocked on next restart.

--- core/module/Lamb/lamb_module/Shadowlamb/city/Seattle/location/Forest.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Seattle/location/Forest.php
@@ -1,6 +1,7 @@
 <?php
 final class Seattle_Forest extends SR_Tower
 {
+	public function getFoundPercentage() { return 15.0; }
 	public function getFoundText(SR_Player $player) { return $this->lang($player, 'found'); }
 	public function onEnter(SR_Player $player)
 	{
