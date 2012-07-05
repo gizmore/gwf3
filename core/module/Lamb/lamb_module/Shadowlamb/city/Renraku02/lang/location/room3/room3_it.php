296554d17d276020def12a1b12ad691614f19d09
monnino <monnino@wechall.net>
2012-07-05 21:08:45 +0000

    Shadowlamb: Renraku2 italian translation files.

---
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Renraku02/lang/location/room3/room3_it.php
@@ -0,0 +1,6 @@
+<?php
+$lang = array(
+	'found' => 'Hai trovato una stanza con la targa "0203". Sembra chiusa a chiave.',
+	'enter' => 'Entri nella stanza 0203. Ci sono solo delle scrivanie vuote e qualche cassa qui.',
+);
+?>
