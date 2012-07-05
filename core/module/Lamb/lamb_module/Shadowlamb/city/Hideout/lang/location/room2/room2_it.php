319b41549ea196fd40298d3d6e707bb270d86c90
monnino <monnino@wechall.net>
2012-07-05 22:38:05 +0000

    Shadowlamb: Hideout and OrkHQ italian translation.

---
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Hideout/lang/location/room2/room2_it.php
@@ -0,0 +1,8 @@
+<?php
+$lang = array(
+	'found' => 'Trovi un\'altra stanza. Sembra tutto tranquillo all\'interno.',
+	'enter' => 'Vedi tre Lamer che dormono.',
+	'wakeup' => 'Svegli i tre lamer malamente... Quando realizzano chi sei, ti attaccano.',
+	'no_wake' => 'Non ci sono lamer da svegliare.',
+);
+?>
