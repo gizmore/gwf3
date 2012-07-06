fc246ff8546d47cf78424c376411513746ce1034
monnino <monnino@wechall.net>
2012-07-06 17:06:58 +0000

    Shadowlamb: fix to file. Wonder how that ended up there.

--- core/module/Lamb/lamb_module/Shadowlamb/city/TrollHQ/lang/location/kitchen/kitchen_it.php
+++ core/module/Lamb/lamb_module/Shadowlamb/city/TrollHQ/lang/location/kitchen/kitchen_it.php
@@ -1,4 +1,4 @@
-exit_en.php<?php
+<?php
 $lang = array(
 	'found' => 'Hai trovato una stanza con una porta dorata. Probabilmente la cucina.',
 	'enter' => 'Entri nella cucina, una stanza piena di odori pungenti e... un cuoco!',
