c41453ea73a623de5895879092ccb0b4ce350cb2
monnino <monnino@wechall.net>
2012-07-06 16:20:58 +0000

    Shadowlamb: Harbor italian translation.

---
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Harbor/lang/location/depot1/depot1_it.php
@@ -0,0 +1,8 @@
+<?php
+$lang = array(
+	'found' => 'Hai trovato un grande magazzino denominato "Magazzino1".',
+	'enter' => 'Entri nel magazzino. E\' quasi vuoto, tranne che per qualche cianfrusaglia stipata in vecchie casse.',
+
+	'suprise' => 'Due guardie del deposito ti sorprendono ed attaccano.',
+);
+?>
---
+++ core/module/Lamb/lamb_module/Shadowlamb/city/Harbor/lang/location/ship1/ship1_it.php
@@ -0,0 +1,7 @@
+<?php
+$lang = array(
+	'found' => 'Trovi una nave chiamata "Saint Marry". "Nome da idioti" pensi tra te e te.',
+	'enter' => 'Sali sulla "Saint Marry"...',
+	'no_enter' => 'Non trovi nessun punto da cui poter salire sulla barca... Non ancora.',
+);
+?>
