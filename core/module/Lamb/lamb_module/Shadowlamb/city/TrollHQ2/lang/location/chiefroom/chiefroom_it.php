ff910d0823c023046ab4f2732136afc68e72925d
monnino <monnino@wechall.net>
2012-07-06 19:05:34 +0000

    Shadowlamb: TrollHQ2 italian translation

---
+++ core/module/Lamb/lamb_module/Shadowlamb/city/TrollHQ2/lang/location/chiefroom/chiefroom_it.php
@@ -0,0 +1,6 @@
+<?php
+$lang = array(
+	'found' => 'Hai trovato una stanza con un cartello che recita "Residenza di Larry". Sembra sia la stanza del capo.',
+	'enter' => 'Entri nella stanza e individue alcuni Orchi e qualche Troll che si stanno radunando attorno ad un grosso Troll che sembra il Leader del gruppo.',
+);
+?>
