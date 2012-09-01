2c87d1bb33e5f8b43b3be89797467859a5111e7c
Gizmore <gizmore@wechall.net>
2012-09-01 20:14:03 +0000

    Shadowlamb: Added a new vegetable for mel. (thx mel)

---
+++ core/module/Lamb/lamb_module/Shadowlamb/item/vegetable/Dildo.php
@@ -0,0 +1,10 @@
+<?php
+final class Item_Dildo extends SR_Item
+{
+	public function getItemLevel() { return -1; }
+	public function getItemPrice() { return 95.99; }
+	public function getItemWeight() { return 300; }
+	public function getItemDescription() { return "A large black dildo with pearly burls. 20cm long and 7cm thick makes it perfect to please the fem when she demands."; }
+	public function isItemLootable() { return false; }
+}
+?>
