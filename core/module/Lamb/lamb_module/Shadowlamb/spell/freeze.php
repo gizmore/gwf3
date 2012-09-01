e7331d7a5c842dcb671779a1b4a912143c7d6c8d
Gizmore <gizmore@wechall.net>
2012-09-01 16:12:09 +0000

    Shadowlamb: The freeze spell was not working at all. (thx noother)

--- core/module/Lamb/lamb_module/Shadowlamb/spell/freeze.php
+++ core/module/Lamb/lamb_module/Shadowlamb/spell/freeze.php
@@ -2,7 +2,6 @@
 /**
  * The freeze spell will make a target busy.
  * @author gizmore
- *
  */
 final class Spell_freeze extends SR_OffensiveSpell
 {
@@ -12,22 +11,8 @@ final class Spell_freeze extends SR_OffensiveSpell
 	public function getCastTime($level) { return Common::clamp(20-$level, 10, 40); }
 	public function getManaCost(SR_Player $player, $level)
 	{
-//		$level = $this->getLevel($player);
 		return 6 + $level;
 	}
-
-
-//	public function cast(SR_Player $player, SR_Player $target, $level, $hits, SR_Player $potion_player)
-//	{
-//		$wis = $player->get('wisdom') * 30;
-//		$min = 20+$level*10;
-//		$max = 40+$level*20+$wis;
-//		$seconds = rand($min, $max);
-//		$target->busy($seconds);
-//		$append = $append_ep = sprintf('%s seconds frozen.', $seconds);
-//		$this->announceADV($player, $target, $level, $append, $append_ep);
-//		return true;
-//	}
 
 	public function cast(SR_Player $player, SR_Player $target, $level, $hits, SR_Player $potion_player)
 	{
@@ -35,15 +20,11 @@ final class Spell_freeze extends SR_OffensiveSpell
 		$min = 20+$level*10;
 		$max = 40+$level*20+$wis;
 		$seconds = rand($min, $max);
-		$ef = Shadowfunc::diceFloat(0.0, $level, 1) + 1.0;
-		$target->busy($ef*10);
-		$effect = new SR_Effect($seconds, array('frozen'=>$ef), SR_Effect::MODE_ONCE_EXTEND);
-		$target->addEffects($effect);
-// 		$append = $append_ep = sprintf('%s seconds frozen with power %01f.', $seconds, $ef);
-// 		$this->announceADV($player, $target, $level, $append, $append_ep);
+		$target->busy($seconds*10);
+		$ef = Shadowfunc::diceFloat(0.1, $level, 1);
+		$target->addEffects(new SR_Effect($seconds, array('frozen'=>$ef), SR_Effect::MODE_ONCE_EXTEND));
 		$this->announceADV($player, $target, $level, '10050', $seconds, $ef);
 		return true;
 	}
 }
-
 ?>
