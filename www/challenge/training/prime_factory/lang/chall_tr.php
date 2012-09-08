b1cec20af3cfb606c0b9b16b14a2326f95ae1edc
Gizmore <gizmore@wechall.net>
2012-09-08 22:42:26 +0000

    WC5: Turkish translation of two challenges. (thx D ! G ! T A L)

---
+++ www/challenge/training/prime_factory/lang/chall_tr.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang = array(
+'info' => '
+Bu görev basittir:<br/>
+1 milyondan fazla ilk iki asal bul, rakamları toplamı da bir asal sayıdır.<br/>
+Örnek: 23 bir asal sayıdır ve toplam da ayrıca bir asal sayı 5\'tir.<br/>
+Çözelti, iki sayı oluşmaktadır.<br/>
+Örnek: birinci sayı 1.234.567 ise,<br/> 
+ve ikinci sayı olduğunu 8.765.432,<br/>
+çözelti12345678765432<br/>
+',
+);
+
+?>
---
+++ www/challenge/training/www/basic/lang/chall_tr.php
@@ -0,0 +1,23 @@
+<?php
+$lang = array(
+'title' => 'Eğitim: WWW-Temelleri',
+'info' =>
+'Bildiğiniz gibi &quot;World Wide Web&quot; İnternetin popüler parçası olduğunu.<br/>'.
+'Www HTML belgeleri veya başka bir içerik sunmak HTTP sunucuları, oluşur.<br/>'.
+'Burada Senin görevin basittir. HTTP sunucusu üzerinde yer alan internet onu erişilebilir yapmak ve bu dosyayı teslim:% 1 $ s.<br/>'.
+'Bu dosyanın içeriği tam olmalıdır:<br/>'.
+'%2$s<br/>'.
+'Dosyası tam olmalıdır %3$s Olmak üzere boyut olarak bayt.<br/>'.
+'<br/>'.
+'<br/>'.
+'Bu sunucuyu kurduktan sonra , bir bağlantı noktasını seçin, genellikle 80 ve düğmesine basabilirsiniz<br/>'.
+'iyi Eğlenin!',
+
+'content' => 'Benim adım %s ve ben doğrayın.',
+
+'btn_go' =>'Ben hazırım. Benim sunucu kontrol edin .',
+'err_file_not_found' => 'Sizin web sunucusu görünüşte Internet\'ten erişilebilir değil.',
+'err_wrong' => 'Dosyanızın içeriği (%3$s bytes)<br/>%1$s<br/>',
+'İstediğiniz saparsa (%4$s bytes)<br/>%2$s<br/>Lütfen düzeltin!',
+);
+?>
