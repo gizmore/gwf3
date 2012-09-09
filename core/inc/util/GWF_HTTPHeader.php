ace92862c987901fa66c567f9d71b0eb43f90066
spaceone <space@wechall.net>
2012-09-09 11:02:52 +0000

    GWF3: adapted GWF_HTTPHeader to be CLI compatible

--- core/inc/util/GWF_HTTPHeader.php
+++ core/inc/util/GWF_HTTPHeader.php
@@ -1,5 +1,11 @@
 <?php
 
+if(!isset($_HEADER))
+{
+	$_HEADER = array();
+	global $_HEADER;
+}
+
 if (false === function_exists('http_response_code'))
 {
 	function http_response_code($code)
@@ -26,6 +32,14 @@ final class GWF_HTTPHeader
 		if (!isset(self::$HEADERS))
 		{
 			self::$HEADERS = headers_list();
+			if(PHP_SAPI === 'cli')
+			{
+				global $_HEADER;
+				if(is_array($_HEADER))
+				{
+					self::$HEADERS = $_HEADER;
+				}
+			}
 			array_map('strtolower', self::$HEADERS);
 		}
 		return self::$HEADERS;
@@ -46,23 +60,77 @@ final class GWF_HTTPHeader
 	 */
 	public static function setHeaders($headers)
 	{
-		foreach ((array) $headers as $key => $val)
+		$ret = true;
+		foreach ($headers as $key => $val)
 		{
-			# TODO: key/value dict?
-			header($val);
+			$header = array($key, $val);
+			if (is_numeric($key))
+			{
+				$header = $val;
+			}
+			$ret = $ret && self::setHeader($header);
 		}
+		return $ret;
+	}
+
+	/*
+	 *
+	 * @param string|array $header
+	 */
+	public static function setHeader($header, $replace=true)
+	{
+		if (!is_array($header)) {
+			$header = array_map('trim', explode(':', $header, 2));
+		}
+		list($key, $val) = $header;
+
+		if(!headers_sent() || PHP_SAPI === 'cli')
+		{
+			global $_HEADER;
+			if (!$replace && isset($_HEADER[$key]))
+			{
+				if ($_HEADER[$key] !== $val)
+				{
+					$_HEADER[$key] = array($_HEADER[$key], $val);
+				}
+			}
+			else
+			{
+				$_HEADER[$key] = $val;
+			}
+		}
+		if (!headers_sent())
+		{
+			header(sprintf('%s: %s', $key, $val), $replace);
+			return true;
+		}
+		return PHP_SAPI === 'cli';
 	}
  
 	/**
-	 * remove response header
+	 * remove response headers
 	 *
 	 */
 	public static function removeHeaders($headers)
 	{
+		$ret = true;
 		foreach ((array) $headers as $header)
 		{
-			header_remove($header);
+			$ret = $ret && self::removeHeader($header);
 		}
+		return $ret;
+	}
+
+	public static function removeHeader($header)
+	{
+		global $_HEADER;
+		unset($_HEADER[$header]);
+		if (!headers_sent())
+		{
+			header_remove($header);
+			return true;
+		}
+		return PHP_SAPI === 'cli';
 	}
 
 	/**
