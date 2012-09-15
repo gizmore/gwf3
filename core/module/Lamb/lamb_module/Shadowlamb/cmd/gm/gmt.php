47b57b37263c37207fa71495cfdc761883c7fc55
Gizmore <gizmore@wechall.net>
2012-09-15 04:30:49 +0000

    Shadowlamb: New command #gmloot to try out loot generators. Changed #gmt to always work and make you encounter enemies at will >;-|

--- core/module/Lamb/lamb_module/Shadowlamb/cmd/gm/gmt.php
+++ core/module/Lamb/lamb_module/Shadowlamb/cmd/gm/gmt.php
@@ -25,20 +25,74 @@ final class Shadowcmd_gmt extends Shadowcmd
 			$bot->reply(sprintf('The player %s has not started a game yet.', $args[0]));
 			return false;
 		}
-
+		
+		
+		$npcnames = explode(',', $args[1]);
+		foreach ($npcnames as $i => $npcname)
+		{
+			if (false === ($npc = Shadowrun4::getNPCByAbbrev($player, $npcname)))
+			{
+				$bot->reply(sprintf('Unknown NPC: %s.', $npcname));
+				return false;
+			}
+			$npcnames[$i] = $npc->getNPCClassName();
+		}
+		
+		
 		$p = $target->getParty();
 		$a = $p->getAction();
-		if ($a !== SR_Party::ACTION_INSIDE && $a !== SR_Party::ACTION_OUTSIDE) {
-			$bot->reply('The party with '.$args[0].' is moving.');
-			return false;
+		$ep = NULL;
+		
+		switch ($a)
+		{
+			case SR_Party::ACTION_HIJACK:
+			case SR_Party::ACTION_TRAVEL:
+			case SR_Party::ACTION_DELETE:
+				$bot->reply('Cannot #gmt because target action is '.$a.'!');
+				return false;
+			
+			case SR_Party::ACTION_TALK:
+				$p->getEnemyParty()->popAction(true);
+			case SR_Party::ACTION_SLEEP:
+				$p->popAction(false);
+				break;
+
+			case SR_Party::ACTION_FIGHT:
+				$ep = $p->getEnemyParty();
+				break;
+				
+			case SR_Party::ACTION_INSIDE:
+			case SR_Party::ACTION_OUTSIDE:
+			case SR_Party::ACTION_EXPLORE:
+			case SR_Party::ACTION_GOTO:
+			case SR_Party::ACTION_HUNT:
 		}
+
+// 		if ($a !== SR_Party::ACTION_INSIDE && $a !== SR_Party::ACTION_OUTSIDE) {
+// 			$bot->reply('The party with '.$args[0].' is moving.');
+// 			return false;
+// 		}
 	
-		if (false === ($ep = SR_NPC::createEnemyParty(explode(',', $args[1])))) {
+		if (false === ($ep2 = SR_NPC::createEnemyParty($npcnames)))
+		{
 			$bot->reply('cannot create party! check logs');
 			return false;
 		}
 		
-		if ($ep->getLeader() instanceof SR_TalkingNPC)
+		if ($ep === NULL)
+		{
+			$ep = $ep2;
+		}
+		else
+		{
+			$ep->mergeParty($ep2);
+		}
+		
+		if ($p->isFighting())
+		{
+			$p->fight($ep, true);
+		}
+		elseif ($ep->getLeader() instanceof SR_TalkingNPC)
 		{
 			$p->talk($ep, true);
 		}
