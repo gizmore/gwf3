087b7c42edc0aeb5c0f53ed859f9741bbd324af8
livinskull <livinskull@wechall.net>
2012-11-01 00:17:49 +0000

    Lamb3: special age fix ;)

--- core/module/Lamb/lamb_plugin/pauth/age.php
+++ core/module/Lamb/lamb_plugin/pauth/age.php
@@ -3,7 +3,7 @@ $bot instanceof Lamb;
 $user = $bot->getCurrentUser();
 if ($user->getName() === 'Wyshfire')
 {
-	return $bot->reply('You are 21');
+	return $bot->reply('You are 20');
 }
 $bot->reply('You have no age specified. An age is required to execute this function.');
 ?>
