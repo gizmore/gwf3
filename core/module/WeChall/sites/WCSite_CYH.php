4a2e2d23f6f7e34bf72709e9cd24d62d54c9507c
Gizmore <gizmore@wechall.net>
2012-11-21 16:17:53 +0000

    WC5: new site WCSite_CYH.

---
+++ core/module/WeChall/sites/WCSite_CYH.php
@@ -0,0 +1,37 @@
+<?php
+class WCSite_CYH extends WC_Site
+{
+	public function parseStats($url)
+	{
+		if (false === ($result = GWF_HTTP::getFromURL($url, false)))
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+
+		$stats = explode(':', $result);
+		if (count($stats) != 7)
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+
+		# username:rank:score:maxscore:challssolved:challcount:usercount
+		$i = 0;
+		$username = $stats[$i++];
+		$onsitesrank = (int)$stats[$i++];
+		$onsitescore = (int)$stats[$i++];
+		$maxscore = (int)$stats[$i++];
+		$challssolved = (int)$stats[$i++];
+		$challcount = (int)$stats[$i++];
+		$usercount = (int)$stats[$i++];
+		
+		if ($maxscore === 0 || $challcount === 0 || $usercount === 0)
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+		$this->updateSite($maxscore, $usercount, $challcount);
+		
+		return array($onsitescore, $onsitesrank, $challssolved);
+	}
+}
+?>
