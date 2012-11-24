4c981116c8d6205e3be328ea5196575c1607f0e4
Gizmore <gizmore@wechall.net>
2012-11-24 00:23:43 +0000

    Dog: Commit 7/10. dog_timer.

---
+++ core/module/Dog/dog_timer/all_servers_one/timers/infinity/30/solvers_wechall.php
@@ -0,0 +1,8 @@
+<?php
+require_once DOG_PATH.'dog_lib/Dog_WC_Solvers.php';
+
+$url = 'http://www.wechall.net/index.php?mo=WeChall&me=API_ChallSolved&ajax=true&datestamp=%DATE%&no_session=true';
+$format = "%1\$s \x02%2\$s\x02 has just solved \x02%3\$s\x02. This challenge has been solved %4\$d times. (https://www.wechall.net/%5\$s)";
+
+dog_wc_solvers('WC', '[WeChall]', $url, array('#wechall'), 200, $format);
+?>
