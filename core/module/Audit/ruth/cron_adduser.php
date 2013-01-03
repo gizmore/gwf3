084f0e1e987292697e719d93ce7411b4cc5e10e1
Gizmore <gizmore@wechall.net>
2013-01-03 13:50:30 +0000

    Warchall: Underscore was not allowed in username. (thx New_Luca)

--- core/module/Audit/ruth/cron_adduser.php
+++ core/module/Audit/ruth/cron_adduser.php
@@ -30,7 +30,7 @@ while (false !== ($row = $db->fetchAssoc($result)))
 {
 	$username = $row['username'];
 	
-	if (!preg_match('/^[a-z][a-z0-9]{0,31}$/iD', $username))
+	if (!preg_match('/^[a-z][a-z0-9_]{0,31}$/iD', $username))
 	{
 		GWF_Cronjob::error('Username invalid: '.$username);
 		continue;
