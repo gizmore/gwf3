e78431986a93ddb7023f14d3e3fe54bc163241ae
livinskull <livinskull@wechall.net>
2013-01-07 19:58:35 +0000

    Warchall: improved ANSI escape sequence decoding on log playback (colors ftw!)

--- www/tpl/war/js/warchall.js
+++ www/tpl/war/js/warchall.js
@@ -87,32 +87,120 @@ function alLog(script, times, console_selector)
 	this.onAppend = function(append)
 	{
 		append = this.shellDecode(append.htmlspecialchars());
-		console.log(append);
+		//console.log(append);
 		this.e.append(append);
 		return '';
 	};
 	
 	this.shellDecode = function(s)
 	{
-		console.log(s);
-		s = s.replace(']0;', '', 'g'); // START OF LINE / LINE 0
-		s = s.replace('[?1034h', '', 'g'); // HEX?
-		s = s.replace('~', '', 'g'); // ??
-		s = s.replace('[1m', '</font>', 'g');
-//		s = s.replace('[0;32m')
-		s = s.replace('[1;30m', '<font color="gray">', 'g');
-		s = s.replace('[0;32m', '<font color="green">', 'g');
-		s = s.replace('[00;32m', '<font color="green">', 'g');
-		s = s.replace('[01;31m', '<font color="red">', 'g');
-		s = s.replace('[01;34m', '<font color="blue">', 'g');
-		s = s.replace('[0;33m', '<font color="brown">', 'g');
-		s = s.replace('[00m', '', 'g'); // ?? (start of input?)
-		s = s.replace('[0m', '', 'g'); // ?? (tab?)
-//		s = s.replace('[0m', '');
-//		s = s.replace('[00', '');
-//		s = s.replace(';32m',  '</font>');
-//		s = s.replace('[0m', '</font>';
-		return s;
+		var esc = String.fromCharCode(27);
+		var out = '';
+		var chain = false;
+		
+		//console.log(s);
+		
+		// escape sequences can be "chained", ie one escape character and then sequences with [
+		for (var i=0; i<s.length; ++i) {
+			if ((s.charAt(i) == esc && s.charAt(i+1) == '[') 
+				|| (chain && s.charAt(i) == '[')) 
+				{
+				var seq = '';
+				
+				i += 2;
+				var max=10;
+				while (max-- && !s.charAt(i).match(/[HfABCDsuJKmh|p]/)) {
+					seq += s.charAt(i);
+					++i;
+				}
+				
+				seq += s.charAt(i);
+				//console.log('seq: ' + seq);
+				
+				/* ^[?1034h => VT52 escape code ??, 1 == set cursor to app, 
+				 *  0 == reset?; 3 == 132 columns; 4 == smooth scrolling
+				 */
+				
+				switch (seq.charAt(seq.length - 1)) {
+					case 'H':		// cursor pos
+					case 'f':		// cursor pos
+					case 'A':		// cursor up
+					case 'B':		// cursor down
+					case 'C':		// cursor forward
+					case 'D':		// cursor backward
+					case 's':		// save cursor pos
+					case 'u':		// restore cursor pos
+						break;
+						
+					case 'J':		// 2J == erase display
+					case 'K':		// erase line
+						break;
+					
+					case 'm':		// color / graphic mode
+						var codes = seq.substring(0, seq.length - 1).split(';');
+						var style = '';
+						
+						if (seq == 'm' || seq == '0m' || seq == '00m')
+							out += '</span>';	// all attributes off
+						
+						for (var j=0; j<codes.length; ++j) {
+							switch (parseInt(codes[j])) {
+								// text attributes
+								case 1: style += 'font-weight:bold;'; break;
+								case 4: style += 'text_decoration:underline;'; break;
+								case 5: style += 'text_decoration:blink;'; break;
+								case 7:		// reverse video on
+								case 8:		// concealed on ??
+									break;
+									
+								// foreground colors
+								case 30: style += 'color:black;'; break;
+								case 31: style += 'color:red;'; break;
+								case 32: style += 'color:green;'; break;
+								case 33: style += 'color:yellow;'; break;
+								case 34: style += 'color:blue;'; break;
+								case 35: style += 'color:magenta;'; break;
+								case 36: style += 'color:cyan;'; break;
+								case 37: style += 'color:white;'; break;
+								
+								// background colors
+								case 40: style += 'background-color:black;'; break;
+								case 41: style += 'background-color:red;'; break;
+								case 42: style += 'background-color:green;'; break;
+								case 43: style += 'background-color:yellow;'; break;
+								case 44: style += 'background-color:blue;'; break;
+								case 45: style += 'background-color:magenta;'; break;
+								case 46: style += 'background-color:cyan;'; break;
+								case 47: style += 'background-color:white;'; break;
+							}
+						}
+						
+						if (style.length > 0) 
+							out += '<span style="' + style + '">';
+
+						break;
+						
+					case 'h':		// set mode (screen mode), probably never used
+					case '|':		// reset mode
+					case 'p':		// set keyboard string
+						break;
+				}
+				
+				chain = true;
+			} else {
+
+				// if weird escape, drop it
+				// TODO somehow remove that double-prompt afterwards also (I think this belongs to that weird escape sequence)
+				if (s.substring(i, i+4) == (esc+']0;')) {
+					i += 3;
+				} else {
+					chain = false;
+					out += s.charAt(i);
+				}
+			}
+		}
+		
+		return out;
 	};
 }
 
