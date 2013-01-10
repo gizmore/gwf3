d8812031e0306120ff76a009b40ef87b126967d5
Gizmore <gizmore@wechall.net>
2013-01-10 20:12:00 +0000

    WC5: New Challenge Site: Hacking-Challenges.de (thx C-tecx)

---
+++ core/module/WeChall/sites/german/WCSite_HC.php
@@ -0,0 +1,36 @@
+<?php
+class WCSite_HC extends WC_Site
+{
+	public function parseStats($url)
+	{
+		if (false === ($result = GWF_HTTP::getFromURL($url, false)))
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+
+		$stats = explode(':', $result);
+		if (count($stats) !== 7)
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+	 	# username:rank:score:maxscore:challssolved:challcount:usercount
+		$uname = $stats[0];
+		$rank = intval($stats[1]);
+		$onsitescore = intval($stats[2]);
+		$maxscore = intval($stats[3]);
+		$challssolved = intval($stats[4]);
+		$challcount = intval($stats[5]);
+		$usercount = intval($stats[6]);
+		
+		if ($maxscore === 0 || $challcount === 0 || $usercount === 0)
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+		$this->updateSite($maxscore, $usercount, $challcount);
+		
+		return array($onsitescore, $rank, $challssolved);
+	}
+}
+?>
