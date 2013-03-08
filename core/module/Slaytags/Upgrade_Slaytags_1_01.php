864aacc29147eebbe5c3333c9def66047c127a36
Gizmore <gizmore@wechall.net>
2013-03-08 23:47:45 +0000

    Slaytags: Rudimentary edit for BPM and MusicKey.

--- core/module/Slaytags/Upgrade_Slaytags_1_01.php
+++ core/module/Slaytags/Upgrade_Slaytags_1_01.php
@@ -1,3 +1,8 @@
 <?php
-
-?>
+function Upgrade_Slaytags_1_01(Module_Slaytags $module)
+{
+	GWF_Website::addDefaultOutput(GWF_HTML::message('Slaytags', "BPM and Key"));
+	$songs = GDO::table('Slay_Song');
+	$songs->createColumn('ss_bpm');
+	$songs->createColumn('ss_key');
+}
