7eb3ddc64b2dec53ca5c9d848f64fc2d957f65a5
Gizmore <gizmore@wechall.net>
2013-07-20 11:46:54 +0000

    WeChall: Fixed MissingMethod error in challenge html_head.

--- www/challenge/html_head.php
+++ www/challenge/html_head.php
@@ -2,7 +2,7 @@
 if(defined('WC_HTML_HEAD__DEFINED')) { return; }
 
 $_GET['mo'] = 'WeChall';
-$_GET['me'] = 'Challenge';
+$_GET['me'] = 'null';
 
 require_once 'protected/config.php';
 require_once '../gwf3.class.php';
@@ -11,7 +11,7 @@ $gwf = new GWF3(dirname(dirname(__FILE__).'../'), array(
 	'bootstrap' => false, # Init GWF_Bootstrap?
 	'website_init' => true, # Init GWF_Website?
 	'autoload_modules' => true, # Load modules with autoload flag?
-	'load_module' => true, # Load the requested module?
+	'load_module' => false, # Load the requested module?
 	'load_config' => false, # Load the config file? (DEPRECATED) # TODO: REMOVE
 	'start_debug' => true, # Init GWF_Debug?
 	'get_user' => true, # Put user into smarty templates?
@@ -23,10 +23,10 @@ $gwf = new GWF3(dirname(dirname(__FILE__).'../'), array(
 	'ignore_user_abort' => true, # Ignore abort and continue the script on browser kill?
 ));
 
-//if (false === ($wechall = GWF_Module::loadModuleDB('WeChall', true, true)))
-//{
-//	die('GWF3/WC5 not installed?');
-//}
+if (false === ($wechall = GWF_Module::loadModuleDB('WeChall', true, true)))
+{
+	die('GWF3/WC5 not installed?');
+}
 
 //$error = GWF_Module::startup();
 
