18007cb1187d96e8d498427d6741393fd91b2e07
Gizmore <gizmore@wechall.net>
2013-07-27 05:11:35 +0000

    Dog: Updated readme_install. (thx epoch_qwert)

--- core/module/Dog/dog_bin/readme_install.txt
+++ core/module/Dog/dog_bin/readme_install.txt
@@ -1,27 +1,28 @@
-Lamb install guide.
+Lamb4/Dog install guide.
 
-Lamb3 is a GWF3 module meanwhile, so it needs at least the gwf3 core in "core/inc/", "core/inc/3p/" and the lamb module itself in "core/module/Lamb"
-Also, the "protected/" folder is used for the logfiles, db_backups and the gwf3_config.
+Lamb4/Dog is a GWF3 module meanwhile, so it needs at least the gwf3 core in "core/inc/", "core/inc/3p/" and the Dog module itself in "core/module/Dog"
+Also, the "www/protected/" folder is required for the logfiles, db_backups and the gwf3_config.
+Also, the "www/dbimg" folder is required by GWF_Mail and various modules to write application data files.
 
 ===================================
 
 1) Create a database.
 
-Of course you need to create a database for lamb, by issuing these commands:
+Of course you need to create a database for dog, by issuing these commands:
 
-CREATE USER 'lamb3'@'localhost' IDENTIFIED BY 'lamb3';
-CREATE DATABASE lamb3;
-GRANT ALL ON lamb3.* TO 'lamb3'@'localhost' IDENTIFIED BY 'lamb3';
+CREATE USER 'dog'@'localhost' IDENTIFIED BY 'dog';
+CREATE DATABASE dog;
+GRANT ALL ON dog.* TO 'dog'@'localhost' IDENTIFIED BY 'dog';
 
-If you plan to use lamb+website, you can use the same database for both of it, but it's also possible to have a separate gwfconfig for lamb.
+If you plan to use Dog+GW3Website, you can use the same database for both of it, but it's also possible to have a separate "config.php" for Dog.
 
 ===================================
 
 2b) Install via install wizard.
 
 You will need a webserver to access install/wizard.php
-Simply follow the instructions and all modules will be installed.
-You will then have a working gwf3 website + bot. 
+Simply follow the instructions to install gwf3 and the needed modules.
+You will then have a working gwf3 website + Dog. 
 
 ===================================
 
@@ -29,31 +30,30 @@ You will then have a working gwf3 website + bot.
 
 This will install only the minimal components required for the Lamb bot.
 
-First you need a working gwf3 config for lamb.
+First you need a working gwf3 config for Dog.
 As we don't use install wizard, we have to create it manually from config.example.php
 
-cd protected
+
+cd www/protected
 cp config.example.php config.php 
 nano config.php
 
-Now we edit the lamb config in core/module/Lamb/lamb_bin/Lamb_Config.php
 
-Then we patch lamb_mini_install.php to make use of the right gwf3 config file.
-
-Now we try to run the lamb_mini_install.php:
-
-cd core/module/Lamb/lamb_bin
-php lamb_mini_install.php
-
-Now we create our own .sh file to launch the bot:
-cd core/module/Lamb/lamb_bin
-cp lamb.sh my_lamb.sh
-nano my_lamb.sh
-
-IMPORTANT Is to give the right config files to the lamb.php!
-
-If everything works well, run the bot via:
-cd core/module/Lamb/lamb_bin
-./my_lamb.sh
+To mini_install Dog simply run "php core/modules/Dog/dog_bin/dog.php config.php install"
+This will also already launch the bot.
+It is safe to exexute with "install" on any launch.
 
 ===================================
+
+ANNOYING: Adding the first irc network
+
+You have to do this manually in the database after the install.
+You have to add 1 row to dog_servers and 1 row to dog_nicknames
+
+dog_servers example: 2 	irc.freenode.net 	en 	CFILMPQbcefgijklmnopqrstvz 	DOQRSZaghilopswz 	305 	6697 	NULL
+dpg_nicks   example: 1 	2 	Lamb3 	uhohnono 	Dawg 	dog.gizmore.org 	Doggy 	0
+
+===================================
+
+LAUNCHing THE BOT:
+php core/modules/Dog/dog_bin/dog.php config.php install
---
+++ www/protected/config.example.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * JUST EXAMPLE
+ * MOST PATHES ARE WRONG
+ * PATCHING BY HAND IS LAME
+ */
+#######################
+### Error reporting ###
+#######################
+ini_set('display_errors', 0);
+error_reporting(0xffffffff);
+
+###################
+### Main Config ###
+###################
+define('GWF_DOMAIN', 'some.net'); # HOSTNAME?
+define('GWF_SITENAME', 'Dog');  # any string: sitename
+define('GWF_WEB_ROOT_NO_LANG', '/'); # Website root usually / or rarely /myGWF/
+define('GWF_LOG_BITS', 0x7fffffff); # see GWF_Log
+
+#################
+### 3rd Party ###
+#################
+# ADJUST THESE. SMARTY IS INCLUDED IN GWF3
+define('GWF_SMARTY_PATH', '/data/_ProjectPDT7/GWF3/core/inc/3p/smarty/Smarty.class.php'); # Path to Smarty.class.php. Smarty replaced the GWF template engine and has to be available.
+define('GWF_JPGRAPH_PATH', '/opt/php/jpgraph/jpgraph.php'); # Path to jpgraph.php. JPGraph is a library to draw graphs with php. It is available under the GPL.
+define('GWF_GESHI_PATH', '/opt/php/geshi/geshi.php'); # Path to geshi.php. GeSHi is a GPL licensed Syntax highlighter.
+
+##############
+### Smarty ###
+##############
+# ADJUST THESE. SMARTY IS INCLUDED IN GWF3
+define('GWF_SMARTY_DIRS', '/data/_ProjectPDT7/GWF3/extra/temp/smarty'); # Path to smarty template directory.
+define('GWF_ERRORS_TO_SMARTY', false); # Group all Error and display them in one Box?
+define('GWF_MESSAGES_TO_SMARTY', false); # Same as above with success-messages
+
+################
+### Defaults ###
+################
+define('GWF_DEFAULT_URL', 'login'); # Default 1st URL ()
+define('GWF_DEFAULT_LANG', 'en'); # Default language should be english
+define('GWF_DEFAULT_MODULE', 'Login'); # Default 1st Module ()
+define('GWF_DEFAULT_METHOD', 'Form'); # Default 1st Method  ()
+define('GWF_DEFAULT_DESIGN', 'default'); # Default Design (default)
+define('GWF_DEFAULT_DOCTYPE', 'html5'); # Default Design (default)
+define('GWF_ICON_SET', 'default'); # Default Icon-Set. Example: 'default'.
+
+################
+### Language ###
+################
+define('GWF_LANG_ADMIN', 'en'); # Admin language should be english
+define('GWF_SUPPORTED_LANGS', 'en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr'); # All supported languages
+
+###############
+### Various ###
+###############
+define('GWF_ONLINE_TIMEOUT', 10); # A request will mark you online for N seconds
+define('GWF_CRONJOB_BY_WEB', 0); # chance in permille to trigger www cronjob by client (0-1000)
+define('GWF_CAPTCHA_COLOR_BG', 'FFFFFF');
+define('GWF_USER_STACKTRACE', true); # Show a stacktrace to the user, if set.
+
+##############################
+### Database Configuration ###
+##############################
+define('GWF_SECRET_SALT', 'xxxxxxxxxxxxxxxx'); # Changeme
+define('GWF_CHMOD', 0777); # CHMOD mask for file creation. 0700 for mpm-itk env. 0777 in worst case
+define('GWF_DB_HOST', 'localhost');
+define('GWF_DB_USER', 'dog');
+define('GWF_DB_PASSWORD', 'dog');
+define('GWF_DB_DATABASE', 'dog');
+define('GWF_DB_TYPE', 'mysql'); # currently only mysql is supported
+define('GWF_DB_ENGINE', 'myISAM'); # innoDB or myISAM
+define('GWF_TABLE_PREFIX', ''); # Change at will, dual install possible.
+
+#############################
+### Session/Cookie Config ###
+#############################
+define('GWF_SESS_NAME', 'DOG'); # cookie name
+define('GWF_SESS_SECURE', false); # cookie is ssl only
+define('GWF_SESS_LIFETIME', 432000); # cookie lifetime
+define('GWF_SESS_PER_USER', 3); # sessions max per user
+
+###################
+### IP Settings ###
+###################
+define('GWF_IP_QUICK', 'hash_32_1');  # Hashed IP Duplicates (see GWF_IP6.php)
+define('GWF_IP_EXACT', 'bin_32_128'); # Complete IP storage (see GDO/IP6.php)
+
+####################
+### Email Config ###
+####################
+define('GWF_DEBUG_EMAIL', 0xFF); # 0=NONE, 1=DB ERRORS, 2=PHP_ERRORS, 4=404, 8=403
+define('GWF_BOT_EMAIL', 'robot@giz.org');
+define('GWF_ADMIN_EMAIL', 'gizmore@giz.org');
+define('GWF_STAFF_EMAILS', ''); # separate cc plain emails by, (not really used yet)
+define('GWF_SUPPORT_EMAIL', 'support@giz.org'); # all staff members should catch those
+?>
