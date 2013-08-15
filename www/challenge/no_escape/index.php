cf9b3cd6e2f4d61f3a38b9117469f8dbc4519d82
Gizmore <gizmore@wechall.net>
2013-08-15 16:23:44 +0000

    WC5: Added "no escape" challenge to the SVN.

---
+++ www/challenge/no_escape/index.php
@@ -0,0 +1,34 @@
+<?php
+define('NO_ESCAPE_USER', 'gizmore_noesc');
+define('NO_ESCAPE_DB', 'gizmore_noesc');
+define('NO_ESCAPE_PW', 'gizmore_noesc');
+require_once 'code.include';
+
+chdir('../../');
+define('GWF_PAGE_TITLE', 'No Escape');
+require_once('challenge/html_head.php');
+if (false === ($chall = WC_Challenge::getByTitle('No Escape')))
+{
+	$chall = WC_Challenge::dummyChallenge('No Escape', 2, '/challenge/no_escape/index.php', false);
+}
+$chall->showHeader();
+
+if (false !== ($who = Common::getGetString('vote_for')))
+{
+	noesc_voteup($who);
+}
+
+htmlTitleBox($chall->lang('title'), $chall->lang('info', array('code.include', 'index.php?highlight=christmas')));
+
+if (Common::getGet('highlight') === 'christmas')
+{
+	$msg = file_get_contents('challenge/no_escape/code.include');
+	$msg = '[code=php title=code.include]'.$msg.'[/code]';
+	echo GWF_Box::box(GWF_Message::display($msg, true, false, false));
+}
+
+echo noesc_DisplayVotes($chall);
+
+echo $chall->copyrightFooter();
+require_once('challenge/html_foot.php');
+?>
---
+++ www/challenge/no_escape/install.php
@@ -0,0 +1,31 @@
+<?php
+chdir("../../");
+require_once("challenge/html_head.php");
+html_head("Install No Escape");
+if (!GWF_User::isAdminS())
+{
+	return htmlSendToLogin("Better be admin !");
+}
+
+/*
+CREATE DATABASE gizmore_noesc;
+GRANT ALL ON gizmore_noesc.* TO 'gizmore_noesc'@'localhost' IDENTIFIED BY 'xxxxxxxxxx';
+# Now execute install.php
+*/
+define('NO_ESCAPE_USER', 'gizmore_noesc');
+define('NO_ESCAPE_DB', 'gizmore_noesc');
+define('NO_ESCAPE_PW', 'gizmore_noesc');
+require_once 'code.include';
+
+noesc_createTable();
+
+$title = 'No Escape';
+$solution = false;
+$score = 2;
+$url = "challenge/no_escape/index.php";
+$creators = "Gizmore";
+$tags = 'Exploit,PHP,MySQL';
+
+WC_Challenge::installChallenge($title, $solution, $score, $url, $creators, $tags, true);
+
+require_once("challenge/html_foot.php");
