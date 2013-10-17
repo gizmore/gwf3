f6b349129135a49c8d7c74527e9a9d0980d70fc1
dloser <dloser@hotmail.com>
2013-10-17 23:48:00 +0000

    Dog: 'langauge' typo in Translate module (thx alucardo)

--- core/module/Dog/dog_module/Translate/lang/translate_en.php
+++ core/module/Dog/dog_module/Translate/lang/translate_en.php
@@ -2,8 +2,8 @@
 $lang = array(
 	'help_translate' => 'Usage: translate <text goes here>. Translates some foreign language to the channels language. There are also shortcuts available, like .en-de, .pl-ja, etc.',
 
-	'err_src' => 'The source langauge is unknown.',
-	'err_dst' => 'The target langauge is unknown.',
+	'err_src' => 'The source language is unknown.',
+	'err_dst' => 'The target language is unknown.',
 
 	'weird' => 'Could not parse the result.',
 	'trans' => "\X02Translation:\X02 %s",
