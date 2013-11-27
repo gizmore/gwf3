64d44f74293c3a4861209e2663da0cee2c073eab
Gizmore <gizmore@wechall.net>
2013-11-27 23:18:56 +0000

    WC5: Commited QMine partially + obvious herring message. (thx quangntenemy)

---
+++ www/challenge/quangntenemy/QMine/lang/chall_en.php
@@ -0,0 +1,13 @@
+<?php
+$lang = array(
+	'title' => 'QMine',
+	'info' =>
+		'The tiger tamer &quot;quangntenemy&quot; has created a java challenge for us.<br/>'.PHP_EOL.
+		'Quangster is known for his java knowledge and i am happy he releases a challenge here :)<br/>'.PHP_EOL.
+		'Your mission is to find the hidden solution, of course ;)<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Happy Challenging!<br/>'.PHP_EOL.
+		'quangntenemy and gizmore'.PHP_EOL,
+	'java' => 'Load the applet please so i can download and crack it!',
+	'err_obvious' => 'You have found the obvious password, but it is not the goal here :P',
+);
