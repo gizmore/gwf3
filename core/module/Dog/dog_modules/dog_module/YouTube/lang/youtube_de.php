3715e42b9fe77b872710b7157a445f92e397325c
Gizmore <gizmore@wechall.net>
2013-12-16 17:53:56 +0000

    Dog: Youtube video announcements do now include duration.

--- core/module/Dog/dog_modules/dog_module/YouTube/lang/youtube_de.php
+++ core/module/Dog/dog_modules/dog_module/YouTube/lang/youtube_de.php
@@ -1,4 +1,4 @@
 <?php
 $lang = array(
-	'video' => "\X02[YouTube] Titel:\X02 %s | \X02Bewertung:\X02 %s/5.00 | \X02Views:\X02 %s | \X02Bewertungen:\X02 %s.",
+	'video' => "\X02[YouTube]\X02 %s (%s) | \X02Rating:\X02 %s/5.00 | \X02Viewed:\X02 %s | \X02Rated:\X02 %s.",
 );
