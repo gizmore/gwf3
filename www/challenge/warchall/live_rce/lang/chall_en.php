a171ae9faafe20ccff8ecb9c31ce2cef15e5612d
Gizmore <gizmore@wechall.net>
2014-01-17 23:26:52 +0000

    WeChall: We changed the LIVE_RCE completely to something notable and fun. (thx phiber)

--- www/challenge/warchall/live_rce/lang/chall_en.php
+++ www/challenge/warchall/live_rce/lang/chall_en.php
@@ -2,9 +2,11 @@
 $lang = array(
 	'title' => 'Live RCE',
 	'info' =>
-		'You can try your php exploit skills on this <a href="%s">serious programming flaw</a>.'.PHP_EOL.
-		'Happy Exploiting'.PHP_EOL.
-		'gizmore',
-		
-		
+		'Nurfed and phiber finally fixed this challenge and made it a solveable relict.<br/>'.PHP_EOL.
+		'We hope you enjoy!<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Access the <a href="%s">challenge here on warchall</a>.<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'Happy Exploiting<br/>'.PHP_EOL.
+		'nurfed and phiber',
 );
--- www/challenge/warchall/live_rce/www/index.php
+++ www/challenge/warchall/live_rce/www/index.php
@@ -1,6 +1,34 @@
-$rce = unserialize($_REQUEST['meh']);
-array_map($rce, 'print');
 <?php
-$rce = unserialize($_REQUEST['meh']);
-array_map($rce, 'print');
-
+require '../config.php';
+$username = isset($_GET['username']) ? (string)$_GET['username'] : 'Guest';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Live RCE [Warchall]</title>
+<style type="text/css">
+* {
+margin: 0;
+padding: 0;
+}
+body {
+background: #000;
+color: #0f0;
+padding: 24px;
+}
+p {
+padding: 12px 0;
+}
+</style>
+</head>
+<body>
+<h1>Live RCE!</h1>
+<p>Hello <?php echo $username; ?>!</p>
+<p>Here are your $_SERVER vars:</p>
+<pre>
+<?php print_r($_SERVER); ?>
+</pre>
+<p>Kind Regards<br/>The Warchall staff!</p>
+<!-- We hope you like our signatures :) -->
+</body>
+</html>
