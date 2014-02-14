81bfb33980f1c7f3f8298e82af7a4f40ddac8087
spaceone <space@wechall.net>
2014-02-14 20:21:34 +0000

    new pytong setuid wrapper

--- www/challenge/space/pytong/pytong.py
+++ www/challenge/space/pytong/pytong.py
@@ -1,59 +1,59 @@
 #!/usr/bin/python
-
 import sys
 import os
+import os.path
+
+SOLUTION = "/home/level/12/solution.txt"
+
 
 # Your main objective is to return True
-def main(argv):
+def main(filepath):
+	# We want to prevent some noobish solutions
+	if any(ipattern in filepath for ipattern in ('proc', 'uptime', 'tmp', 'random', 'full', 'zero', 'null')):
+		raise ValueError('nononono: hacking is not allowed')
+
 	# You have to give me an valid file!
-	if not os.path.exists(argv):
-		print('sorry file "%s" does not exists' % argv)
-		return False
+	if not os.path.exists(filepath):
+		raise ValueError('sorry file "%s" does not exists' % filepath)
 
 	# We are opening the file here and store the content in 'jjk'
-	print('opening '+argv)
-	gizmore = open(argv)
-	jjk = gizmore.read()
-	gizmore.close()
+	print('opening %s' % filepath)
+	with open(filepath) as gizmore:
+		jjk = gizmore.read()
 	print('closed')
 
 	# The file was closed, does it still exists?
-	if not os.path.exists(argv):
+	if not os.path.exists(filepath):
 		# The file does not exists anymore, you have found a solution
 		print('You are l33t')
 		return True
 	else:
 		# Ok, we will reopen the file and store its content in 'kwisatz'
-		spaceone = open(argv)
-		kwisatz = spaceone.read()
-		spaceone.close()
+		with open(filepath) as spaceone:
+			kwisatz = spaceone.read()
 
 		# Does the content differs from old content?
 		if jjk != kwisatz:
 			# content differs so return True
 			print('You are a winner')
 			return True
-	return False
 
-def validate_arg(value):
-	# We also want to prevent some noobish solutions
-	for ipattern in ['proc', 'uptime', 'tmp', 'random', 'full', 'zero', 'null']:
-		if ipattern in value:
-			raise ValueError('nononono: hacking is not allowed')
+	raise ValueError('fail...')
 
-# Call main() when running file directly
+
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
+	if len(sys.argv) < 2:
 		print('wrong argcount')
 		sys.exit(1)
 
-	rc = 2
 	try:
 		# Objective: return True here!
-		validate_arg(sys.argv[1])
 		success = main(sys.argv[1])
-	except:
-		print('an Exception occured: corrupted file or no file permissions?')
+	except (ValueError, OSError, IOError) as exc:
+		print('%s' % (exc,))
+		sys.exit(3)
 	else:
-		rc = 0 if success else 2
-	sys.exit(rc)
+		#os.setuid(os.geteuid())
+		os.setgid(os.getegid())
+		with open(SOLUTION) as fd:
+			print(fd.read())
--- www/challenge/space/pytong/wrap.c
+++ www/challenge/space/pytong/wrap.c
@@ -1,89 +1,19 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define BUFSIZE 256
 
-int escape_single_quotes(char *to, char *from, int max)
+#define PYTHON "/usr/bin/python"
+#define CHALLENGE "/home/level/12/pytong.py"
+
+int main(int argc, char **argv)
 {
-	int l = 0;
-
-	for (;*from;from++)
-	{
-		switch (*from)
-		{
-			case '\'':
-				if (l>=max-4) return 0;	// not enough space for escaped chars!
-// replace ' with '\''
-				*(to++)= '\'';
-				*(to++)= '\\';
-				*(to++)= '\'';
-				l += 3;
-			default:
-				if (l>=max-1) return 0;	// not enough space for this char!
-				*(to++)= *from;
-				l++;
-		}
+	if (argc < 2) {
+		printf("usage: %s file\n", *argv);
+		return 2;
 	}
+//	setgid(getegid());
+//	setuid(geteuid());
 
-	*to = 0;
-
+	execle(PYTHON, PYTHON, CHALLENGE, argv[1], (char *)0, (char *)0);
 	return 1;
-}
-
-int main(int argc, char *argv[])
-{
-	FILE *fp;
-	FILE *solution;
-	char buf[BUFSIZE];
-	char escaped_name[BUFSIZE];
-	char *filename;
-
-	setregid(1013, 1013); // gid of level12
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s filename\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	filename = argv[1];
-
-	if (!escape_single_quotes(escaped_name, filename, BUFSIZE))
-	{
-		fprintf(stderr, "Escaped filename is too long!\n");
-		exit(EXIT_FAILURE);
-	}
-
-	if (snprintf(buf, BUFSIZE, "/usr/bin/python /home/level/12/pytong.py '%s'", escaped_name) >= BUFSIZE)
-	{
-		fprintf(stderr, "Filename %s is too long!\n", filename);
-		exit(EXIT_FAILURE);
-	}
-
-	unsetenv("PYTHONPATH");
-
-	if (!(fp = popen(buf, "r")))
-	{
-		fprintf(stderr, "Command execution failed! Sorry!\n");
-		exit(EXIT_FAILURE);
-	}
-	while(fgets(buf, sizeof(buf), fp)!=NULL)
-	{
-		printf("%s", buf);
-	}
-	if (0 == pclose(fp))
-	{
-		if(!(solution = fopen("/home/level/12/solution.txt", "r")))
-		{
-			fprintf(stderr, "Sorry! Solution is right but an error occured, please try again!\n");
-			exit(EXIT_FAILURE);
-		}
-		while(fgets(buf, sizeof(buf), solution)!=NULL)
-		{
-			printf("%s", buf);
-		}
-		fclose(solution);
-
-	}
-
-	return EXIT_SUCCESS;
 }
