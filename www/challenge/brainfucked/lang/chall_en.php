d503901a2637aa18874260c16af314e31c04c154
Gizmore <gizmore@wechall.net>
2014-06-28 13:29:15 +0000

    WeChall: Commited Brainfucked challenge to SVN.

---
+++ www/challenge/brainfucked/lang/chall_en.php
@@ -0,0 +1,11 @@
+<?php
+$lang = array(
+	'title' => 'Brainfucked',
+	'hint' => '',
+	'info' =>
+		'I found something exciting today, and it was easy to put it into a challenge.<br/>'.PHP_EOL.
+		'I hope you hate <a href="sourcecode.php">it</a>! >;-)<br/>'.PHP_EOL.
+		'<br/>'.PHP_EOL.
+		'%s'.PHP_EOL.
+		'gizmore<br/>'.PHP_EOL,
+);
