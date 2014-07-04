0bbb6a20d79ecb58eeb0121cf5ed24f7f3cc1d4b
spaceone <space@wechall.net>
2014-07-04 12:20:40 +0000

    Lamb: corrected blame functionality

--- core/module/Lamb/lamb_plugin/pfun/blame.php
+++ core/module/Lamb/lamb_plugin/pfun/blame.php
@@ -5,5 +5,8 @@ shuffle($goats);
 $rand_keys = array_rand($goats, count($goats));
 shuffle($goats);
 $scapegoat = $bot->getCurrentChannel() === "#revolutionelite" ? 'sabretooth' : $goats[$rand_keys[rand(0, count($goats) - 1)]];
-$bot->reply("I blame $scapegoat.");
+if($scapegoat === "spaceone")
+	$bot->reply("Well i can't blame spaceone anymore. You need to blame yourself.");
+else
+	$bot->reply("I blame $scapegoat.");
 ?>
