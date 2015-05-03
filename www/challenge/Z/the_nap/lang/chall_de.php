28aef5f7ada53c768bd7198aefd16d1b633ebda1
SpaceOne <space@wechall.net>
2015-05-04 00:00:36 +0200

    fix german translation of "the nap" challenge

--- www/challenge/Z/the_nap/lang/chall_de.php
+++ www/challenge/Z/the_nap/lang/chall_de.php
@@ -2,17 +2,17 @@
 $lang = array(
 	'title' => 'Das Nickerchen',
 	'info' => 
-		'Du wachst um Vier Uhr Morgens auf, und stellst traurig fest, das wieder ein langweiliger Arbeitstag ansteht.<br/>'.PHP_EOL.
-		'Du machst dir Frühstück, und liest deine Lieblingszeitung "Der Angriff".<br/>'.PHP_EOL.
-		'Es ist der 5. Juni 1944. Die Nachrichten sagen voraus, das es in den nächsten Tagen keine Invasionen geben wird.<br/>'.PHP_EOL.
-		'Sogar dein Boss hat zur Zeit Urlaub. Du kommst um Fünf Uhr bei der Wehrmacht an, und<br/>'.PHP_EOL.
-		'Der Kollege von der Nachtschicht begrüsst dich, und überreicht dir die täglichen Codes.<br/>'.PHP_EOL.
+		'Du wachst um Vier Uhr Morgens auf und stellst traurig fest, dass wieder ein langweiliger Arbeitstag ansteht.<br/>'.PHP_EOL.
+		'Du machst dir Frühstück und liest deine Lieblingszeitung "Der Angriff".<br/>'.PHP_EOL.
+		'Es ist der 5. Juni 1944. Die Nachrichten sagen voraus, dass es in den nächsten Tagen keine Invasionen geben wird.<br/>'.PHP_EOL.
+		'Sogar dein Boss hat zur Zeit Urlaub. Du kommst um Fünf Uhr bei der Wehrmacht an, <br/>'.PHP_EOL.
+		'der Kollege von der Nachtschicht begrüsst dich und überreicht dir die täglichen Codes.<br/>'.PHP_EOL.
 		'Daraufhin verlässt er schnell die Kommunikationszentrale.<br/>'.PHP_EOL.
-		'Er ist wirklich ein fauler Zeitgenosse, und die Enigma-Einstellungen für heute natürlich noch nicht eingegeben.<br/>'.PHP_EOL.
+		'Er ist wirklich ein fauler Zeitgenosse und hat die Enigma-Einstellungen für heute natürlich noch nicht eingegeben.<br/>'.PHP_EOL.
 		'Du denkst, heute wird mal wieder ein richtig fauler Arbeitstag - allein.<br/>'.PHP_EOL.
-		'Du trinkst erstmal ein Bier, und wartest auf eingehende Nachrichten. Weil nichts passiert,<br/>'.PHP_EOL. 
+		'Du trinkst erstmal ein Bier und wartest auf eingehende Nachrichten. Weil nichts passiert<br/>'.PHP_EOL. 
 		'schlummerst du ein. Du träumst gut, aber plötzlich klingelt das Radio.<br/>'.PHP_EOL.
-		'Als du aus deinen Träumen erwachst, schüttest du dein Bier um, und es durchweicht das tägliche Codebuch.<br/>'.PHP_EOL.
+		'Als du aus deinen Träumen erwachst, schüttest du dein Bier um und es durchweicht das tägliche Codebuch.<br/>'.PHP_EOL.
 		'Du fluchst, nimmst das Radio und zeichnest die verschlüsselte Nachricht auf:<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
 		'<pre style="margin: 8px 12px">'.PHP_EOL.
@@ -22,16 +22,16 @@ $lang = array(
 		'XVSZ JXCW BKNV IJMG RFOV YWYZ CKOZ ZPIV JLEN'.PHP_EOL.
 		'ZUUX NEAP QGOV'.PHP_EOL.
 		'</pre>'.PHP_EOL.
-		'Nach dem Aufzeichnen, bemerkst du das die Schlüssel für heute teilweise zerstört wurden.<br/>'.PHP_EOL.
-		'Du kannst dir vorstellen das dein Vorgesetzter sehr wütend sein wird, wenn er zurückkehrt.,<br/>'.PHP_EOL.
+		'Nach dem Aufzeichnen bemerkst du, dass die Schlüssel für heute teilweise zerstört wurden.<br/>'.PHP_EOL.
+		'Du kannst dir vorstellen das dein Vorgesetzter sehr wütend sein wird, wenn er zurückkehrt,<br/>'.PHP_EOL.
 		'falls du es nicht schaffst die Nachricht davor zu entschlüsseln.<br/>'.PHP_EOL.
 		'Das versaute Codebuch ist hier: <a href="%1$s">Codebuch</a>;<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
-		'Deine Aufgabe ist die Nachricht zu entschlüsseln, und die Lösung<br/>'.PHP_EOL.
-		'ist das letzte Deutsche Wort, kleingeschrieben, und Angehängt,<br/>'.PHP_EOL.
-		'die gesamte Anzahl der möglichen Einstellungen in bits, wenn die Verdrahtung der Rotoren Geheim ist.<br/>'.PHP_EOL.
+		'Deine Aufgabe ist die Nachricht zu entschlüsseln. Die Lösung<br/>'.PHP_EOL.
+		'ist das letzte deutsche Wort, kleingeschrieben, und <br/>'.PHP_EOL.
+		'die gesamte Anzahl der möglichen Einstellungen in bits angehängt - falls die Verdrahtung der Rotoren geheim ist.<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
-		'Zum Beispiel hat AES128 128 bits. Also wenn das letzte Deutsche Wort &quot;WeChall&quot; ist, und<br/>'.PHP_EOL.
+		'Zum Beispiel hat AES128 128 bits. Also wenn das letzte deutsche Wort &quot;WeChall&quot; ist und<br/>'.PHP_EOL.
 		'die Anzahl der möglichen Einstellungen in bits 128 beträgt, lautet die Lösung:<br/>'.PHP_EOL.
 		'wechall128<br/>'.PHP_EOL.
 		'</div>'.PHP_EOL.
