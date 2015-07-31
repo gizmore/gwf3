39b5bf18bfc0f2263e0983fc543f001e066b626c
Daniel Correa <admin@sinfocol.org>
2015-07-30 20:51:59 -0500

    Rename yabfdbg_en.php to yabfdbg_es.php

---
+++ core/module/WeChall/lang/yabfdbg_es.php
@@ -0,0 +1,16 @@
+<?php
+$lang = array(
+	'page_title' => 'YaBfDbg',
+	'meta_tags' => 'GTK, El mejor depurador de Brainfuck, Gizmore, Breakpoints, Yet Another Brainfuck Debugger',
+	'info_box_1' => 
+		'Este es aún otro depurador para Brainfuck (Yet Another Brainfuck Debugger) escrito en gtkmm.<br/>'.
+		'No sé exactamente como instalarlo. Puedes darme retroalimentación en alguna parte!<br/>'.
+		'Necesitarás al menos gtk+ instalado para correrlo, y gtkmm para compilarlo.<br/>'.
+		'Puedes obtener un <a href="%s">ejecutable de Windows acá</a>.<br/>'.
+		'<a href="%s">El código fuente está disponible acá</a> y debe compilar bajo Linux y Windows (requiere gtkmm).<br/>'.
+		'<br/>'.
+		'Gracias a <a href="/profile/awe">awe</a>, ahora hay un archivo configure y makefile disponible para Linux. Muchas gracias, fue un trabajo awegrandioso de parte tuya :)',
+	'features' => 'Características',
+	'info_box_2' => 'Inserte puntos de ruptura (breakpoints) presionando la letra \'B\'.<br/>Trazar, correr, y ejecutar paso a paso un programa.',
+);
+?>
