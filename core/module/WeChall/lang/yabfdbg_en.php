b1cc4243eb68c493fa1710d5f5b90f9c5939a29b
Daniel Correa <admin@sinfocol.org>
2015-07-30 20:42:20 -0500

    Update yabfdbg_en.php

--- core/module/WeChall/lang/yabfdbg_en.php
+++ core/module/WeChall/lang/yabfdbg_en.php
@@ -6,7 +6,7 @@ $lang = array(
 		'This is Yet another Brainfuck DeBuGger written in gtkmm.<br/>'.
 		'I don\'t know exactly how to install it. Leave me feedback somewhere!<br/>'.
 		'You will need at least gtk+ installed to run it, and gtkmm to compile it.<br/>'.
-		'You can get a <a href="%s">windows exeuteable here</a>.<br/>'.
+		'You can get a <a href="%s">windows executable here</a>.<br/>'.
 		'<a href="%s">Source is available here</a> and should compile under linux and windows (needs gtkmm).<br/>'.
 		'<br/>'.
 		'Thanks to <a href="/profile/awe">awe</a>, there is now a linux configure and makefile available. Thanks a lot man, this was awe-some from you :)',
