7b596982a39724be4e2180ba5ca3ecf8bbcd2335
Daniel Correa <admin@sinfocol.org>
2015-07-30 21:57:46 -0500

    Create jcs_es.php

---
+++ core/module/WeChall/lang/jcs_es.php
@@ -0,0 +1,12 @@
+<?php
+$lang = array(
+	'page_title' => 'JColorSucker',
+	'meta_tags' => 'Obtener RGB ARGB, Píxeles de la imagen, Java, Gizmore, Herramienta',
+	'box' =>
+		'Propósito: obtener el color de los píxeles de una imagen y almacenarlos en un archivo de texto.<br/><br/>'.
+		'Características:<br/>'.
+		'- aplicar una cuadrícula en la imagen, de la cual se obtendrán los píxeles<br/>'.
+		'- especificar los componentes y el orden en como se obtendrán<br/><br/>'.
+		'Obtén el progama y el código fuente en este enlace: <a href="%s">JCS.jar</a>.',
+);
+?>
