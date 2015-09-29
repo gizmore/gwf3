494c47b905cf08009f7653169924d68dc3a2cdfa
dloser <dloser@hotmail.com>
2015-09-29 20:50:35 +0200

    Dog: fixed handling of MODE command

--- core/module/Dog/dog_event/MODE.php
+++ core/module/Dog/dog_event/MODE.php
@@ -1,41 +1,66 @@
 <?php
 $msg = Dog::getIRCMsg();
+$rawmsg = $msg->getRaw();
 $argv = $msg->getArgs();
 $argc = count($argv);
-$channel = Dog::setupChannel();
 
-# Set mode for channel
-if ($argc === 2)
+if (false === strpos('&#+!', $argv[0][0]))
 {
-	
-}
+	# MODE <nickname> *( ( "+" / "-" ) *( "i" / "w" / "o" / "O" / "r" ) )
 
-elseif ($channel === false)
-{
-	# HUH?!
-}
-
-# Set mode for user
-# :gizmore!gizmore@localhost MODE #sr +o Dog
-elseif ($argc === 3)
-{
-	$chan_name = array_shift($argv);
-	$symbols = str_split(array_shift($argv));
-	$sign = array_shift($symbols);
-	foreach ($symbols as $i => $symbol)
+} else {
+	# MODE <channel> *( ( "-" / "+" ) *<modes> *<modeparams> )
+	$channel = Dog::setupChannel();
+	$arg = 1;
+	while ($arg != $argc)
 	{
-		if (false !== ($user = $channel->getUserByName($argv[$i])))
+		$sign = $argv[$arg][0];
+		if ($sign !== '-' && $sign !== '+')
 		{
-			$channel->setUser($user, $symbol, $sign==='+');
+			Dog_Log::error("No valid sign for argument $arg of $rawmsg");
+			break;
 		}
-		else
+
+		$modes = substr($argv[$arg++], 1);
+		for ($i=0; $i<strlen($modes); $i++)
 		{
-			echo "DUNNO!!!\n";
+			$mode = $modes[$i];
+
+			if (false !== strpos('Oohv', $mode)) # member status
+                        {
+				$nick = $argv[$arg++];
+				if (false !== ($user = $channel->getUserByName($nick)))
+				{
+					$channel->setUser($user, $mode, $sign==='+');
+				} else {
+					Dog_Log::warning("Unknown user $nick in $rawmsg");
+				}
+
+			} elseif (false !== strpos('aimnqpsrt', $mode))
+			{
+
+			} elseif ($mode === 'k')
+			{
+				$arg++;
+
+			} elseif ($mode === 'l')
+			{
+				if ($sign === '+')
+				{
+					$arg++;
+				}
+
+			} elseif (false !== strpos('beI', $mode))
+			{
+				$arg++;
+
+			} else {
+				Dog_Log::error("Unknown channel mode $mode in $rawmsg");
+				$arg = $argc; # not safe to continue
+				break;
+			}
 		}
-// 		else if ($argv[$i] === Dog::getNickname())
-// 		{
-			
-// 		}
 	}
 }
+
 ?>
