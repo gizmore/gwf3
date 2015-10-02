gizmore/gwf3

commit 3ff6e70972397109edb17be73a9c1e1f9e90ebfc

Dog: fixed undefined variable in debug statement

core/module/Dog/dog_include/Dog_Init.php
@@ -204,7 +204,7 @@ final class Dog_Init
 			}
 			else
 			{
-				Dog_Log::debug(sprintf("Server %d-%s for Timer in path \"%s\" is not Online.", $serverid, $entry, $fullpath));
+				Dog_Log::debug(sprintf("Server %d-%s for Timer in path \"%s\" is not Online.", $server->getID(), $entry, $fullpath));
 			}
 		}
 		else

commit 565015f6561776c90f77e5623d978d70ca7bf2d3

Dog: fixed null dereference caused by commit 3ff6e70

core/module/Dog/dog_include/Dog_Init.php
@@ -198,13 +198,14 @@ final class Dog_Init
 	{
 		if (false !== ($server = Dog_Server::getByTLD($entry)))
 		{
-			if (false !== ($server = Dog::getServerByID($server->getID())))
+			$sid = $server->getID();
+			if (false !== ($server = Dog::getServerByID($sid)))
 			{
 				GWF_File::filewalker($fullpath, true, array(__CLASS__, 'initTimersDir'), false, $server);
 			}
 			else
 			{
-				Dog_Log::debug(sprintf("Server %d-%s for Timer in path \"%s\" is not Online.", $server->getID(), $entry, $fullpath));
+				Dog_Log::debug(sprintf("Server %d-%s for Timer in path \"%s\" is not Online.", $sid, $entry, $fullpath));
 			}
 		}
 		else