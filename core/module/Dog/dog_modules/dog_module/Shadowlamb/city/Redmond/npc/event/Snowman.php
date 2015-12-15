40a97098381e7cf516a16efb549feec7d1f1c528
Christian <gizmore@gizmore.org>
2015-12-15 01:05:22 +0100

    Snowman event is back

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Redmond/npc/event/Snowman.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Redmond/npc/event/Snowman.php
@@ -3,7 +3,7 @@ final class Redmond_Snowman extends SR_NPC
 {
 	public function getNPCLevel() { return 2; }
 	public function getNPCPlayerName() { return 'Snowman'; }
-	public function getNPCMeetPercent(SR_Party $party) { return 0.00; }
+	public function getNPCMeetPercent(SR_Party $party) { return 10.00; }
 	public function canNPCMeet(SR_Party $party) { return true; }
 	public function getNPCEquipment()
 	{
