11bd99c1cc20d58ed516ccb5abd9b04940840f88
gizmore <gizmore@wechall.net>
2016-10-11 11:25:44 +0200

    [WANDA] 2 pages typed.

---
+++ core/module/Wanda/content/book1/de/page2.php
@@ -0,0 +1,25 @@
+<h4>Wanda, die kleine Heuschrecke</h4>
+
+<p>
+Das einzige, woran sich Wanda, und ihr Stamm erinnern, wurde in den alten Kammern und Liedern festgehalten.
+</p>
+
+<?php echo $tVars['module']->wandaImage(1, 2, 1); ?>
+
+<p>
+Verstehen kann Wanda dies alles noch nicht.
+Wanda ist eines von vielen Kindern.
+Ihre Eltern sind sehr ehrliche und lebenslustige Heuschrecken.
+</p>
+
+<p>
+Sie hielten sich immer an die Regeln des Stammes, <em>ohne</em> diese in Frage zu stellen. 
+In ihren jungen Jahren interessierte dies Wanda alles noch nicht.
+Sie kümmerte sich lieber um das Spielen mit ihren Freunden.
+Wanda hat einen schönen grünen Hautpanzer und lernt gerne von den anderen das Zirpen, wie man über die Felder geht, und wie man den Gefahren der Steppe begegnet.
+Sie hörte immer zu, wenn sie mit den Älteren sprach, und nahm sich freundliches Zirpen zu Herzen.
+Schrillen begegnete sie zu dieser Zeit noch mit Angst.
+Am meisten Angst machten ihr jedoch die Warnungen der Alten.
+</p>
+
+<?php echo $tVars['module']->wandaImage(1, 2, 2); ?>
---
+++ core/module/Wanda/content/book1/de/pageBlank.php
@@ -0,0 +1,12 @@
+<h4>Wanda, die kleine Heuschrecke</h4>
+
+<p>
+<b>Wanda</b>, die kleine Heuschrecke, wurde jung geboren.
+</p>
+
+<?php echo $tVars['module']->wandaImage(1, 1, 1); ?>
+
+<p>
+Im abfressen ist Wanda's Stamm spezialist.
+</p>
+
