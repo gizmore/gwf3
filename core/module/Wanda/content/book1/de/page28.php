a8c636d3ead293cff0d17c554c2b90893d92e320
gizmore <gizmore@wechall.net>
2016-10-12 08:05:16 +0200

    [WANDA] Book1 typed.

---
+++ core/module/Wanda/content/book1/de/page28.php
@@ -0,0 +1,25 @@
+<p>
+Von den großen Heuspeichern der Menschen lassen sich die vier heute nicht aufhalten.
+Veronica bestimmt; es geht weiter in Richtung Oase.
+Langsam gehen die vier weiter.
+Immer wieder sieht Wanda zurück, doch mit vier Leuten dreht man nicht einfach um.
+Entscheidungen treffen sich fast wie von selbst, und bei schwierigen Dingen berät man sich.
+Vorsichtig aber fröhlich hüpfen sie weiter.
+Am Rande entdecken sie wieder etwas neues.
+Kleine bis mittelgroße Hügel, die wie Ringe auf dem Sand erscheinen.
+</p>
+
+<?php echo $tVars['module']->wandaImage(1, 28, 1); ?>
+
+<p>
+Olivia erinnert sich, so etwas schon einmal erzählt bekommen zu haben, und rät, lieber kehrt zu machen.
+Alle stimmen zu, und auf dem Rückweg halten nun alle Ausschau nach Ameisen.
+Es ist ein unausgesprochenes Geheimnis, was sie nun teilen.
+Zu sehen gibt es die Ameisen immer noch nicht.
+Vom sprechen ganz zu schweigen.
+</p>
+
+<p>
+«Wo mögen sie hin sein? Warum sind sie fort?», fragt sich Wanda.
+Am späten Nachmittag machen sie schliesslich kehrt und treffen abends erschöpft zu Hause ein.
+</p>
---
+++ core/module/Wanda/content/book1/de/page29.php
@@ -0,0 +1,62 @@
+<p>
+Dieser Herbst sollte der schlimmste in der <em>Geschichte des Fravier Stammes</em> werden. <!-- 3564020356 -->
+</p>
+
+<p>
+Angekündigt hat er sich durch immer heissere und heulende Winde.
+Angst bei schlechtem Wetter hatte Wanda schon immer.
+«Du brauchst keine Angst vor Blitz und Donner haben.», sagte Fritz Wandamann des öfteren. <!-- R.I.P my oldest father. -->
+Doch es war nicht der Blitz oder der Donner.
+Es war der Wind.
+Einfach nur Wind, der sogar in Kreisen über die Kornspeicher hinwegfegte, sie zeriss, und die ganze Ernte verteilte.
+</p>
+
+<?php echo $tVars['module']->wandaImage(1, 29, 1); ?>
+
+<p>
+Wanda hat es ebenfalls hart getroffen.
+Alleine draußen sucht sie Schutz unter einem Sandhügel.
+Der Wind pfeift so scharf, daß sie die Augen schliessen muss, und nur auf das Überleben hofft…
+Dann versinkt sie langsam im Sand und spürt den Wind kaum noch…
+</p>
+
+<p>
+Benommen blickt sie auf… in die Augen einer Ameise!
+Wanda erstarrt.
+</p>
+
+
+<p>
+Noch vor wenigen Tagen, da brannten ihr die Fragen geradezu auf ihren Chitin-Lippen.
+Nun, wo sie die Augen so plötzlich und nah vor sich sieht, stellt sie sich nur noch eine Frage…
+«Soll ich weglaufen?… Wohin?… Wo bin ich?!»
+</p>
+<p>
+Die Ameise weicht ein kleines Stück zurück…
+</p>
+
+
+
+
+
+<pre>
+Nachwort:
+Diese 29 Seiten widme ich allen meinen Freunden.
+Zu viele sind es, die ich nun aufzählen muss.
+Wer spass mit diesem ersten Band hatte, der darf uns gerne etwas Spenden.
+Auch wenn wir das meiste verfeiern werden, so erreicht es doch zu 100% die Künstler hinter diesem Werk.
+Ein zweiter Teil befindet sich in Arbeit.
+
+PS: Die Bilder sind von Anja Ingwersen, Danziger Str. 64 Cremlingen.
+DANKE!
+
+PPS: Dank an den Richter, der alles für mich bewahrte :)
+
+Christian Busch
+Am Bauhof 15
+31224 Peine
+Germany
+
+a.k.a.
+gizmore@wechall.net
+</pre>
