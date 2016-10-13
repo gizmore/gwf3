9c3662499b4794e10de5bcb83ebd24aae8c927c1
gizmore <gizmore@wechall.net>
2016-10-13 07:32:48 +0200

    [WANDA] Layout improvements.

--- www/tpl/default/module/Wanda/page.php
+++ www/tpl/default/module/Wanda/page.php
@@ -3,17 +3,14 @@
 	<?php echo $tVars['text']; ?>
 	
 	<div class="wanda-paginator">
-
-	<?php if ($tVars['prev']) { ?>
+		<?php if ($tVars['prev']) { ?>
 		<a class="wanda-prev-page" href="<?php echo $tVars['prev']; ?>">«</a>
-	<?php } ?>
-	
-	<?php if ($tVars['next']) { ?>
+		<?php } ?>
+		<?php if ($tVars['next']) { ?>
 		<a class="wanda-next-page" href="<?php echo $tVars['next']; ?>">»</a>
-	<?php } ?>
-
-	</div>
+		<?php } ?>
 	
-	<div id="wanda-book-page-badge"><?php echo $tVars['page']?></div>
+		<div id="wanda-book-page-badge"><?php echo $tVars['page']?></div>
+	</div>
 
 </div>
