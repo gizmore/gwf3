e103cc0d895e9caf5d257f13b961a6ceb2355522
gizmore <gizmore@wechall.net>
2016-10-13 07:17:26 +0200

    [WANDA] Page 31 typed. First book finished in german.

---
+++ core/module/Wanda/content/book2/de/page2.php
@@ -0,0 +1,36 @@
+<p>
+Es kommt Wanda vor wie eine Ewigkeit. Wie in Trance folgt sie der Ameise.
+</p>
+
+<p>
+«Ob ich hier je wieder hinausfinde?» – fragt sie sich.
+Plötzlich macht die Ameise halt, und Wanda öffnet die Augen.
+Sehen kann sie nichts, denn es ist stockdunkel.
+Sie schliesst wieder die Augen, und atmet tief durch.
+Die Ameise surrt tief und leise, ganz langsam, so daß Wanda nach kurzer Zeit einschläft; Tief im Ameisenbau.
+</p>
+
+<p>
+In dieser Nacht träumt Wanda.
+Es ist ein wirrer Traum, wo die Heuschrecken ihr Heu in der Erde lagern, damit es länger frisch bleibt.
+Es werden auch Erdlöcher gebaut, um der Sonne zu entrinnen, und Schutz vor dem Sturm zu finden.
+Wanda wacht erschrocken auf.
+</p>
+
+<?php echo $tVars['module']->wandaImage(2, 2, 1); ?>
+
+<p>
+Sie reisst die Augen auf, und kann etwas sehen.
+Kleine Lichter leuchten an der Wand und erinnern an Sterne.
+Ihre Ameise scheint nicht in der Nähe.
+</p>
+
+<p>
+Wanda untersucht die Wand, und die leuchtenden Punkte scheinen Pilze zu sein… sie drückt auf einen drauf.
+Langsam und behutsam erhöht sie den Druck, und die Farbe ändert sich leicht von blau auf grün.
+</p>
+
+<p>
+Wanda atmet tief ein.
+</p>
+
