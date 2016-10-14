d21ca478563007340efec02862c567067998c3bf
gizmore <gizmore@wechall.net>
2016-10-14 07:13:58 +0200

    [WANDA] Credits Scroller Text

---
+++ core/module/Wanda/method/Credits.php
@@ -0,0 +1,105 @@
+<?php
+final class Wanda_Credits extends GWF_Method
+{
+	public function scrollText()
+	{
+		$scroll_text = <<<EOC
+...........25............
+.Well that is a scroller.
+.                       .
+.       H               .
+.	            E       .
+.       R               .
+.               E       .
+.                       .
+.                       .
+.       W       E       .
+.		                .
+.         THANK!        .
+.                       .
+.      THE PEOPLE!      .
+.                       .
+.                       .
+.   WHO INSPIRED US!    .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.   WHO GAVE US GIFTS   .
+.                       .
+.                       .
+.                       .
+.                       .
+.     !BITWARRIORS!     .                  .
+.                       .
+.                       .
+.                       .
+.                       .
+.      FAIR LIGHT       .
+.         TLC           .
+.   WORLD O.F WONDERS   .
+.       SKID ROW        .
+.      BYTEBANDIT       .
+.         AS.D          .
+.      LUCAS ARTS       .
+.       BULLFROG        .
+.        E LITE         .
+.          Z            .
+.          Z            .
+.          Z            .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.     !!!TEACHERS!!!    .
+.                       .
+.       HERR MIEHE      .
+.       FRAU MORAWE     .
+.       HERR CLEMENS    .
+.       FRAU GÜNTHER    .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.   art is (c)by Anja   .
+.   Text by gizmore     .
+.........................
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+.                       .
+EOC;
+		return $scroll_text;
+	}
+	
+}
+
+
