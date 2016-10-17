a5b9f21e2aef86be774bfec4c202cdffb75c703a
gizmore <gizmore@wechall.net>
2016-10-17 06:29:54 +0200

    [Wanda] 15 pages available in english

--- core/module/Wanda/content/book1/en/page14.php
+++ core/module/Wanda/content/book1/en/page14.php
@@ -1,24 +1,24 @@
 <p>
-Der Frühling ist wunderschön, und die meiste Zeit verbringen die Freunde draussen im Schutz des Stammes.
-Beim Spielen finden sich neue Freunde, die sich anschliessen.
-Caroline ist kräftig gebaut und lacht gerne.
-Michelle ist etwas langsam, aber sie hat ein reines Herz.
-Die kleine Gruppe ist nun also auf sechs Heuschrecken angewachsen.
+The spring is wonderful, and the most time is spent by the friends outside, under protection of the tribe.
+On the playground they find new friends who follow happily.
+Caroline is a bit heavy and laughs with joy.
+Michelle is a bit slow, but has a pure heart.
+The small group has thus grown to six grasshoppers.
 </p>
 
 <p>
-Auch alleine streift Wanda nun ab und zu umher,
-und eines Tages wird ihre Sehnsucht zufällig erfüllt;
-Sie sieht eine Ameise. Ihre Ameise.
+Wanda is now also striving alone from time to time,
+and one day her passion is fulfiled.
+She sees an ant. Her ant.
 </p>
 
 <?php echo $tVars['module']->wandaImage(1, 14, 1); ?>
 
 <p>
-Sie scheint in einem Loch zu wohnen, und trägt allerlei Zeug hinein;
-Blätter, Steine, und andere Dinge, die sie aus der Ferne nicht richtig erkennen kann.
-Die Ameise scheint immer gleichgültig und starr zu schauen, wann immer Wanda sie ansieht.
-«Sind Ameisen wirklich so böse, wie Großmutter erzählt hat?» – denkt Wanda ängstlich.
-Sie würde die Ameise gerne fragen, aber die Ameise scheint schwer beschäftigt.
-Wie sie wohl heisst? <!-- Crying a bit again for Djengiz Kahn :')  I am sorry for what happened. -->
+It seems to live in a hole and carries a lot of things in and out;
+Leaves, Stones, and other things, which she cannot recognize from far behind.
+The ant always seems to look callous and cold, whenever Wanda looks at it.
+«Are ants really so evil as her grandma told?» – Wanda thinks a bit freightened.
+She would love to ask, but the ant seems very busy.
+What is her name?
 </p>
--- core/module/Wanda/content/book1/en/page15.php
+++ core/module/Wanda/content/book1/en/page15.php
@@ -1,33 +1,33 @@
 <p>
-Wanda ist heute besonders mutig.
-Früh morgens steht sie auf, schleicht sich an den Ameisenhügel, und…
-wartet…
+Today Wanda is really brave.
+Early in the morning, she sneaks to the ant hills, and…
+waits…
 </p>
 
 <p>
-Nach einiger Zeit krabbeln die ersten Ameisen aus dem Bau.
-Wanda will etwas sagen, aber für sie sehen alle Ameisen gleich aus.
-Sie kann sich nicht mehr genau erinnern, welche Ameise diesen starren Blick hatte, oder ob sie ihre Ameise je wiedererkennen würde.
+After some time, the first ants ripple out of their hill.
+Wanda wants to say something, but the ants look all alike to her.
+She cannot remember exactly which ant had this stare glaze look, or if she ever will recognize her special ant again.
 </p>
 
 <?php echo $tVars['module']->wandaImage(1, 15, 1); ?>
 
 <p>
-Langsam weicht Wanda zurück.
-Sie ist verunsichert.
-Soll sie einfach eine ansprechen?
-<em>So</em> böse sehen Ameisen nicht aus.
-Zumindest nicht, wenn man genau hinsieht.
-Wanda hat <em>so viele Fragen</em> an die Ameisen.
-Wenn sie nur auf eine direkte Frage eine direkte Antwort bekäme.
-Welche Frage würde sie wählen, wenn sie nur eine einzige stellen dürfte.
+Slowly wanda backs up.
+She is alienated.
+Should she just talk to a random ant.
+Ants do not look <em>that</em> evil.
+At least if you take a close look.
+Wanda got so many questions for the ants.
+If she just could ask a direct question and would get a clean answer.
+Which question would she choose, if she had just only one.
 </p>
 
 <p>
-Wanda ist überwältigt.
-Nur wenige Minuten war sie in Gedanken versunken, und plötzlich sind unzählbar viele Ameisen am Hügel zu sehen.
-Sie laufen links und rechts und in alle Richtungen in die Ferne.
-Wanda beschliesst umzukehren.
-Zurück zu ihrem Stamm.
-<em>Den Fraviers.</em>
+Wanda is overwhelmed.
+Only a few minutes she was lost in thoughts and suddenly there are countless ants to be seen at the hill.
+They run left and right and in all directions into the distance.
+Wanda decides to go home.
+Back to her tribe.
+<em>The Fraviers.</em>
 </p>
