7ab6bc6ee9b7327d539dc24852c7efef1d9e7824
gizmore <gizmore@wechall.net>
2016-10-19 07:36:40 +0200

    [ITMB] General impression

---
+++ core/module/ITMB/lang/itmb_de.php
@@ -0,0 +1,9 @@
+<?php
+$lang = array(
+		
+	'home_paragraph' => 'Willkommen bei IT Multiservice Busch in Peine.<br/>Ich habe ein Forum erstellt, einen Chatraum, sowie eine Seite über meine Angebote. Am besten gefiele mir eine größere Programmieraufgabe.',
+	'home_title' => '',
+		
+	'contact_chat_phrase' => 'You can also %s.',
+	'contact_chat_link' => 'chat with us',
+);
