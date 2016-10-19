4abe28b1700a6a1e43e64dde72c0e7014dd501db
gizmore <gizmore@wechall.net>
2016-10-19 08:23:57 +0200

    [Wanda] 4 pages left to translate to en.

--- core/module/Wanda/content/book1/en/page25.php
+++ core/module/Wanda/content/book1/en/page25.php
@@ -1,25 +1,27 @@
 <p>
-Am nächsten Morgen ist der Ärger vergessen.
-Zum ärgern ist auch keine Zeit, denn es muss geerntet werden.
-Wie gewohnt bekommen die kleinen das Feld in der Mitte zur Probe.
-Alle Felder stehen wieder in höchster Blüte, und die sieben können kaum abwarten, zu zeigen, was sie im letzten Jahr gelernt hatten.
-Ihre Zusammenarbeit läuft nun deutlich besser, aber es holpert immer noch hier und da.
-«Veronica achtet gar nicht darauf, wo die Blätter hinfallen…», denkt Wanda.
-Olivia und Maice sind emsig bei der Sache.
-Und…
-«WANDA!», ruft es von oben, «DU SOLLST STAPELN!»
+In the next morning the anger is gone.
+For anger there is no time anyway, because, it is harvest time.
+As usual the kids get the small field in the middle to practice.
+</p>
+
+<p>
+All fields bloom high, and the seven can't wait to show what they had learned over the last year.
+Their teamwork is now knowingly better, but it is still hopple here and there.
+«Veronica does not even look where the leafs are falling…», thinks Wanda.
+Olivia and Marcy are very busy.
+And…
+«WANDA!», it shouts from above, «YOU GOTTA STACK!»
 </p>
 
 <?php echo $tVars['module']->wandaImage(1, 25, 1); ?>
 
 <p>
-Wanda schreckt auf, und sieht einen Haufen voll unerledigter Arbeit.
-Sie macht sich an's Werk, und Alexandra, Caroline und Michelle wissen was zu tun ist.
-Sie tragen das Erntegut einfach, wie im letzten Jahr, lose richtung Heuspeicher. 
+Wanda shricks up and sees a load of undone work.
+She starts working, and Alexandra, Caroline and Michelle know what to do.
+They simply carry the harvested goods, like over the last year, loose in the haystacks direction.
 </p>
 
 <p>
-Wanda ist froh, daß sie sich auf ihre drei Träger verlassen kann.«»
-«Was für Probleme die Ameisen wohl haben?» – denkt sie –
-als sie die Halme nun mit einem Flechtboden anhäuft.
+Wanda is happy that she can count on her three carriers.
+«I wonder what problems ants got?» – she thinks – as she prepares the plad ground for the stalks.
 </p>
