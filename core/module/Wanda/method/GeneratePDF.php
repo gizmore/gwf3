d1c09687624d9c0de4e335b5adb068bb6c2a3f4f
gizmore <gizmore@wechall.net>
2016-11-09 05:19:42 +0100

    [TGC] wss proxy works now

---
+++ core/module/Wanda/method/GeneratePDF.php
@@ -0,0 +1,42 @@
+<?php
+final class Wanda_GeneratePDF extends GWF_Method
+{
+	public function getHTAccess()
+	{
+		return 'RewriteRule ^wanda/pdf/book/([0-9]+)/?$ index.php?mo=Wanda&me=GeneratePDF'.PHP_EOL;
+	}
+
+	public function execute()
+	{
+		GWF_Website::addJavascript(GWF_WEB_ROOT.'js/jsxm/xm.js');
+		GWF_Website::addJavascript(GWF_WEB_ROOT.'js/jsxm/xmeffects.js');
+		GWF_Website::addJavascript(GWF_WEB_ROOT.'js/jsxm/xmgfx.js');
+		GWF_Website::addJavascript(GWF_WEB_ROOT.'tpl/wanda/js/wanda_credits_text.js');
+		GWF_Website::addJavascript(GWF_WEB_ROOT.'tpl/wanda/js/wanda_credits.js');
+		GWF_Website::addCSS(GWF_WEB_ROOT.'tpl/wanda/css/credits.css');
+		$tVars = array(
+		// 			'rand' => GWF_Random::rand(0, 65535),
+		// 			'scrollText' => $this->scrollText(),
+		// 			'width' => $this->gridWidth(),
+		// 			'height' => $this->gridHeight(),
+		);
+		return $this->module->templatePHP('credits.php', $tVars);
+	}
+
+	###############
+	### private ###
+	###############
+	private function gridWidth()
+	{
+		return strpos($this->scrollText(), "\n");
+	}
+
+	private function gridHeight()
+	{
+		$w = $this->gridWidth() + 1;
+		return (int)(mb_strlen($this->scrollText()) / $w);
+	}
+
+}
+
+
--- www/gwf_cronjob.sh
+++ www/gwf_cronjob.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cd www
 php gwf_cronjob.php
