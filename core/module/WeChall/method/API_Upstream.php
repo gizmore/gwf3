e280d5e2968cf69e688d1daf7284587c75cc1b24
gizmore <gizmore@wechall.net>
2017-11-09 17:45:37 +0100

    [WeChall] New bot API very similiar to APIHistory2. (thx tehron)

---
+++ core/module/WeChall/method/API_Upstream.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Very similiar to WC_HistoryUser2.
+ * Show latest regat activity.  
+ * @author gizmore
+ */
+final class WeChall_API_Upstream extends GWF_Method
+{
+	public function execute()
+	{
+		if (false === ($time = Common::getGetInt('time', false)))
+		{
+			http_response_code(405);
+			die('The mandatory parameter \'time\' is not set. Try \'&time=0\'.');
+		}
+		if (false === ($lastid = Common::getGetInt('lastid', false)))
+		{
+			http_response_code(405);
+			die('The mandatory parameter \'lastid\' is not set. Try \'&lastid=0\'.');
+		}
+		
+		$limit = Common::clamp(Common::getGetInt('limit', 10), 1, 100);
+		
+		$conditions = "userhist_date>=$time AND regat_options&4=0";
+		$users = GWF_TABLE_PREFIX.'user';
+		$sites = GWF_TABLE_PREFIX.'wc_site';
+		$regat = GWF_TABLE_PREFIX.'wc_regat';
+		$query =
+		"SELECT userhist_date, userhist_sid, userhist_type, user_name, site_name, site_classname,".
+		" userhist_onsiterank, userhist_onsitescore, site_maxscore,".
+		" userhist_totalscore, userhist_gain_score, regat_options, ".
+		" ((SELECT COUNT(*) FROM $users u2 WHERE u2.user_level>u.user_level AND u2.user_options&0x10000000=0) + 1) as global_rank ".
+		"FROM ".GWF_TABLE_PREFIX.'wc_user_history2 '.
+		"INNER JOIN $users u ON u.user_id=userhist_uid ".
+		"INNER JOIN $sites ON site_id=userhist_sid ".
+		"INNER JOIN $regat ON regat_uid=userhist_uid AND regat_sid=userhist_sid ".
+		"WHERE $conditions ".
+		"ORDER BY userhist_date DESC ".
+		"LIMIT $limit";
+		if (false === ($result = gdo_db()->queryRead($query)))
+		{
+			http_response_code(405);
+			return GWF_HTML::err('ERR_DATABASE', array(__FILE__, __LINE__));
+		}
+// 		die($query);
+		
+		require_once GWF_CORE_PATH.'module/WeChall/WC_RegAt.php';
+		$regats = GDO::table('WC_RegAt');
+		$json = array(
+			'result' => array(),
+			'time' => null,
+			'lastid' => null,
+		);
+		$foundSid = false;
+		while ($regat = $regats->fetch($result, GDO::ARRAY_O))
+		{
+			$t = (int)$regat->getVar('userhist_date');
+			
+			if ($t === $time)
+			{
+				$sid = (int)$regat->getVar('userhist_sid');
+				$foundSid = $sid === $lastid ? true : $foundSid;
+				if ($foundSid)
+				{
+					continue;
+				}
+			}
+			
+			$after = (int)$regat->getVar('userhist_totalscore');
+			$gain = (int)$regat->getVar('userhist_gain_score');
+			$before = $after - $gain;
+			$json['result'][] = array(
+				'username' => $regat->getVar('user_name'),
+				'globalrank' => $regat->getVar('global_rank'),
+				'type' => $regat->getVar('userhist_type'),
+				'datetime' => $regat->getVar('userhist_date'),
+				'sitename' => $regat->getVar('site_name'),
+				'siterank' => $regat->getVar('userhist_onsiterank'),
+				'sitescore' => $regat->getVar('userhist_onsitescore'),
+				'siteclass' => $regat->getVar('site_classname'),
+				'before' => $before,
+				'after' => $after,
+				'gain' => array(
+					'score' => $gain,
+					'percent' => $before != 0 ? round(($after/$before-1.00)*100, 2) : null,
+				),
+			);
+			if ($json['time'] === null)
+			{
+				$json['time'] = $t;
+				$json['lastid'] = (int)$regat->getVar('userhist_sid');
+			}
+		}
+
+		# Output
+		$json['result'] = $json['result'];
+		$out = json_encode($json);
+		header('Content-Type: application/json; charset=UTF-8');
+		echo $out;
+		die();
+	}
+}
