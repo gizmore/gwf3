3d51153122c297b2dfd66ca18054ddee928ee85f
gizmore <gizmore@wechall.net>
2018-08-07 17:12:28 +0200

    2 more donations

--- www/tpl/default/module/WeChall/donations.php
+++ www/tpl/default/module/WeChall/donations.php
@@ -34,13 +34,13 @@ Sum:    €300,00
 Goal:   €350,00
 </pre>
 
-<pre>Donations 2018:    1 donation
-Sum:    €500,00
+<pre>Donations 2018:    3 donations
+Sum:    €555,00
 Goal:   €350,00
 </pre>
 
-<pre>Totals:            8 donations
-Sum:    €800,00
+<pre>Totals:           10 donations
+Sum:    €855,00
 Goal:   €700,00
 </pre>
 
@@ -70,6 +70,8 @@ We currently have the following ca. expenses for WeChall:<br/>
 <li>8.Dec.2017 – <em>&quot;Awesome work guys - glad to be part of the community!&quot;</em> – <span class="donor">benito255</span></li>
 <li>----- 2018 -----</li>
 <li>28.Jan.2018 – <em>&quot;37K users and only 8 donations?! Shame on you!&quot;</em> – ???</li>
+<li>31.Jul.2018 - <em>&quot;You folks do good work.&quot;</em> - ???</li>
+<li>1.Aug.2018 - <em>&quot;That's nothing but here's some €€€ for this awesome website!&quot;</em> - ???</li>
 </ol>
 </div>
 
