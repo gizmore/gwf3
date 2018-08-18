fb8aefb17d2c793f1101f0eca86196fd90065a2e
dloser <dloser@hotmail.com>
2018-08-18 18:02:06 +0200

    [DOG] removed obsolete SVN timer

--- core/module/Dog/dog_timer/all_servers_one/timers/infinity/120/svn_gwf.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once DOG_PATH.'dog_lib/Dog_SVN_Info.php';
-dog_svn_info('GWF3-SVN', 'https://80.246.62.155/GWF3/', '', '', 'http://trac.gwf3.gizmore.org/changeset/%REV%', array('#shadowlamb', '#wechall', '#sr'));
-?>
