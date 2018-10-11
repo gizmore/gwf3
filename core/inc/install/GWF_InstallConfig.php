gizmore/gwf3

commit a4dfe678594fc5563c98e447a6d7a303702049c2

[GWF] cleanup and bugfixes.

core/inc/install/GWF_InstallConfig.php
@@ -208,35 +208,35 @@ final class GWF_InstallConfig
 		$temp = array(
 			# Main
 			array('text',   'Main', 'GWF_DOMAIN', $domain, 'Example: \'www.foobar.com\'.'),
-			array('text',   'Main', 'GWF_SITENAME', 'Gizmore Website Framework', 'Your Site`s name. htmlspecialchars() it yourself.'),
+			array('text',   'Main', 'GWF_SITENAME', 'GWF', 'Your Site`s name. htmlspecialchars() it yourself.'),
 			array('text',   'Main', 'GWF_WEB_ROOT_NO_LANG', $self, 'Add trailing and leading slash. Example: \'/\' or \'/mywebdir/\'.'),
 			array('text',   'Main', 'GWF_DEFAULT_DOCTYPE', 'html5', 'Set the default html-doctype for gwf. Modules can change it.'),
 			array('int10',  'Main', 'GWF_LOG_BITS', 0xfff, 'bitmask for logging: NONE = 0; GWF_WARNING = 0x01; GWF_MESSAGE = 0x02; GWF_ERROR = 0x04; GWF_CRITICAL = 0x08; PHP_ERROR = 0x10; DB_ERROR = 0x20; SMARTY = 0x40; HTTP_ERROR = 0x80; HTTP_GET = 0x100; HTTP_POST = 0x200; IP = 0x400;'),
 
 			# 3rd Party
-			array('text', '3rd Party', 'GWF_SMARTY_PATH', GWF_SMARTY_PATH, 'Path to Smarty.class.php. Smarty replaced the GWF template engine and has to be available.'),
-			array('text', '3rd Party', 'GWF_JPGRAPH_PATH', '/opt/php/jphraph/jpgraph.php', 'Path to jpgraph.php. JPGraph is a library to draw graphs with php. It is available under the GPL.'),
+#			array('text', '3rd Party', 'GWF_SMARTY_PATH', GWF_SMARTY_PATH, 'Path to Smarty.class.php. Smarty replaced the GWF template engine and has to be available.'),
+#			array('text', '3rd Party', 'GWF_JPGRAPH_PATH', '/opt/php/jphraph/jpgraph.php', 'Path to jpgraph.php. JPGraph is a library to draw graphs with php. It is available under the GPL.'),
 			array('text', '3rd Party', 'GWF_GESHI_PATH', '/opt/php/geshi/geshi.php', 'Path to geshi.php. GeSHi is a GPL licensed Syntax highlighter.'),
 
 			# Smarty
-			array('text', 'Smarty', 'GWF_SMARTY_DIRS', $path.'extra/temp/smarty/', 'Path to smarty directories for cache, config and compiling. With trailing slash.'),
-			array('bool', 'Smarty', 'GWF_ERRORS_TO_SMARTY', true, 'Errors and Messsages are added to a smarty templatevar. If not set it is added before Pagecontent.'),
+#			array('text', 'Smarty', 'GWF_SMARTY_DIRS', $path.'extra/temp/smarty/', 'Path to smarty directories for cache, config and compiling. With trailing slash.'),
+#			array('bool', 'Smarty', 'GWF_ERRORS_TO_SMARTY', true, 'Errors and Messsages are added to a smarty templatevar. If not set it is added before Pagecontent.'),
 
 			# Defaults
 			array('text', 'Defaults', 'GWF_DEFAULT_LANG', 'en', 'Fallback language. Should be \'en\'.'),
-			array('text', 'Defaults', 'GWF_DEFAULT_MODULE', 'GWF', '1st visit module. Example: \'MyModule\'.'),
+			array('text', 'Defaults', 'GWF_DEFAULT_MODULE', 'GWF', '1st visit module. Example: \'Wanda\'.'),
 			array('text', 'Defaults', 'GWF_DEFAULT_METHOD', 'About', '1st visit method. Example: \'Home\'.'),
-			array('text', 'Defaults', 'GWF_DEFAULT_DESIGN', 'default', 'Default design. Example: \'default\'.'),
-			array('text', 'Defaults', 'GWF_ICON_SET', 'default', 'Default Icon-Set. Example: \'default\'.'),
+			array('text', 'Defaults', 'GWF_DEFAULT_DESIGN', 'default', 'Default design. Example: \'wanda\'.'),
+#			array('text', 'Defaults', 'GWF_ICON_SET', 'default', 'Default Icon-Set. Example: \'default\'.'),
 			array('text', 'Defaults', 'GWF_DOWN_REASON', 'Converting the database atm. should be back within 45 minutes.', 'The Message if maintainance-mode is enabled.'),
 			
 			# Language
-			array('text', 'Language', 'GWF_LANG_ADMIN', 'en', 'Admins language. Should be \'en\'.'),
-			array('text', 'Language', 'GWF_SUPPORTED_LANGS', 'en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr;lv', 'Separate 2 char ISO codes by semicolon. Currently (partially) Supported: en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr'),
+#			array('text', 'Language', 'GWF_LANG_ADMIN', 'en', 'Admins language. Should be \'en\'.'),
+#			array('text', 'Language', 'GWF_SUPPORTED_LANGS', 'en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr;lv', 'Separate 2 char ISO codes by semicolon. Currently (partially) Supported: en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr'),
 			
 			# Various
-			array('int10', 'Various', 'GWF_ONLINE_TIMEOUT', 60, 'A request will mark you online for N seconds.'),
-			array('int10', 'Various', 'GWF_CRONJOB_BY_WEB', 0, 'Chance in permille to trigger cronjob by www clients (0-1000)'),
+			array('int10', 'Various', 'GWF_ONLINE_TIMEOUT', 300, 'A request will mark you online for N seconds.'),
+			array('int10', 'Various', 'GWF_CRONJOB_BY_WEB', 1, 'Chance in permille to trigger cronjob by www clients (0-1000)'),
 			array('bool',  'Various', 'GWF_USER_STACKTRACE', true, 'Show stacktrace to the user on error? Example: true.' ),
 			
 			# Database
@@ -252,15 +252,15 @@ final class GWF_InstallConfig
 			
 			# Session
 			array('text',  'Session', 'GWF_SESS_NAME', 'GWF', 'Cookie Prefix. Example: \'GWF\'.'),
-			array('int10', 'Session', 'GWF_SESS_LIFETIME', 60*240, 'Session lifetime in seconds.'),
-			array('int10',  'Session', 'GWF_SESS_PER_USER', '1', 'Number of allowed simultanous sessions per user. Example: 1'),
+			array('int10', 'Session', 'GWF_SESS_LIFETIME', 8*60*60 'Session lifetime in seconds.'),
+			array('int10',  'Session', 'GWF_SESS_PER_USER', '2', 'Number of allowed simultanous sessions per user. Example: 1'),
 			
 			# IP
 			array('text',  'IP', 'GWF_IP_QUICK', 'hash_32_1', 'Hashed IP Duplicates. See core/inc/util/GWF_IP6.php'),
 			array('text',  'IP', 'GWF_IP_EXACT', 'bin_32_128', 'Complete IP storage. See core/inc/util/GWF_IP6.php'),
 			
 			# EMail
-			array('int10', 'EMail', 'GWF_DEBUG_EMAIL', 15, 'Send Mail on errors? 0=NONE, 1=DB ERRORS, 2=PHP_ERRORS, 4=404, 8=403, 16=MailToScreen)'),
+			array('int10', 'EMail', 'GWF_DEBUG_EMAIL', 15, 'Send Mail on errors? 0=NONE, 1=DB ERRORS, 2=PHP_ERRORS, 4=404, 8=403, 16=MailToScreen, 31=DEBUG)'),
 			array('text',  'EMail', 'GWF_BOT_EMAIL', 'robot@'.$domain, 'Robot sender email. Example: robot@www.site.com.'),
 			array('text',  'EMail', 'GWF_ADMIN_EMAIL', isset($_SERVER['SERVER_ADMIN']) ? $_SERVER['SERVER_ADMIN'] : 'admin@'.$domain, 'Hardcoded admin mail. Example: admin@www.site.com.'),
 			array('text',  'EMail', 'GWF_SUPPORT_EMAIL', 'support@'.$domain, 'Support email. Example: support@www.site.com.'),

GWF3.php
@@ -1,6 +1,14 @@
 <?php
 define('GWF_DEBUG_TIME_START', microtime(true));
-define('GWF_CORE_VERSION', '3.04-2012.Apr.1');
+define('GWF_CORE_VERSION', '4.00-2016.Nov.7');
+
+# Easy autoconfig and removed from installer.
+if (!defined('GWF_LANG_ADMIN')) define('GWF_LANG_ADMIN', 'en');
+if (!defined('GWF_ERRORS_TO_SMARTY')) define('GWF_ERRORS_TO_SMARTY', true);
+if (!defined('GWF_ICON_SET')) define('GWF_ICON_SET', 'default');
+if (!defined('GWF_SMARTY_PATH')) define('GWF_SMARTY_PATH', GWF_CORE_PATH.'inc/3p/smarty/Smarty.class.php');
+if (!defined('GWF_SMARTY_DIRS')) define('GWF_SMARTY_DIRS', GWF_PATH.'extra/temp/smarty/');
+if (!defined('GWF_SUPPORTED_LANGS')) define('GWF_SUPPORTED_LANGS', 'en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr;lv');
 
 /**
  * Mini GWF3 loader.
@@ -74,4 +82,3 @@ class GWF3
 		define('GWF_WEB_ROOT', $root);
 	}
 }
-?>

core/inc/install/data/db_backup.example.sh
@@ -6,6 +6,8 @@
 #
 # @author: gizmore
 #
+cd "$(dirname "$0")"
+
 ###################
 ### DB settings ###
 ###################

core/inc/util/GWF_Method.php
@@ -18,6 +18,7 @@ abstract class GWF_Method
 
 	public function __construct(GWF_Module $module) { $this->module = $module; return $this; }
 	public abstract function execute();
+	public function executeAjax() {}
 	public function getModule() { return $this->module; }
 	public function getLang() { return $this->module->getLang(); }
 	public function getUserGroups() { return NULL; }
@@ -43,8 +44,7 @@ abstract class GWF_Method
 			return $cache;
 		}
 
-
-		return $this->execute();
+		return $_REQUEST['_ajax'] ? $this->executeAjax() : $this->execute();
 	}
 
 	public function getETag() { return false; }

core/module/Category/GWF_Category.php
@@ -153,7 +153,7 @@ final class GWF_Category extends GWF_Tree
 //		foreach ($cats as $cat)
 //		{
 //			$catid = $cat->getID();
-//			$text = $cat->getTranslatedText($langid);
+//			$text = $cat->getTranslation($langid);
 //			$sel = $value === $catid ? ' selected="selected"' : '';
 //			$back .= sprintf('<option value="%s"%s>%s</option>', $catid, $sel, $text);
 //		}
@@ -161,20 +161,6 @@ final class GWF_Category extends GWF_Tree
 //		$back .= '</select>';
 //		return $back;
 //	}
-	
-	public function getTranslatedText($langid=0)
-	{
-		$langid = (string) $langid;
-		if ($langid === '0') {
-			$langid = (string) GWF_Language::getCurrentID();
-		}
-		if (isset($this->gdo_data['translations'][$langid])) {
-			return $this->gdo_data['translations'][$langid]['cl_translation'];
-		}
-		else {
-			return $this->getVar('key for '.$langid);
-		}
-	}
 	
 	public static function categoryExists($catid)
 	{

core/module/News/GWF_News.php
@@ -285,7 +285,7 @@ final class GWF_News extends GDO
 		{
 			return GWF_HTML::lang('no_category');
 		}
-		return $cat->getTranslatedText($langid);
+		return $cat->getTranslation($langid);
 	}
 	
 	public function displayCategory()
@@ -348,7 +348,7 @@ final class GWF_News extends GDO
 			
 			$cat->loadTranslations();
 			
-			$back[] = $cat->getTranslatedText($langid);
+			$back[] = $cat->getTranslation($langid);
 		}
 		return $back;
 	}

core/module/News/lang/news_de.php
@@ -127,6 +127,8 @@ $lang = array(
 	# V3.00 (fixes)
 	'rss_img_title' => GWF_SITENAME.'  Logo',
 	'cfg_news_comments' => 'Kommentare aktivieren',
+		
+	# v3.01 (deletion)
+	'msg_deleted' => 'Die News wurde als gelöscht markiert.',
+	'err_newsid' => 'Der newsid Parameter wird erwarten.',
 );
-
-?>

core/module/News/method/Delete.php
@@ -1,0 +1,51 @@
+<?php
+final class News_Delete extends GWF_Method
+{
+	private $newsID;
+	
+	private function validate()
+	{
+		if (false === ($this->newsID = Common::getPostString('newsid')))
+		{
+			return $this->module->error('err_newsid');
+		}
+	}
+		
+	
+	
+	public function deleteNewsByID($newsid)
+	{
+		if (false === ($news = GWF_News::getNewsItem($newsid)))
+		{
+			return $this->module->error('err_news');
+		}
+		
+		return $this->deleteNews($news);
+	}
+		
+	public function deleteNews($news)
+	{
+		if (!$news->delete()) {
+		}
+		return true;
+	}
+	
+	
+	public function execute()
+	{
+		if (false !== ($error = $this->validate())) {
+			return $error;
+		}
+		
+		if (true !== ($result = $this->deleteNewsByID($this->newsID))) {
+			
+		}
+		
+		
+		GWF_Website::redirect('/news');
+	}
+	
+	public function executeAjax()
+	{
+	}
+}

commit b12801a4990de8364f04505382eae88914a07b84

GWF3: Fix installation wizard due to missing config.php variables
Errors introduced in a4dfe678594fc5563c98e447a6d7a303702049c2.

core/inc/install/GWF_InstallConfig.php
@@ -214,25 +214,25 @@ final class GWF_InstallConfig
 			array('int10',  'Main', 'GWF_LOG_BITS', 0xfff, 'bitmask for logging: NONE = 0; GWF_WARNING = 0x01; GWF_MESSAGE = 0x02; GWF_ERROR = 0x04; GWF_CRITICAL = 0x08; PHP_ERROR = 0x10; DB_ERROR = 0x20; SMARTY = 0x40; HTTP_ERROR = 0x80; HTTP_GET = 0x100; HTTP_POST = 0x200; IP = 0x400;'),
 
 			# 3rd Party
-#			array('text', '3rd Party', 'GWF_SMARTY_PATH', GWF_SMARTY_PATH, 'Path to Smarty.class.php. Smarty replaced the GWF template engine and has to be available.'),
+			array('text', '3rd Party', 'GWF_SMARTY_PATH', GWF_SMARTY_PATH, 'Path to Smarty.class.php. Smarty replaced the GWF template engine and has to be available.'),
 #			array('text', '3rd Party', 'GWF_JPGRAPH_PATH', '/opt/php/jphraph/jpgraph.php', 'Path to jpgraph.php. JPGraph is a library to draw graphs with php. It is available under the GPL.'),
 			array('text', '3rd Party', 'GWF_GESHI_PATH', '/opt/php/geshi/geshi.php', 'Path to geshi.php. GeSHi is a GPL licensed Syntax highlighter.'),
 
 			# Smarty
-#			array('text', 'Smarty', 'GWF_SMARTY_DIRS', $path.'extra/temp/smarty/', 'Path to smarty directories for cache, config and compiling. With trailing slash.'),
-#			array('bool', 'Smarty', 'GWF_ERRORS_TO_SMARTY', true, 'Errors and Messsages are added to a smarty templatevar. If not set it is added before Pagecontent.'),
+			array('text', 'Smarty', 'GWF_SMARTY_DIRS', $path.'extra/temp/smarty/', 'Path to smarty directories for cache, config and compiling. With trailing slash.'),
+			array('bool', 'Smarty', 'GWF_ERRORS_TO_SMARTY', true, 'Errors and Messsages are added to a smarty templatevar. If not set it is added before Pagecontent.'),
 
 			# Defaults
 			array('text', 'Defaults', 'GWF_DEFAULT_LANG', 'en', 'Fallback language. Should be \'en\'.'),
 			array('text', 'Defaults', 'GWF_DEFAULT_MODULE', 'GWF', '1st visit module. Example: \'Wanda\'.'),
 			array('text', 'Defaults', 'GWF_DEFAULT_METHOD', 'About', '1st visit method. Example: \'Home\'.'),
 			array('text', 'Defaults', 'GWF_DEFAULT_DESIGN', 'default', 'Default design. Example: \'wanda\'.'),
-#			array('text', 'Defaults', 'GWF_ICON_SET', 'default', 'Default Icon-Set. Example: \'default\'.'),
+			array('text', 'Defaults', 'GWF_ICON_SET', 'default', 'Default Icon-Set. Example: \'default\'.'),
 			array('text', 'Defaults', 'GWF_DOWN_REASON', 'Converting the database atm. should be back within 45 minutes.', 'The Message if maintainance-mode is enabled.'),
 			
 			# Language
 #			array('text', 'Language', 'GWF_LANG_ADMIN', 'en', 'Admins language. Should be \'en\'.'),
-#			array('text', 'Language', 'GWF_SUPPORTED_LANGS', 'en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr;lv', 'Separate 2 char ISO codes by semicolon. Currently (partially) Supported: en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr'),
+			array('text', 'Language', 'GWF_SUPPORTED_LANGS', 'en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr;lv', 'Separate 2 char ISO codes by semicolon. Currently (partially) Supported: en;de;fr;it;pl;hu;es;bs;et;fi;ur;tr;sq;nl;ru;cs;sr'),
 			
 			# Various
 			array('int10', 'Various', 'GWF_ONLINE_TIMEOUT', 300, 'A request will mark you online for N seconds.'),