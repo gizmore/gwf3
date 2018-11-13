e91695986d58e308b5136c3b7b54ba8452743f1d
spaceone <space@wechall.net>
2018-11-13 17:38:59 +0100

    GWF3: fix crash if Links module is not correctly installed (GWF install)
    PHP Fatal Error(1): Call to a member function countUnread() on a non-object in core/inc/util/GWF_Notice.php line 34

--- core/inc/util/GWF_Notice.php
+++ core/inc/util/GWF_Notice.php
@@ -31,7 +31,7 @@ final class GWF_Notice
 			$links = GWF_Module::loadModuleDB('Links');
 			$links instanceof Module_Links;
 
-			if (0 < ((int)$unread = $links->countUnread($user)))
+			if ($links && 0 < ((int)$unread = $links->countUnread($user)))
 			{
 				return sprintf($pattern, $unread);
 			}
