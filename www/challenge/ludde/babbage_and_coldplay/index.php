6b97c0cb2767f01d28422ed3012bd5bbe8e42529
spaceone <space@wechall.net>
2018-11-26 18:16:25 +0100

    WC: remove hash

--- www/challenge/ludde/babbage_and_coldplay/index.php
+++ www/challenge/ludde/babbage_and_coldplay/index.php
@@ -5,7 +5,7 @@ require_once('challenge/html_head.php');
 require_once GWF_CORE_PATH.'module/WeChall/solutionbox.php';
 if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE)))
 {
-	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 3, 'challenge/ludde/babbage_and_coldplay/index.php', 'fe91bf59f3a2476d45f78ef00701a24b');
+	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 3, 'challenge/ludde/babbage_and_coldplay/index.php');
 }
 $chall->showHeader();
 # -------------------------- #
