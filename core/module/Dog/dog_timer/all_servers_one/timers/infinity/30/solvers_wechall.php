f18e6a3c1c8a849884a29b0465ed45c61dff5541
spaceone <space@wechall.net>
2019-03-06 20:47:04 +0100

    Lamb: increase announcement of wechall maximum solvers to 700

--- core/module/Dog/dog_timer/all_servers_one/timers/infinity/30/solvers_wechall.php
+++ core/module/Dog/dog_timer/all_servers_one/timers/infinity/30/solvers_wechall.php
@@ -4,5 +4,5 @@ require_once DOG_PATH.'dog_lib/Dog_WC_Solvers.php';
 $url = 'http://www.wechall.net/index.php?mo=WeChall&me=API_ChallSolved&ajax=true&datestamp=%DATE%&no_session=true';
 $format = "%1\$s \x02%2\$s\x02 has just solved \x02%3\$s\x02. This challenge has been solved %4\$d times. (https://www.wechall.net/%5\$s)";
 
-dog_wc_solvers('WC', '[WeChall]', $url, array('#wechall'), 200, $format);
+dog_wc_solvers('WC', '[WeChall]', $url, array('#wechall'), 700, $format);
 ?>
