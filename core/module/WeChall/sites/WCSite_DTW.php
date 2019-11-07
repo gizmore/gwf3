01dea26bc3964e4c473f84b4c4337d3e606e6621
gizmore <gizmore@wechall.net>
2019-11-07 20:39:56 +0100

    Rename HTS to DTW

---
+++ core/module/WeChall/sites/WCSite_DTW.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Hack This Site
+ */
+final class WCSite_DTW extends WC_Site
+{
+	public function parseStats($url)
+	{
+		if (false === ($result = GWF_HTTP::getFromURL($url, false))) {
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		$stats = explode(":", $result);
+		if (count($stats) !== 4) {
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+		$onsitescore = intval($stats[2]);
+		$score = intval($stats[1]);
+		$rankname = $stats[0]; 
+		$usercount = 39500;
+		$challcount = 102; #intval($stats[3]);
+		
+		if ( ($score > ($onsitescore * 2)) || ($challcount <= 2) || ($onsitescore < 0))
+		{
+			return htmlDisplayError(WC_HTML::lang('err_response', array(GWF_HTML::display($result), $this->displayName())));
+		}
+		
+		return array($score, -1, -1, $onsitescore, $usercount, $challcount);
+	}
+}
+?>
