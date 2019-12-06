38826175cb31811bbfc12382d1faacd77482b21e
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-06 02:33:46 +0100

    Update AresViper11.php

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/item/fireweapon/pistols/AresViper11.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/item/fireweapon/pistols/AresViper11.php
@@ -1,14 +1,14 @@
 <?php
 final class Item_AresViper11 extends SR_Pistol
 {
-	public function getAttackTime() { return 40; }
-	public function getAmmoName() { return 'Ammo_11mm'; }
-	public function getBulletsMax() { return 8; }
-	public function getBulletsPerShot() { return 1; }
-	public function getReloadTime() { return 75; }
+	public function getAttackTime() { return 35; }
+	public function getAmmoName() { return 'Ammo_7mm'; }
+	public function getBulletsMax() { return 36; }
+	public function getBulletsPerShot() { return 3; }
+	public function getReloadTime() { return 55; }
 	public function getItemLevel() { return 13; }
-	public function getItemWeight() { return 1950; }
-	public function getItemPrice() { return 750; }
+	public function getItemWeight() { return 1050; }
+	public function getItemPrice() { return 950; }
 	public function getItemDescription() { return 'A russian modification of the Ares Viper. It is illegal.'; }
 	public function getItemRequirements() { return array('firearms'=>2,'pistols'=>2); }
 	public function getItemModifiersA(SR_Player $player)
