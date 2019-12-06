75c5b2326e0594804944920cc759e7c211f8e407
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-06 02:32:31 +0100

    Update AresViper.php
    It is a sliver-gun, not a bullet blaster, like all the others.

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/item/fireweapon/pistols/AresViper.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/item/fireweapon/pistols/AresViper.php
@@ -1,14 +1,14 @@
 <?php
 final class Item_AresViper extends SR_Pistol
 {
-	public function getAttackTime() { return 30; }
-	public function getAmmoName() { return 'Ammo_9mm'; }
-	public function getBulletsMax() { return 8; }
-	public function getBulletsPerShot() { return 1; }
-	public function getReloadTime() { return 55; }
+	public function getAttackTime() { return 18; }
+	public function getAmmoName() { return 'Ammo_5mm'; }
+	public function getBulletsMax() { return 36; }
+	public function getBulletsPerShot() { return 3; }
+	public function getReloadTime() { return 45; }
 	public function getItemLevel() { return 8; }
-	public function getItemWeight() { return 1350; }
-	public function getItemPrice() { return 650; }
+	public function getItemWeight() { return 1050; }
+	public function getItemPrice() { return 675; }
 	public function getItemDescription() { return 'The famous "Ares Viper(tm)" pistol.'; }
 	public function getItemRequirements() { return array('firearms'=>2); }
 	public function getItemModifiersA(SR_Player $player)
