90fe93091025ba130c6865041dfcb311e0441b5b
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-06 11:46:28 +0100

    Update AresViper.php

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/item/fireweapon/pistols/AresViper.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/item/fireweapon/pistols/AresViper.php
@@ -3,11 +3,11 @@ final class Item_AresViper extends SR_Pistol
 {
 	public function getAttackTime() { return 18; }
 	public function getAmmoName() { return 'Ammo_5mm'; }
-	public function getBulletsMax() { return 36; }
+	public function getBulletsMax() { return 24; }
 	public function getBulletsPerShot() { return 3; }
-	public function getReloadTime() { return 45; }
+	public function getReloadTime() { return 35; }
 	public function getItemLevel() { return 8; }
-	public function getItemWeight() { return 1050; }
+	public function getItemWeight() { return 950; }
 	public function getItemPrice() { return 675; }
 	public function getItemDescription() { return 'The famous "Ares Viper(tm)" pistol.'; }
 	public function getItemRequirements() { return array('firearms'=>2); }
