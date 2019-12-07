c328db493d8d2e9d8459c17c3cf40f492d9eddd7
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-07 21:14:45 +0100

    Update Seattle.php

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Seattle/Seattle.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/city/Seattle/Seattle.php
@@ -4,7 +4,7 @@ final class Seattle extends SR_City
 	const TIME_TO_DELAWARE = 350;
 	const TIME_TO_VEGAS = 1050;
 	
-	public function getImportNPCS() { return array('Redmond_Cyberpunk','Redmond_Ork','Redmond_Ueberpunk','Redmond_Burglar','Redmond_Prostitute', 'Redmond_Snowman'); }
+	public function getImportNPCS() { return array('Redmond_Cyberpunk','Redmond_Ork','Redmond_Ueberpunk','Redmond_Burglar','Redmond_Prostitute', 'Seattle_Prostitute', 'Seattle_Stalker', 'Seattle_Mugger', Redmond_Snowman'); }
 	public function getArriveText(SR_Player $player) { return 'You arrive at Seattle. It is a big city with a bunch of slums and a big commercial centrum.'; }
 	public function getSquareKM() { return 8; }
 	public function getMinLevel() { return 8; }
