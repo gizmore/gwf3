bbb856f8d41ca01216703ffb0fa0f5382b6982fd
Andre Michael Pietroschek <58571653+AMP1972@users.noreply.github.com>
2019-12-08 03:11:51 +0100

    Update OrkHQ.php
    Shamane is in, so fat ork ain't alone any longer..?

--- core/module/Dog/dog_modules/dog_module/Shadowlamb/city/OrkHQ/OrkHQ.php
+++ core/module/Dog/dog_modules/dog_module/Shadowlamb/city/OrkHQ/OrkHQ.php
@@ -2,11 +2,10 @@
 final class OrkHQ extends SR_Dungeon
 {
 	public function getCityLocation() { return 'Redmond_OrkHQ'; }
-	public function getImportNPCS() { return array('Redmond_ToughGuy','Redmond_Ork','Redmond_OrkLeader'); }
+	public function getImportNPCS() { return array('Redmond_ToughGuy','Redmond_Ork', 'Redmond_Orc_Shamane', 'Redmond_OrkLeader'); }
 	public function getArriveText(SR_Player $player) { return 'You enter the ork headquarters. It smells like rotten beef here. You can taste fear now...'; }
 	public function getSquareKM() { return 0.4; }
 	public function getExploreTime() { return 60; }
 	public function getGotoTime() { return 50; }
 	public function getAreaSize() { return 100; }
 }
-?>
