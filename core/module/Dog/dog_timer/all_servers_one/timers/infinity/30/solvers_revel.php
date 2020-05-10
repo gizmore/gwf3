6c6b7b93630d6b131d8d456bd77cd1bd6e76ace0
gizmore <gizmore@wechall.net>
2020-05-10 20:19:09 +0200

    [Dog] Disabled revel solvers. (thx tehron)

--- core/module/Dog/dog_timer/all_servers_one/timers/infinity/30/solvers_revel.php
+++ core/module/Dog/dog_timer/all_servers_one/timers/infinity/30/solvers_revel.php
@@ -4,5 +4,4 @@ require_once DOG_PATH.'dog_lib/Dog_WC_Solvers.php';
 $url = 'https://www.revolutionelite.co.uk/w3ch4ll/solvers_revel.php?datestamp=%DATE%';
 $format = "%s \x02%s\x02 has just solved \x02%s\x02. This challenge has been solved %d times. (http://revolutionelite.co.uk)";
 
-dog_wc_solvers('RevEl', '[RevEl]', $url, array('#revolutionelite'), 99999, $format);
-?>
+# dog_wc_solvers('RevEl', '[RevEl]', $url, array('#revolutionelite'), 99999, $format);
