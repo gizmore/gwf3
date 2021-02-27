b734f8495334d616f1eb7ae6a3fb495641fa43b2
gizmore <gizmore@wechall.net>
2021-02-27 20:44:10 +0100

    Dog: init gwf for trans

--- www/challenge/contribute/lang/chall_en.php
+++ www/challenge/contribute/lang/chall_en.php
@@ -5,11 +5,17 @@ $lang = array(
     "Hello %s<br/>\n".
     "<br/>\n".
     "I am working on a new PHP Framework; <a href=\"%s\">GDO6</a><br/>\n".
-    "In this challenge your job is to contribute something.<br/>\n".
+    "In this challenge your job is to contribute something to one of the gdo6-projects.<br/>\n".
+    "<br/>\n".
     "May it be a bug report or a pull request on github, or maybe a translation of a lang file.<br/>\n".
-    "There are also @TODO tags scattered across the project, in case you want to contribute code.<br/>\n".
-    "Coordination via github is preferred.<br/>\n".
+    "Or maybe you want to work on some CSS for one of the gdo6 sites like tbs.wechall.net.<br/>\n".
+    "There are also @TODO tags scattered across all the projects, in case you want to contribute code.<br/>\n".
+    "<br/>\n".
+    "What i excpect is that you setup a gdo6 environment on your box and play with it a bit.<br/>\n".
+    "<br/>\n".
+    "Coordination via github is preferred, but not required.<br/>\n".
     "There are around 100 gdo6-* repositories, and i am sure you can find something to contribute.<br/>\n".
     "<br/>\n".
+    "Thanks!<br/>\n".
     "gizmore<br/>\n",
 );
