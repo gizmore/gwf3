9fbfcf55d804b8f62b9d144b32b85a04c03111d3
tehron <tehron@quantentunnel.de>
2021-03-01 22:28:23 +0100

    WeChall: allow temp. down sites to be updated

--- core/module/WeChall/WC_Site.php
+++ core/module/WeChall/WC_Site.php
@@ -589,7 +589,7 @@ class WC_Site extends WC_SiteBase
 	
 	public function displayStatus()
 	{
-		$color = $this->isUp() ? 'green' : 'red';
+		$color = $this->isScored() ? 'green' : 'red';
 		$text = WC_HTML::lang('site_dot_'.$color);
 		return sprintf('<img src="%stpl/wc4/img/dot_%s.png" alt="%s" title="%s" />', GWF_WEB_ROOT, $color, $text, $text);
 	}
@@ -992,7 +992,7 @@ class WC_Site extends WC_SiteBase
 	{
 		require_once 'WC_RegAt.php';
 		
-		if (!$this->isUp()) {
+		if (!$this->isScored()) {
 			return new GWF_Result(WC_HTML::lang('err_site_down', array($this->displayName())), true);
 		}
 		
