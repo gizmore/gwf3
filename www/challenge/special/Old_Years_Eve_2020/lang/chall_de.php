9ab84f68fe00a2817a0d4ae803d0ba86f909af6b
gizmore <gizmore@wechall.net>
2021-03-03 01:25:16 +0100

    WC: German translation for old years eve challenge.

---
+++ www/challenge/special/Old_Years_Eve_2020/lang/chall_de.php
@@ -0,0 +1,19 @@
+<?php
+$lang = array(
+	'title' => 'Altjahrestag 2020',
+	'info' =>
+    "Hallo %s,<br/>\n" .
+    "<br/>\n" .
+    "Es geht das Gerücht um, die &quot;Revolution Elite Agentur&quot; ist in eine Zeitreise verwickelt. Dies stellte sich als wahr heraus.<br/>\n" .
+    "Anscheinend ist ein Zeitreisender gestrandet, und Sie konnten dieser Person habhaft werden.<br/>\n" .
+    "Unsere eigenen Agenten konnten das RevEl Netzwerk infiltrieren und eine Datei von einer Diskette kopieren, die der Besucher mit sich brachte.<br/>\n" .
+    "Bitte <a href=\"file\">untersuche diese</a> auf jede Art von Daten die Du finden kannst.<br/>\n" .
+    "<br/>\n" .
+    "Viel Glück!<br/>\n" .
+    "<br/>\n" .
+    "<hr/>\n" .
+    "<br/>\n" .
+    "Vielen Dank an Dr_Blutig für das Implemntieren dieser Challenge.<br/>\n" .
+    "<br/>\n" .
+    "Wir wünschen Euch ein Frohes neues Jahr 2020=>2021!\n",
+);
