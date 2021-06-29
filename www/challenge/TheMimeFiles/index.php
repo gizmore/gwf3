70f80f5da86ead7dfb16e4e23dbfa4ddbcfc10ec
gizmore <gizmore@wechall.net>
2021-06-29 04:42:41 +0200

    [WC] New challenge - The Mime Files

--- www/challenge/TheMimeFiles/index.php
+++ www/challenge/TheMimeFiles/index.php
@@ -2,6 +2,7 @@
 chdir('../../');
 define('GWF_PAGE_TITLE', 'The Mime Files');
 require_once('challenge/html_head.php');
+require(GWF_CORE_PATH.'module/WeChall/solutionbox.php');
 if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE)))
 {
     $chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 3, 'challenge/TheMimeFiles/index.php', false);
@@ -11,6 +12,8 @@ $chall->showHeader();
 $href = 'http://themimefiles.warchall.net';
 $href3 = 'https://github.com/gizmore/gwf3/tree/master/www/challenge/TheMimeFiles/www';
 echo GWF_Box::box($chall->lang('info', [$href, $href3]), $chall->lang('title'));
+
+formSolutionbox($chall);
 
 echo $chall->copyrightFooter();
 
