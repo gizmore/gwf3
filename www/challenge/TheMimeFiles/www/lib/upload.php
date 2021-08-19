368895395d61e39b1275c2e118220e5962aa2e0a
Florian Best <best@univention.de>
2021-08-19 23:32:24 +0200

    WC[TheMimeFiles]: fix HTML escaping

--- www/challenge/TheMimeFiles/www/lib/upload.php
+++ www/challenge/TheMimeFiles/www/lib/upload.php
@@ -4,6 +4,8 @@
  * Copyright, 2021 by the mime org.
  */
 
+require_once './shared.php';
+
 if (isset($_POST['upload']))
 {
     uploadFile($_FILES['mimefile']);
@@ -13,37 +15,37 @@ function uploadFile(array $file)
 {
     if ($file['error'])
     {
-        echo "Error " . $file['error']. " - Your upload failed!";
+        echo "Error " . html($file['error']). " - Your upload failed!";
         return false;
     }
-    
+
     $mime = mime_content_type($file['tmp_name']);
-    
+
     if (strpos($mime, 'image') !== 0)
     {
         echo "Error 9 - Please upload only mime images!";
         return false;
     }
-    
+
     $data = file_get_contents($file['tmp_name']);
-    
+
     if (stripos($data, '<?php') !== false)
     {
         echo "Error 10 - You are an evil hacker!"; # This does not seem to help :/
         return false;
     }
-    
+
     $dir = "upload/" . session_id();
     @mkdir($dir, 0700, true); # create upload dir
-    
-    $path = 'upload/'.session_id().'/'.$file['name'];
-    
+
+    $path = 'upload/' . session_id() . '/' . $file['name'];
+
     if (!rename($file['tmp_name'], $path))
     {
         echo "Error 11 - Cannot move uploaded file.";
         return false;
     }
-    
+
     echo "Your file has been uploaded!";
     return true;
 }
