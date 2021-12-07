c9c5e47e3f6d12d1ea388bc32282885780696d9a
gizmore <gizmore@wechall.net>
2021-12-07 09:30:09 +0100

    [WC] New challenge - gifts

--- www/challenge/christmas2021/gifts/lang/chall_de.php
+++ www/challenge/christmas2021/gifts/lang/chall_de.php
@@ -5,7 +5,7 @@ $lang = array(
 		'Um es kurz zu machen... ich hab\' Mist gebaut und bekomme dieses Jahr wohl keine Weihnachtsgeschenke.<br/>'.PHP_EOL.
 		'Kannst Du mir was schenken? Ein <a href="%s">kvirc</a> <a href="%s">Script</a> das eine Nachricht an alle Kanäle sendet?<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
-		'Ich sag schonmal Danke!<br/>'.PHP_EOL.
+		'Schmeiss den Code einfach in die Solutionbox. Ich sag schonmal Danke!<br/>'.PHP_EOL.
 		' - gizmore'.PHP_EOL,
 	'err_santa' => 'Mach\' ma\' hinne. Noch %s Tage bis Weihnachten. Kann doch nicht so schwer sein!',
 );
--- www/challenge/christmas2021/gifts/lang/chall_en.php
+++ www/challenge/christmas2021/gifts/lang/chall_en.php
@@ -5,7 +5,7 @@ $lang = array(
 		'Long Story Short, i think i messed up life and will get no christmas gifts this year.<br/>'.PHP_EOL.
 		'Maybe you could make me a gift? A <a href="%s">kvirc</a> <a href="%s">script</a> that sends a message to all channels?<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
-		'Thanks mate!<br/>'.PHP_EOL.
+		'Just put all the code in the solutionbox. Thanks mate!<br/>'.PHP_EOL.
 		' - gizmore'.PHP_EOL,
 	'err_santa' => 'Please hurry, it\'s %s days until christmas. Can\'t be that hard!',
 );
