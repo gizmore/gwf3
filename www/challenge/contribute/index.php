798c5388bf38749695d246c840287ae572ffd5d4
gizmore <gizmore@wechall.net>
2022-02-19 00:22:00 +0100

    [WC] Contribute Challenge adjusted.

--- www/challenge/contribute/index.php
+++ www/challenge/contribute/index.php
@@ -11,15 +11,29 @@ $chall->showHeader();
 
 $user = GWF_User::getStaticOrGuest();
 $username = $user->displayUsername();
-$url = 'https://github.com/gizmore/gdo6';
-
-echo GWF_Box::box($chall->lang('info', [$username, $url]), $chall->lang('title'));
+$args = [$username,
+	'https://github.com/gizmore/gdo6',
+	'https://github.com/gizmore/gdo6/blob/master/DOCS/GDO_MODULES.md',
+	'https://github.com/gizmore/gdo6/tree/master/GDO/Core/GDT.php',
+	'https://github.com/gizmore/gdo6/tree/master/GDO/Core/GDO.php',
+	'https://github.com/gizmore/gdo6/blob/master/GDO/Core/lang/core_en.php',
+	'https://github.com/gizmore/gdo6-bootstrap5-theme/blob/main/css/gdo6-bootstrap5.css',
+	'https://tbs.wechall.net',
+	'https://github.com/gizmore/gdo6/blob/master/DOCS/GDO_TODO.md',
+	'https://github.com/gizmore/gdo6/blob/master/DOCS/GDO_INSTALL_CLI.md',
+	'http://localhost:80',
+];
+echo GWF_Box::box($chall->lang('info', $args), $chall->lang('title'));
 
 $url = 'https://github.com/gizmore/gdo6/blob/master/DOCS/GDO_CONTRIBUTORS.md#list-of-contributors';
 echo GWF_Box::box($chall->lang('halloffame', [$url]));
 
 $url = 'https://github.com/gizmore/gdo6/blob/master/DOCS/GDO_MODULES.md#list-of-modules';
 echo GWF_Box::box($chall->lang('modules', [$url]));
+
+$url1 = 'https://github.com/gizmore/gwf3';
+$url2 = 'https://github.com/gizmore/gwf3/tree/master/core/module/WeChall';
+echo GWF_Box::box($chall->lang('alternate', [$url1, $url2]));
 
 echo "\n<!-- https://www.youtube.com/watch?v=DfN_ZMmiLQw -->\n";
 
--- www/challenge/contribute/lang/chall_en.php
+++ www/challenge/contribute/lang/chall_en.php
@@ -5,16 +5,15 @@ $lang = array(
     "Hello %s<br/>\n".
     "<br/>\n".
     "I am working on a new PHP Framework; <a href=\"%s\">GDO6</a><br/>\n".
-    "In this challenge your job is to contribute something to one of the gdo6 modules or the core.<br/>\n".
+    "In this challenge your job is to contribute something to one of the <a href=\"%s\">gdo6 modules</a> or <a href=\"%s\">the</a> <a href=\"%s\">core</a>.<br/>\n".
     "<br/>\n".
-    "May it be a bug report or a pull request on github, or maybe a translation of a lang file.<br/>\n".
-    "Or maybe you want to work on some CSS for one of the gdo6 sites like tbs.wechall.net or ranzgruppe.com.<br/>\n".
-    "There are also @TODO tags scattered across all the projects, in case you want to contribute code.<br/>\n".
+    "May it be a mail with a bug report or a pull request on github. Maybe a translation of a <a href=\"%s\">lang file</a>.<br/>\n".
+    "Or maybe you want to work on some <a href=\"%s\">CSS</a> for one of the gdo6 sites like <a href=\"%s\">tbs.wechall.net</a>.<br/>\n".
+    "There are also <a href=\"%s\">@TODO tags</a> scattered across all the projects, in case you want to contribute code.<br/>\n".
     "<br/>\n".
-    "What i excpect is that you setup a gdo6 environment on your box and play with it a bit.<br/>\n".
+    "What i would like to see is that you <a href=\"%s\">setup a gdo6 environment</a> on a <a href=\"%s\">computer</a> and play with it a bit.<br/>\n".
     "<br/>\n".
-    "Coordination via github is preferred, but not required.<br/>\n".
-    "There are around 100 gdo6-* repositories, and i am sure you can find something to contribute.<br/>\n".
+    "Many of the 100+ module repositories lack implementation, tests and design. :)<br/>\n".
     "<br/>\n".
     "Thanks in advance for your contribution!<br/>\n".
     "gizmore<br/>\n",
@@ -22,5 +21,7 @@ $lang = array(
     'halloffame' => 'There is <a href="%s">a list of contributors</a> you can visit.',
     
     'modules' => 'There is <a href="%s">a list of modules</a> you can work on.',
+	
+	'alternate' => '<b>Alternatively</b> you can contribute to any of the <a href="%s">gwf3</a> projects, including <a href="%s">WeChall</a>.',
     
 );
