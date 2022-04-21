670019186cf40e15450da2249f302e9fb760ddb2
gizmore <gizmore@wechall.net>
2022-04-21 18:03:00 +0200

    [WC] New training challenge: 'No DNS'

---
+++ www/challenge/training/net/nodns/lang/chall_de.php
@@ -0,0 +1,14 @@
+<?php
+$lang = array(
+    'title' => 'No DNS',
+    'info' => '
+Hallo Hacker,<br/>
+<br/>
+In dieser kleinen Training-Challenge must Du eine Anfrage an <a href="%s">%1$s</a> senden.<br/>
+<br/>
+Ich hoffe diese Informationen sind ausreichend.<br/>
+<br/>
+ - gizmore<br/><br/>Vielen Dank an die WeChall Admins; dloser, tehron and livinskull :)',
+	
+	'err_host' => 'Du musst eine Anfrage an &quot;%s&quot; senden.',
+);
