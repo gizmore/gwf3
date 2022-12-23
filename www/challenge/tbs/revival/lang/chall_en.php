38dac92fccfe72d23da300aac416e32c226d458b
gizmore <gizmore@wechall.net>
2022-12-23 10:38:55 +0100

    [WC] tbs tribute challenge info

--- www/challenge/tbs/revival/lang/chall_en.php
+++ www/challenge/tbs/revival/lang/chall_en.php
@@ -7,11 +7,12 @@ Hello %s,<br/>
 Challenge sites come and challenge sites go.
 And wechall is in business since 2008.<br/>
 <br/>
-Before that, the first site i played was TBS,
+Before that, the first site i played was
+<a href="http://bright-shadows.net">TBS</a>,
 and it was an awesome time.<br/>
 <br/>
-I am trying to revive the site
-and made a tribute challenge for it.<br/>
+We are trying to revive the site
+and I made a tribute challenge for it.<br/>
 <br/>
 Can you combine the two streams to reveal the secret message?<br/>
 <br/>
