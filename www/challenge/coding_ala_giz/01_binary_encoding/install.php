4b26d7aca1021a3b2435586f99af4f41fb267519
gizmore <gizmore@wechall.net>
2023-01-15 13:58:07 +0100

    [WC] New allamox challenge

---
+++ www/challenge/coding_ala_giz/01_binary_encoding/install.php
@@ -0,0 +1,20 @@
+<?php
+chdir("../../../../");
+require_once("challenge/html_head.php");
+html_head("CAG: Binary Encoding");
+if (!GWF_User::isAdminS())
+{
+	echo GWF_HTML::err('ERR_NO_PERMISSION');
+	$_GET['no_session'] = 1;
+	require_once("challenge/html_foot.php");
+	return;
+}
+$title = 'CAG: Binary Encoding';
+$solution = false;
+$score = 1;
+$url = "challenge/coding_ala_giz/01_binary_encoding/index.php";
+$creators = "gizmore,aLLamoox";
+$tags = 'Encoding,Training';
+WC_Challenge::installChallenge($title, $solution, $score, $url, $creators, $tags, true);
+require_once("challenge/html_foot.php");
+?>
---
+++ www/challenge/coding_ala_giz/01_binary_encoding/lang/chall_en.php
@@ -0,0 +1,14 @@
+<?php
+$lang = array(
+	'title' => 'CAG: Binary Encoding',
+	'info' =>
+		'Hello future Hackers,<br/>'.PHP_EOL.
+		'<br/>'.
+		'aLLamoox and gizmore proudly present: Coding Ala Giz<br/>'.
+		'<br/>'.
+		'Your first task is easy. Just convert this number to the decimal system: %s<br/>'.
+		'<br/>'.
+		'Greetings<br/>'.
+		' - gizmore and aLLamoox',
+	
+);
