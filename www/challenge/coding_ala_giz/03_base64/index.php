4ed9b9befae82e5e1e3fd37153e179d381909b1a
gizmore <gizmore@wechall.net>
2023-01-16 21:03:34 +0100

    [WC] New Base64 challenge. (thx aLLamox)

---
+++ www/challenge/coding_ala_giz/03_base64/index.php
@@ -0,0 +1,18 @@
+<?php
+$sol = require 'solution2.php';
+$solution = require 'solution.php';
+chdir('../../../');
+define('GWF_PAGE_TITLE', 'CAG: Base64');
+require_once('challenge/html_head.php');
+require(GWF_CORE_PATH.'module/WeChall/solutionbox.php');
+if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE))) {
+	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 1, 'challenge/coding_ala_giz/03_base64/index.php', $sol);
+}
+$chall->showHeader();
+$chall->onCheckSolution();
+$url = 'https://en.wikipedia.org/wiki/Base64';
+$problem = base64_encode($solution);
+echo GWF_Box::box($chall->lang('info', [$url, $problem]), $chall->lang('title'));
+formSolutionbox($chall);
+echo $chall->copyrightFooter();
+require_once('challenge/html_foot.php');
---
+++ www/challenge/coding_ala_giz/03_base64/lang/chall_en.php
@@ -0,0 +1,17 @@
+<?php
+$lang = [
+	'title' => 'CAG: Base64',
+	'info' =>
+		'Hello future Hackers,<br/>'.PHP_EOL.
+		'<br/>'.
+		'Another encoding is the <b>Base64</b>.<br/>'.
+		'You can read about it on <a href="%s">Wikipedia</a>.<br/>'.
+		'<br/>'.
+		'Alternatively, here is a brief explaination from chatGPT:<br/><br/>'.
+		'Base64 is a way of encoding information into a text string that can be quickly decoded. It can be used to represent binary data such as images and audio files in a way that is more compact and widely used than other forms of encoding. Any text string encoded with Base64 can be easily decoded with the appropriate software.<br/>'.
+		'<br/>'.
+		'%s<br/>'.
+		'<br/>'.
+		'Greetings<br/>'.
+		' - gizmore and aLLamoox',
+];
