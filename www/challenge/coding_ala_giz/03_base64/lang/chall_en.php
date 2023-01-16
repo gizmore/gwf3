7d407c905f899e1b8ede366ea0aaab9ec5210755
gizmore <gizmore@wechall.net>
2023-01-16 21:09:24 +0100

    [WC] New Base64 challenge. (thx aLLamox)

--- www/challenge/coding_ala_giz/03_base64/lang/chall_en.php
+++ www/challenge/coding_ala_giz/03_base64/lang/chall_en.php
@@ -7,8 +7,8 @@ $lang = [
 		'Another encoding is the <b>Base64</b>.<br/>'.
 		'You can read about it on <a href="%s">Wikipedia</a>.<br/>'.
 		'<br/>'.
-		'Alternatively, here is a brief explaination from chatGPT:<br/><br/>'.
-		'Base64 is a way of encoding information into a text string that can be quickly decoded. It can be used to represent binary data such as images and audio files in a way that is more compact and widely used than other forms of encoding. Any text string encoded with Base64 can be easily decoded with the appropriate software.<br/>'.
+		'Alternatively, here is a brief proof-checked explaination from chatGPT:<br/><br/>'.
+		'Base64 is a way of encoding information into a text string that can be quickly decoded. It can be used to represent binary data such as images and audio files in a way that is more compatible with any transmissions. It is widely used. Any text string encoded with Base64 can be easily decoded with the appropriate software.<br/>'.
 		'<br/>'.
 		'%s<br/>'.
 		'<br/>'.
