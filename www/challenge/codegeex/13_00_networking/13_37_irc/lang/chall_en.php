6b9be46e03f73fee2fe86733fd801b10a255931e
gizmore <gizmore@wechall.net>
2023-02-15 01:26:15 +0100

    [WC] CGX#5 - IRC.

---
+++ www/challenge/codegeex/13_00_networking/13_37_irc/lang/chall_en.php
@@ -0,0 +1,17 @@
+<?php
+$lang = [
+	'title' => 'CGX#5: IRC with KVIrc',
+	'info' =>
+		'Hello future Hackers,<br/>'.PHP_EOL.
+		'<br/>'.
+		'Codinggeex has released their fifth video.<br/>'.
+		'<br/>'.
+		'Again, this is just a video tutorial, to help you,'. 
+		'to install the KVIrc IRC Client.<br/>'.
+		'<br/>'.
+		'If you don\'t like the videos, do not watch them,'.
+		'and just mark the challenge as solved for you.<br/>'.
+		'<br/>'.
+		'Happy Challenging!<br/>'.
+		' - giz and x',
+];
