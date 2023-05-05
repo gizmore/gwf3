eb01c88c4b5402fc30771dc74304e8da0e05e24c
gizmore <gizmore@wechall.net>
2023-05-05 18:53:01 +0200

    CGX#11: XSS. (thx x)

---
+++ www/challenge/codegeex/09_00_cracking/09_20_cross_site_scripting/README.md
@@ -0,0 +1,16 @@
+# CGX#11: Cross Site Scripting
+
+This video introduces you to the concepts of
+[Cross Site Scripting / XSS](https://owasp.org/www-community/attacks/xss/)
+
+We will exploit a very simple XSS vuln single page application.
+
+
+ToC:
+
+ - What is XSS?
+ - Quotes again (hopefully)
+ - How to output user input?
+ - Charset awareness
+ - Other techniques (HTTP Header injection? URL injection?)
+ - Is Cross-Site-Scripting mostly Dead?
---
+++ www/challenge/codegeex/09_00_cracking/09_20_cross_site_scripting/lang/chall_en.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+	'title' => 'CGX: SQL Injection',
+	'info' =>
+		'Hello future Hackers,<br/>'.PHP_EOL.
+		'<br/>'.
+		'In this tutorial you will learn a tiny bit about <a href="https://en.wikipedia.org/wiki/XSS">XSS</a>.<br/>'.
+		'There is a little training challenge waiting for you, which we will dicuss in the video.<br/>'.
+
+	'<br/>'.
+	'You can see the source of the vulnerable application under <a href="mask1.code">mask1.code</a><br/>'.
+	'<br/>'.
+	'Greetings<br/>'.
+		' - gizmore and x',
+];
