5c708d4f8324870dbc4254b1b1935989d1552b4a
gizmore <gizmore@wechall.net>
2024-01-16 14:55:55 +0100

    [WC] New Challenge: TBS Rehearsal (thx x)

--- www/challenge/tbs/rehearsal/lang/chall_en.php
+++ www/challenge/tbs/rehearsal/lang/chall_en.php
@@ -11,7 +11,7 @@ in memory of TBS - which is still available on bright-shadows.net/index2.php<br/
 We once recorded a song, Hidden in the Hint, but can you find out who is performing?<br/>
 Sadly i could not put the name of this person into the copyright. Please enter the whole hidden message you find as the solution.<br/>
 <br/>
-The song is available in the %s, but you should try it on %s.<br/>
+The song and lyrics are available in the %s, but you should also try it on %s.<br/>
 <br/>
 Enjoy the song, and...<br/>
 Happy Challenging!<br/>
