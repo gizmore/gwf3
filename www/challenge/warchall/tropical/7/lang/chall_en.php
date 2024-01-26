550ff6730ab1e5f8c8d69a73f83dcef12224fdaf
gizmore <gizmore@wechall.net>
2024-01-26 17:48:44 +0100

    [WC] fixed warchall binary path for 07_tropical_fruits (thx tehron)

--- www/challenge/warchall/tropical/7/lang/chall_en.php
+++ www/challenge/warchall/tropical/7/lang/chall_en.php
@@ -3,7 +3,7 @@ $lang = array(
 	'title' => 'Warchall - Seven Tropical Fruits',
 	'info' =>
 		'This is level 7 on the <a href="%s">warchall box</a>.<br/>'.PHP_EOL.
-		'You can find the vulnerable binary + sourcecode in /home/level/tropic/7.<br/>'.PHP_EOL.
+		'You can find the vulnerable binary + sourcecode in &quot;/home/level/07_tropical_fruits/&quot;.<br/>'.PHP_EOL.
 		'You can also <a href="%s">view the source here</a>.<br/>'.PHP_EOL.
 		'Your mission is to exploit it, circumvent the ASLR protection and read a file in the name of the executable.<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
--- www/challenge/warchall/tropical/7/lang/chall_it.php
+++ www/challenge/warchall/tropical/7/lang/chall_it.php
@@ -3,7 +3,7 @@ $lang = array(
 	'title' => 'Warchall - Seven Tropical Fruits ',
 	'info' =>
 		'Questo è il livello 7 della <a href="%s">warchall box</a>.<br/>'.PHP_EOL.
-		'Puoi trovare il file binario ed il sorgente in /home/level/tropic/7.<br/>'.PHP_EOL.
+		'Puoi trovare il file binario ed il sorgente in &quot;/home/level/07_tropical_fruits/&quot;.<br/>'.PHP_EOL.
 		'You can also <a href="%s">view the source here</a>.<br/>'.PHP_EOL.
 		'La tua missione è di sfruttarlo, aggirando la protezione ASLR, e leggere un file nel nome dell\'eseguibile.<br/>'.PHP_EOL.
 		'<br/>'.PHP_EOL.
