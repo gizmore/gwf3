952801b34540e7a126f026681106baa2393bc7e9
gizmore <gizmore@wechall.net>
2024-01-27 06:35:49 +0100

    [WC] patreon link in status sidebarQ

--- core/module/WeChall/method/Sidebar2.php
+++ core/module/WeChall/method/Sidebar2.php
@@ -96,6 +96,7 @@ final class WeChall_Sidebar2 extends GWF_Method
 			'<div><a href="'.GWF_WEB_ROOT.'forum">'.$this->module->lang('rp_postcount', array($postcount)).'</a></div>'.PHP_EOL.
 			'<div><a href="'.GWF_WEB_ROOT.'users">'.$this->module->lang('rp_usercount', array(GDO::table('GWF_User')->countRows())).'</a></div>'.PHP_EOL.
             '<div><a href="'.GWF_WEB_ROOT.'donations">41 donations</a></div>'.PHP_EOL.
+            '<div><a href="https://www.patreon.com/WeChall" target="_blank">0 Patreons</a></div>'.PHP_EOL.
             '<div><a href="'.GWF_WEB_ROOT.'shop">1 Shop</a></div>'.PHP_EOL;
 			return
 			$this->sidebox($this->module->lang('rp_stats').$btn, $box);
