8666ce7102dccd68471cab46bd718d88d8244e10
livinskull <livinskull@gmail.com>
2024-06-16 13:25:03 +0200

    Fix optional before required params in Forum_Move
    All calls to this give both params, removing the default.

--- core/module/Forum/method/Move.php
+++ core/module/Forum/method/Move.php
@@ -18,7 +18,7 @@ final class Forum_Move extends GWF_Method
 		return GWF_HTML::err('ERR_PARAMETER', array(_FILE__, __LINE__, 'move'));
 	}
 	
-	private function move($dir=-1, $bid)
+	private function move($dir, $bid)
 	{
 		if (false === ($board = GWF_ForumBoard::getBoard($bid))) {
 			return $this->module->error('err_board');
