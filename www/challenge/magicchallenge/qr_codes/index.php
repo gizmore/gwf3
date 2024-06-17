46b19d05a733c7b88ddd5fa0b1c50d76f8ccdfaa
Tekknoman <elioteuber@outlook.com>
2024-06-17 20:52:17 +0000

    feat: adding logic to generate details for challenge based on challenge

--- www/challenge/magicchallenge/qr_codes/index.php
+++ www/challenge/magicchallenge/qr_codes/index.php
@@ -1,38 +1,67 @@
 <?php
 chdir("../../../");
 define('GWF_PAGE_TITLE', 'QR Codes');
-require_once("challenge/html_head.php");
-require_once GWF_CORE_PATH.'module/WeChall/solutionbox.php';
-require_once GWF_CORE_PATH.'module/WeChall/WC_CryptoChall.php';
+require_once ("challenge/html_head.php");
+require_once GWF_CORE_PATH . 'module/WeChall/solutionbox.php';
+require_once GWF_CORE_PATH . 'module/WeChall/WC_CryptoChall.php';
 if (false === ($chall = WC_Challenge::getByTitle(GWF_PAGE_TITLE))) {
 	$chall = WC_Challenge::dummyChallenge(GWF_PAGE_TITLE, 1, 'challenge/magicchallenge/qr_codes/index.php');
 }
 $chall->showHeader();
 
-$filename = 'challenge/magicchallenge/qr_codes.include';
-$code = '[php]'.file_get_contents($filename).'[/php]';
-$code = GWF_Message::display($code, true, false, false);
 
 $SOLUTION = require_once 'challenge/magicchallenge/qr_codes/secret.php';
 
+$do_highlight = Common::getGetString('highlight') === 'christmas';
+
+$code = '';
+
+if ($do_highlight) {
+	$code = GWF_Message::display(' [PHP title=collectedSamples.json]' . file_get_contents('challenge/magicchallenge/qr_codes/qr_codes.include') . '[/code]', true, false, false, array());
+	$toggleButtonText = $chall->lang('collapseButton');
+	$toggleButtonLink = '?';
+} else {
+	$toggleButtonText = $chall->lang('expandButton');
+	$toggleButtonLink = '?highlight=christmas';
+}
+
 WC_CryptoChall::checkSolution($chall, $SOLUTION, true, true);
 
-echo GWF_Box::box($chall->lang('info'), $chall->lang('title'));
 
-echo GWF_Box::box($code, "Collected Samples");
-echo GWF_Box::box("TBD");
+$qr_details = get_qr_details($chall);
+
+$dateTimeString = GWF_Time::displayTimestamp($qr_details[2], false, false);
+
+echo GWF_Box::box($chall->lang('info', array($code, $toggleButtonText, $toggleButtonLink, $dateTimeString, $qr_details[0], $qr_details[1])), $chall->lang('title'));
+
 
 formSolutionbox($chall);
 
 echo $chall->copyrightFooter();
-require_once("challenge/html_foot.php");
+require_once ("challenge/html_foot.php");
 ?>
-<?php 
+
+
+
+
+
+<?php
+
+function get_qr_details(WC_Challenge $chall)
+{
+	global $SOLUTION;
+	$qr_seed = WC_CryptoChall::generateSolution($SOLUTION, true, true, 4);
+
+	$locker = (ord($qr_seed[0]) % 30) + 1;
+	$row = (ord($qr_seed[1]) % 20) + 1;
+	$raw_time = GWF_Time::getTimestamp($chall->getDate()) - 3600 * 24 * 30 * (ord($qr_seed[2]) % 12) - ord($qr_seed[0]) * 60 + ord($qr_seed[1]);
+	return [$locker, $row, $raw_time];
+}
 function crypto_caesar_1_ciphertext(WC_Challenge $chall)
 {
 	global $SOLUTION;
 	WC_CryptoChall::checkPlaintext(strtoupper($chall->lang('plaintext')));
-	
+
 	$solution = WC_CryptoChall::generateSolution($SOLUTION, true, true);
 	$pt = $chall->lang('plaintext', array($solution));
 	$pt = strtoupper($pt);
@@ -47,14 +76,13 @@ function crypto_caesar_1_encrypt($pt)
 	$ct = '';
 	$key = rand(1, 25);
 	$len = strlen($pt);
-	for ($i = 0; $i < $len; $i++)
-	{
+	for ($i = 0; $i < $len; $i++) {
 		$c = $pt[$i];
 		if ($c === ' ') {
 			$ct .= ' ';
 		} else {
 			$ord = ord($c) - $a;
-			$ct .= chr((($ord+$key)%26)+$a);
+			$ct .= chr((($ord + $key) % 26) + $a);
 		}
 	}
 	return $ct;
--- www/challenge/magicchallenge/qr_codes/lang/chall_en.php
+++ www/challenge/magicchallenge/qr_codes/lang/chall_en.php
@@ -5,11 +5,11 @@ $lang = array(
 	'info' =>
 		'When I travel , I like to use the lockers at ariports. It is a verry conveniant way to securely leave deposit my luggage, so I can roam around. I especially like that these lockers give me a qr code, with which I can easly grab my luggage, when I come back later. Unfortunately I have lost my qr code. Can you help me crack my locker?<br/>'.
 		'I have scavanged a few qr codes while digging for food in the bins nearby.<br/>'.
+		'<a href="%3$s">%2$s</a><br/>'.
 		'%1$s<br/>'.
-		'<br/>'.
-		'From what I remember, I booked my locker at (TODO: Dynamic Timestamp) in locker number (TODO: Dynamic Locker Number).<br/>',
-	# ASCII only (obvious)
-	'plaintext' => 'TODO: DYNAMIC SOLUTION %1$s.',
+		'From what I remember, I booked my locker at %4$s in locker %5$s row %6$s.<br/>',
+	'expandButton' => '+ Show codes',
+	'collapseButton' => '- Hide codes',
 );
 	
 ?>
