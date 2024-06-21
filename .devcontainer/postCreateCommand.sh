4d18c346c40be6f4529d9d112e7d11e18688da19
TAATEEL1 <elio.teuber@swisscom.com>
2024-06-21 17:52:22 +0000

    fix: adding bcmath to Dockerfiel

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,5 @@ sudo ln -s "$(pwd)/www" /var/www/html
 sudo a2enmod rewrite
 echo 'error_reporting=0' | sudo tee /usr/local/etc/php/conf.d/no-warn.ini
 sudo cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
-# add exec,system,passthru,pcntl_exec,proc_open,shell_exec,popen,link to disable_functions
 sudo sed -i 's/disable_functions =/disable_functions = exec,system,passthru,pcntl_exec,proc_open,shell_exec,popen,link/g' /usr/local/etc/php/php.ini
 sudo apache2ctl start
