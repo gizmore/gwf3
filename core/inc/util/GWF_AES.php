5acba6bcb883f6ba45cceb6c875441361783fb8f
gizmore <gizmore@wechall.net>
2024-06-26 17:40:02 +0200

    AES openssl

--- core/inc/util/GWF_AES.php
+++ core/inc/util/GWF_AES.php
@@ -1,83 +1,104 @@
 <?php
+use function openssl_cipher_iv_length;
+
 /**
- * GWF Helper/Wrapper for AES-256 encryption.
+ * AES crypto utility.
+ *
+ * @version 7.0.0
+ * @since 3.0.1
  * @author gizmore
  */
 final class GWF_AES
 {
-	const IV = 'MyHomeIsMyCastleIamhungrywhereisi'; # <-- 32 chars
-	const MODE = MCRYPT_MODE_CBC;
-	const CIPHER = MCRYPT_RIJNDAEL_256;
 
-	/**
-	 * Encrypt with AES256 using the default IV.
-	 * @param string $data
-	 * @param string $key
-	 */
-	public static function encrypt($data, $key)
-	{
-		return self::encrypt4($data, $key, self::IV);
-	}
+    public const IV = 'MyHomeIsMyCastleIamhungrywhereisi'; # <-- 32 chars
+    public const CIPHER = 'aes-256-cbc';
 
-	/**
-	 * Encrypt with AES256. Use sha256($iv) as IV. It is recommended to call this with a funny IV over the above.
-	 * @param string $data
-	 * @param string $key
-	 * @param string $iv
-	 * @return string data
-	 */
-	public static function encrypt4($data, $key, $iv)
-	{
-		return @mcrypt_encrypt(self::CIPHER, $key, $data, self::MODE, hash('SHA256', $iv, true));
-	}
+    /**
+     * Encrypt with AES256 using the default IV.
+     *
+     * @deprecated unsafe
+     */
+    public static function encrypt(string $data, string $key): string
+    {
+        return self::encrypt4($data, $key, self::IV);
+    }
 
-	/**
-	 * Encrypt with AES256. Use sha256($password) as key. Use a random IV and prepend to the output.
-	 * This is probably the function you are looking for.
-	 * @param string $data
-	 * @param string $password
-	 * @return string data
-	 */
-	public static function encryptIV($data, $password)
-	{
-		$iv_size = mcrypt_get_iv_size(self::CIPHER, self::MODE);
-		$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-		$key = hash('SHA256', $password, true);
-		return base64_encode($iv).mcrypt_encrypt(self::CIPHER, $key, $data, self::MODE, $iv);
-	}
-	
-	/**
-	 * Decrypt data encrypted with with the encryptIV function above.
-	 * @param string $data
-	 * @param string $password
-	 * @return string plaintext
-	 */
-	public static function decryptIV($data, $password)
-	{
-		$iv = substr($data, 0, 44);
-		$key = hash('SHA256', $password, true);
-		return mcrypt_decrypt(self::CIPHER, $key, substr($data, 44), self::MODE, base64_decode($iv));
-	}
+    /**
+     * Encrypt with AES256. Use sha256($iv) as IV. It is recommended to call this with a funny IV over the above.
+     *
+     * @deprecated unsafe
+     */
+    public static function encrypt4(string $data, string $key, string $iv): string
+    {
+        return openssl_encrypt($data, self::CIPHER, $key, null, $iv);
+    }
 
-	/**
-	 * Decrypt with AES256 using the default IV.
-	 * @param string $data
-	 * @param string $key
-	 */
-	public static function decrypt($data, $key)
-	{
-		return self::decrypt4($data, $key, self::IV);
-	}
+    /**
+     * Encrypt with AES256. Use sha256($password) as key. Use a random IV and prepend to the output.
+     * This is probably the function you are looking for.
+     */
+    public static function encryptIV(string $data, string $password)
+    {
+        $iv_size = openssl_cipher_iv_length(self::CIPHER);
+        $iv = openssl_random_pseudo_bytes($iv_size);
+        $key = hash('SHA256', $password, true);
+        $encrypted = base64_encode($iv) . openssl_encrypt($data, self::CIPHER, $key, 0, $iv);
+        $hmac = hash_hmac('sha256', $encrypted, $key);
+        return $encrypted . $hmac;
+    }
 
-	/**
-	 * Decrypt with AES256. Use sha256($iv) as IV.
-	 * @param string $data
-	 * @param string $key
-	 * @param string $iv
-	 * @return string data
-	 */
-	public static function decrypt4($data, $key, $iv)
-	{
-		return @mcrypt_decrypt(self::CIPHER, $key, $data, self::MODE, hash('SHA256', $iv, true));
-	}
+    /**
+     * Decrypt data encrypted with with the encryptIV function above.
+     *
+     * @param string $data
+     * @param string $password
+     *
+     * @return string plaintext
+     */
+    public static function decryptIV($data, $password)
+    {
+        $iv_size = openssl_cipher_iv_length(self::CIPHER);
+        $iv64 = ((int)((4 * $iv_size / 3) + 3)) & ~3;
+        $hmac = substr($data, -64);
+        $data = substr($data, 0, -64);
+        $key = hash('SHA256', $password, true);
+        if ($hmac !== hash_hmac('sha256', $data, $key)) # Only decrypt if cookie has not been tampered
+        {
+            return false;
+        }
+        $iv = substr($data, 0, $iv64);
+        $iv = base64_decode($iv);
+        $data = substr($data, $iv64);
+        return openssl_decrypt($data, self::CIPHER, $key, 0, $iv);
+    }
+
+    /**
+     * Decrypt with AES256 using the default IV.
+     *
+     * @param string $data
+     * @param string $key
+     *
+     * @deprecated unsafe
+     */
+    public static function decrypt($data, $key)
+    {
+        return self::decrypt4($data, $key, self::IV);
+    }
+
+    /**
+     * Decrypt with AES256. Use sha256($iv) as IV.
+     *
+     * @param string $data
+     * @param string $key
+     * @param string $iv
+     *
+     * @return string data
+     * @deprecated unsafe
+     */
+    public static function decrypt4($data, $key, $iv)
+    {
+        return openssl_decrypt($data, self::CIPHER, $key, null, $iv);
+    }
+
 }
