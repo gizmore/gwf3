36b16d4c2018ddfdac6bfa61e470911287357830
gizmore <gizmore@wechall.net>
2024-09-28 18:22:43 +0200

    [Dog] New User Ketza

---
+++ core/module/Dog/dog_plugins/dog_plugin/Fun/Users/ketza_Pb.php
@@ -0,0 +1,8 @@
+<?php
+$lang = array(
+	'en' => array(
+		'help' => 'Usage: %CMD%. Find out what Ketza means.',
+		'ketza' => 'Ketza has no special meaning. It is the reverse of an old nicknam aztek.',
+	),
+);
+return Dog::getPlugin()->rply('ketza');
