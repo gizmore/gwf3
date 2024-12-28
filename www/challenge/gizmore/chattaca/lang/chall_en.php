da9c7f6f48a292fac365ede122f9d9a9b19bb466
gizmore <gizmore@wechall.net>
2024-12-28 22:41:15 +0100

    [WC] Chattaca description

--- www/challenge/gizmore/chattaca/lang/chall_en.php
+++ www/challenge/gizmore/chattaca/lang/chall_en.php
@@ -9,18 +9,18 @@ $lang = array(
     <br/>
     It currently speaks
     <a href="https://github.com/gizmore/pygdo-telegram">Telegram</a> (<a href="https://t.me/+rankW0nN_ig4OGEy">CLICK</a>),
-    <a href="https://github.com/gizmore/pygdo-discord">Discord</a> (<a href="https://discord.gg/nZeqJ2td">CLICK</a>),
-    <a href="https://github.com/gizmore/pygdo-irc">IRC</a>, (where should i make it join?)
-    Bash and HTTP.<br/>
+    <a href="https://github.com/gizmore/pygdo-discord">Discord</a> (<a href="https://discord.gg/JkeErrNQ">CLICK</a>),
+    <a href="https://github.com/gizmore/pygdo-irc">IRC</a>, (<a href="ircs://irc.wechall.net:6697/#shadowlamb">CLICK</a>)
+    WhatsApp (SOON) Bash and HTTP.<br/>
     <br/>
     I have connected GPT-4o to it, and the &quot;AI&quot; can control the bot like a regular user.<br/>
-    Like you can ask chappy to play blackjack and it plays blackjack with kinda itself.<br/>
+    Like, you can ask chappy to play blackjack and it plays blackjack with the bot.<br/>
     Actually, ...it has super admin cow powers and can even issue $bash commands.<br/>
     You can trigger the bot by starting your messages with &quot;chap&quot; or ending your messages with &quot;chappy&quot;"<br/>
     <br/>
     Example: <blockquote>How are you chappy?</blockquote><br/>
     <br/>
-    The AI also knows the password to this ChATTACA challenge, but now you figure out the rest on your own ;)<br/>
+    The AI also knows the password to this ChATTACA challenge, but now you figure out the rest on your own.<br/>
     <br/>
     Shouts go out to quangntenemy for his java bot and tehron for anti bot rules.<br/>
     <br/>
