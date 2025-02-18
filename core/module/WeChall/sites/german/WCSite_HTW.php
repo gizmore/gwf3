7629746356bf7aa33ab2b46f7072137f4a7dcd5b
livinskull <livinskull@gmail.com>
2025-02-18 11:01:03 +0100

    wechall: Add new site "Hack The Web"
    New site: https://hack.arrrg.de/
    
    Implementation for scoring is textbook from the join documentation, so
    it "just works" with the default implementation.

---
+++ core/module/WeChall/sites/german/WCSite_HTW.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * "Hack The Web"
+ * https://hack.arrrg.de/
+ *
+ * Has no email for linking but a "fake email" as token on the profile.
+ */
+class WCSite_HTW extends WC_Site
+{
+
+}
