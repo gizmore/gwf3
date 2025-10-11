9dde67bd2f346c73d2380bd107d0d0acf4ac6359
gizmore <gizmore@wechall.net>
2025-10-11 09:00:30 +0200

    [WC] More Russian translations. (thx chappers)

---
+++ core/module/Dog/dog_modules/dog_module/Scum/lang/scum_de.php
@@ -0,0 +1,26 @@
+<?php
+$lang = array(
+	'conf_max_players' => 'Maximale Anzahl an Spielern. Sollte ein Vielfaches von 4 sein.',
+	'conf_reset_time' => 'Erforderliche Inaktivitätszeit, um ein Zurücksetzen zu ermöglichen.',
+
+	'help_scum' => 'Scum ist ein Kartenspiel. Verwende: %CMD% help, um zu erfahren, wie man spielt.',
+	'help_about' => 'Scum ist ein Kartenspiel. Sieh dir die Regeln auf Wikipedia an: http://de.wikipedia.org/wiki/Asshole_%28Spiel%29. Probiere .scum help commands',
+	'help_help' => 'Verwende %T%scum help <command>, um eine Beschreibung für den angegebenen Befehl zu erhalten. Befehle: help, init, join, start, abort | cards, pass, <Karten spielen>',
+	'help_commands' => '%T%scum <commands>: init, join, start, help, cards, turn, deck, top5, stats, abort ODER <Karten spielen>. Gib scum help <command> ein, um weitere Hilfe zu erhalten.',
+	'help_init' => '%T%scum init startet ein neues Spiel. Gib %T%scum join ein, um dem Spiel beizutreten.',
+	'help_join' => 'Gib %T%scum join ein, um einem gestarteten Spiel beizutreten.',
+	'help_start' => '%T%scum start startet das Spiel.',
+	'help_cards' => '%T%scum cards zeigt dir deine aktuellen Karten.',
+	'help_turn' => '%T%scum turn zeigt Informationen über das aktuelle Spiel.',
+	'help_deck' => '%T%scum turn zeigt Informationen über das aktuelle Spiel.',
+	'help_pass' => '%T%scum pass. Du gibst deinen Zug ab und der nächste Spieler ist an der Reihe. Verwende dies, wenn du nicht spielen kannst oder deine guten Karten behalten möchtest.',
+	'help_top5' => '%T%scum top5 [Sektion] zeigt dir die besten 5 Spielerstatistiken, für eine optionale Sektion.',
+	'help_stats' => '%T%scum stats [Benutzername] zeigt die Statistiken eines Benutzers für Scum an.',
+	'help_abort' => 'Du kannst ein Spiel nach Inaktivität mit %T%scum abort abbrechen.',
+
+	'msg_stats' => '%s hat %d Spiele gespielt und %d gewonnen. Punkte: %d',
+	'msg_inited' => 'Neues Spiel gestartet. Du bist dem Scum beigetreten. %d weitere Personen können %sscum join eingeben.',
+	'msg_aborted' => 'Das Spiel wurde abgebrochen.',
+
+	'your_cards' => 'Deine Karten: %s.',
+);
