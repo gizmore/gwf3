fae4de4c1a132b166a508c06d0f50ecda253b7dd
livinskull <livinskull@gmail.com>
2025-12-31 11:48:07 +0100

    Fix OTW wechall information link on warbox page (thx hubertf)

--- core/module/WeChall/lang/wechall/_wc_it.php
+++ core/module/WeChall/lang/wechall/_wc_it.php
@@ -682,7 +682,7 @@ $lang = array(
 	'title_wartoken' => 'WarTokens',
 	'info_wartoken' => 'On many Warboxes, individual user accounts do not exist. Instead you can identify yourself from a Warbox to WeChall using a "WarToken".<br/><br/>Note that you do still have to link the site on your accounts page (with your default info).<br/><br/>%s first made it possible to score warboxes using ident. Later extensions were made to also allow situations where ident is not (easily) possible.',
 	'title_warboxes' => 'Supported Warboxes',
-	'info_warboxes' => 'Simply solve a chall on a Warbox. When logged in using the password/flag, run one of the following commands:<br/><br/>Some sites (like <a href="http://overthewire.org/help/wechall.html">OTW</a>) may provide custom commands. E.g.:<br/><i>%s</i><br/><br/>Ident method (no longer supported for OTW):<br/><i>%s</i><br/><br/>Non-ident method over SSL:<br/><i>%s</i><br/><br/>That´s it!<br/><br/>You can <a href="%s">link a Warbox</a> like a regular site.<br/><br/><a href="%s">I have a new Warbox, how to add it?</a>',
+	'info_warboxes' => 'Simply solve a chall on a Warbox. When logged in using the password/flag, run one of the following commands:<br/><br/>Some sites (like <a href="https://overthewire.org/information/wechall.html">OTW</a>) may provide custom commands. E.g.:<br/><i>%s</i><br/><br/>Ident method (no longer supported for OTW):<br/><i>%s</i><br/><br/>Non-ident method over SSL:<br/><i>%s</i><br/><br/>That´s it!<br/><br/>You can <a href="%s">link a Warbox</a> like a regular site.<br/><br/><a href="%s">I have a new Warbox, how to add it?</a>',
 	'title_your_wartoken' => 'Your current WarToken',
 	'title_warcredits' => 'Warbox idea',
 	'info_warcredits' => 'Big big thanks to %s for this fabulous idea and implementation!',
