gizmore/gwf3

commit 900913f273c79ce05b0deb28fdddb562188d5203

fix(GWF_BBCode): add target="_blank" only for absoulute URIs
Add target="_blank" only for absolute URIs. There are cases e.g. in the
news system to use BBCode for local links.

Fixes: 5bf190398db24d160977e315c0bd344a5be1bfc7

core/inc/util/GWF_BBCodeItem.php
@@ -371,7 +371,11 @@ final class GWF_BBCodeItem
 
 		$text = $this->filterURLText($text);
 
-		return sprintf('<a href="%s" target="_blank" rel="noopener noreferrer">%s</a>', $the_href, $text);
+		$is_absolute = GWF_Validator::isAbsoluteURL($url);
+		$target = $is_absolute ? '_blank' : '_self';
+		$rel = $is_absolute ? ' rel="noopener noreferrer"' : '';
+
+		return sprintf('<a href="%s" target="%s"%s>%s</a>', $the_href, $target, $rel, $text);
 	}
 
 	public function render_php($htmlspecial, $nl2br, $raw)

core/inc/util/GWF_Validator.php
@@ -10,7 +10,13 @@ final class GWF_Validator
 		if (Common::startsWith($url, '/')) {
 			return true;
 		}
+		// TODO: filter_var($url, FILTER_VALIDATE_URL); ?
 		return preg_match("/^[a-zA-Z]+[:\/\/]+[A-Za-z0-9\-_]+\\.+[A-Za-z0-9\.\/%&=\?\-_]+$/iD", $url) === 1;
+	}
+
+	public static function isAbsoluteURL($url)
+	{
+		return parse_url($url, PHP_URL_SCHEME) !== null;
 	}
 
 //	public static function filterURL($url, $maxlen=255)

commit 9c84fc77152223c5e81ba82e383ff4d1edb0c993

fix(GWF_BBCode): fix reference error
Fixes: 900913f273c79ce05b0deb28fdddb562188d5203

core/inc/util/GWF_BBCodeItem.php
@@ -371,11 +371,9 @@ final class GWF_BBCodeItem
 
 		$text = $this->filterURLText($text);
 
-		$is_absolute = GWF_Validator::isAbsoluteURL($url);
-		$target = $is_absolute ? '_blank' : '_self';
-		$rel = $is_absolute ? ' rel="noopener noreferrer"' : '';
+		$rel = GWF_Validator::isAbsoluteURL($the_href) ? ' target="_blank" rel="noopener noreferrer"' : '';
 
-		return sprintf('<a href="%s" target="%s"%s>%s</a>', $the_href, $target, $rel, $text);
+		return sprintf('<a href="%s"%s>%s</a>', $the_href, $rel, $text);
 	}
 
 	public function render_php($htmlspecial, $nl2br, $raw)