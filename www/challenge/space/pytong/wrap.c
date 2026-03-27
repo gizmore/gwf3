9851b7de3045dc01ada1e0854739f8a839a45a55
SpaceOne <space@wechall.net>
2026-03-27 01:29:36 +0100

    style(WC): cleanup whitespace in C challenge code
    using `clang-format -style="{BasedOnStyle: LLVM, IndentWidth: 2, TabWidth: 2, UseTab: Never}" -i`

--- www/challenge/space/pytong/wrap.c
+++ www/challenge/space/pytong/wrap.c
@@ -5,15 +5,14 @@
 #define PYTHON "/usr/bin/python"
 #define CHALLENGE "/home/level/12/pytong.py"
 
-int main(int argc, char **argv)
-{
-	if (argc < 2) {
-		printf("usage: %s file\n", *argv);
-		return 2;
-	}
-//	setgid(getegid());
-//	setuid(geteuid());
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("usage: %s file\n", *argv);
+        return 2;
+    }
+    // setgid(getegid());
+    // setuid(geteuid());
 
-	execle(PYTHON, PYTHON, CHALLENGE, argv[1], (char *)0, (char *)0);
-	return 1;
+    execle(PYTHON, PYTHON, CHALLENGE, argv[1], (char *)0, (char *)0);
+    return 1;
 }
--- www/challenge/warchall/choose_your_path3/warchall/charp3.c
+++ www/challenge/warchall/choose_your_path3/warchall/charp3.c
@@ -6,49 +6,44 @@
 
 #define BUFSIZE 256
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   FILE *fp;
   char buf[BUFSIZE];
   char *filename = "*-solution.txt";
   int lines, chars, cpl;
 
-  setregid(1645,1645); // set real and effective GID to levelxx
+  // clearenv();
+  setregid(1645, 1645); // set real and effective GID to levelxx
 
   printf("Counting %s ... \n", filename);
 
-  if (snprintf(buf, BUFSIZE, "/usr/bin/wc -l %s", filename)>=BUFSIZE)
-  {
+  if (snprintf(buf, BUFSIZE, "/usr/bin/wc -l %s", filename) >= BUFSIZE) {
     fprintf(stderr, "Filename %s is too long!\n", filename);
     exit(EXIT_FAILURE);
   }
 
-  fp= popen(buf, "r");
-  if (!fp)
-  {
+  fp = popen(buf, "r");
+  if (!fp) {
     fprintf(stderr, "Command execution failed! Sorry!\n");
     exit(EXIT_FAILURE);
   }
-  while (1==fscanf(fp, "%d", &lines))
-  {
+  while (1 == fscanf(fp, "%d", &lines)) {
     fgets(buf, BUFSIZE, fp);
   }
   pclose(fp);
 
   snprintf(buf, BUFSIZE, "/usr/bin/wc -c %s", filename);
-  fp= popen(buf, "r");
-  if (!fp)
-  {
+  fp = popen(buf, "r");
+  if (!fp) {
     fprintf(stderr, "Command execution failed! Bummer!\n");
     exit(EXIT_FAILURE);
   }
-  while (1==fscanf(fp, "%d", &chars))
-  {
+  while (1 == fscanf(fp, "%d", &chars)) {
     fgets(buf, BUFSIZE, fp);
   }
   pclose(fp);
 
-  cpl= (chars*lines) ? chars/lines : 0;
+  cpl = (chars * lines) ? chars / lines : 0;
 
   printf("Chars per line is %d.\n", cpl);
 
