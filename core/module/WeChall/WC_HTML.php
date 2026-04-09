0d215515a552e0b99d6aed0e98f7385e07731dab
gizmore <gizmore@wechall.net>
2026-04-09 09:54:17 +0200

    [WC] 4337 banner changes.

--- core/module/WeChall/WC_HTML.php
+++ core/module/WeChall/WC_HTML.php
@@ -320,7 +320,7 @@ final class WC_HTML
 			}
 		}
 
-        $text .= '<a href="/profile/WeChall" title="Anon#4337 - Support Crew">, WeChall(x4337)</a>'; # When we are satisfied with world-media, we go 4338, if not we go 4339.
+        $text .= '<a href="/profile/WeChall" title="Anon#4338 - Support Crew">, WeChall(x4338)</a>'; # When we will be satisfied with world-media, we go 4338, if we are really, we go 4339.
 		
 // 		$back .= '<div class="wc_head_bigbox" style="max-width:30%;">';
 		$back .= '<div class="wc_head_bigbox" style="float:none;">';
