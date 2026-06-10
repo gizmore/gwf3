9235b9ea6a73a414f6300280d0b48fe6a2a119dd
livinskull <livinskull@gmail.com>
2026-06-10 11:07:08 +0200

    Wechall: Add new site mrradio.kr
    Site uses default score format, works fine for linking locally :)

---
+++ core/module/WeChall/sites/korean/WCSite_MRR.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * https://mrradio.kr/
+ *
+ * Uses default format, so can use parent::parseStats() without overwriting
+ * username:rank:score:maxscore:challssolved:challcount:usercount
+ *
+ */
+class WCSite_MRR extends WC_Site
+{
+}
