1ffef567afa4e62b1b7f776c5757eada00b3dd9b
livinskull <livinskull@gmail.com>
2026-08-07 14:57:48 +0200

    Delete 'RankingSiteSite'
    The file is syntactically broken and can never work. It is also unused.

--- core/module/WeChall/method/RankingSiteSite.php
+++
@@ -1,9 +0,0 @@
-<?php
-final class WeChall_RankingSiteSite extends GWF_Method
-{
-	public function execute()
-	{
-		$query = "SELECT regat_uid, SUM(`regat_score`) FROM $regat users WHERE ";
-	}
-}
-?>
